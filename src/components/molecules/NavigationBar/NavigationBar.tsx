90f7ab5ded886a29384a851eea2a1baeebe602e2
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 09:01:36 -0500

    :sparkles: Create navigation bar

--- src/components/molecules/NavigationBar/NavigationBar.tsx
+++ src/components/molecules/NavigationBar/NavigationBar.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 
 const NavigationBar = () => {
   return (
-    <nav className="space-x-4">
-      <NavigationButton to="/about">About</NavigationButton>
-      <NavigationButton to="/services">Services</NavigationButton>
-      <NavigationButton to="/contact">Contact</NavigationButton>
+    <nav className="h-full justify-center flex w ">
+      <NavigationButton to="/about">Pacientes</NavigationButton>
+      <NavigationButton to="/services">Profesionales</NavigationButton>
+      <NavigationButton to="/contact">Especialistas</NavigationButton>
     </nav>
   );
 };
