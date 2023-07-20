76da0403fb83958caa91e9b8e7abcd76e8dec041
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 09:01:02 -0500

    :sparkles: Add landing images

---
+++ src/components/atoms/Images/LandingImage/LandingImage.tsx
@@ -0,0 +1,8 @@
+"use client";
+import Image from "next/image";
+
+const LandingImage = () => {
+  return <Image src={"/human_1.svg"} alt="user" width={350} height={350} />;
+};
+
+export default LandingImage;
---
+++ src/components/atoms/Images/LandingImage/LandingImage2.tsx
@@ -0,0 +1,8 @@
+"use client";
+import Image from "next/image";
+
+const LandingImage2 = () => {
+  return <Image src={"/human_2.svg"} alt="user" width={350} height={350} />;
+};
+
+export default LandingImage2;
