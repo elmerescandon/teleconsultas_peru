00520b833c09b19b0427b5fd88c7fc0e2f44aa48
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 09:01:14 -0500

    :sparkles: Create main infor landing

---
+++ src/components/templates/MainUserInformation/MainserInformation.tsx
@@ -0,0 +1,20 @@
+import LandingImage from "@/components/atoms/Images/LandingImage/LandingImage";
+import Image from "next/image";
+
+type MainUserInformationProps = {
+  title: string;
+  subtitle: string;
+};
+
+const MainUserInformation = ({ title, subtitle }: MainUserInformationProps) => {
+  return (
+    <div className="flex justify-around flex-col items-center sm:flex-row">
+      <div className="flex flex-col justify-center items-center sm:items-start px-10 gap-5">
+        <div className="font-bold text-5xl max-w-md">{title}</div>
+        <div className="text-xl max-w-md">{subtitle}</div>
+      </div>
+      <LandingImage />
+    </div>
+  );
+};
+export default MainUserInformation;
