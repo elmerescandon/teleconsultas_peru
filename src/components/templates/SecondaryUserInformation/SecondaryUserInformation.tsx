89919758768c11910e0abdcc033985d759eb70b5
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 09:01:26 -0500

    :sparkles: Create main secondary info landing

---
+++ src/components/templates/SecondaryUserInformation/SecondaryUserInformation.tsx
@@ -0,0 +1,26 @@
+import "./SecondaryUserInformation.scss";
+import LandingImage2 from "@/components/atoms/Images/LandingImage/LandingImage2";
+
+type SecondaryUserInformationProps = {
+  title: string;
+  info: string;
+};
+
+const SecondaryUserInformation = ({
+  title,
+  info,
+}: SecondaryUserInformationProps) => {
+  return (
+    <div className="secondary-info flex flex-col sm:flex-row justify-center items-center gap-5 px-10 bg-sky-blue">
+      <div>
+        <LandingImage2 />
+      </div>
+      <div className="flex flex-col items-center sm:items-start gap-2">
+        <div className="font-bold text-3xl max-w-md">{title}</div>
+        <div className="text-lg max-w-md">{info}</div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondaryUserInformation;
