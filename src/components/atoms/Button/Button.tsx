fdee34e85b03fc06a614bc05cb46465445515a73
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 01:43:33 -0500

    :sparkles: Add completely new button effect

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,14 +3,34 @@ import "./Button.scss";
 
 type ButtonProps = {
   content: string;
-  onChangeFunction: () => void;
 };
 
-const Button = ({ content, onChangeFunction }: ButtonProps) => {
+const Button = ({ content }: ButtonProps) => {
+  const buttonStyles = {
+    backgroundColor: "#000000",
+    color: "#FFFFFF",
+    borderRadius: "4px",
+    fontSize: "16px",
+    cursor: "pointer",
+    transition: "background-color 0.2s ease",
+  };
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#FFFFFF";
+    e.currentTarget.style.color = "#000000";
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#000000";
+    e.currentTarget.style.color = "#FFFFFF";
+  };
+
   return (
     <button
-      className="simple-button font-bold py-4 px-8 rounded-md md:ml-4"
-      onChange={onChangeFunction}
+      className="font-bold py-4 px-8 rounded-md h-12 flex items-center justify-center"
+      style={buttonStyles}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {content}
     </button>
