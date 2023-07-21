1b25ad2e616c28146a79076c7ed59723e88a96db
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 00:30:21 -0500

    :sparkles: Add card information component

--- src/components/molecules/CardInformation/CardInformation.tsx
+++ src/components/molecules/CardInformation/CardInformation.tsx
@@ -11,7 +11,7 @@ const CardInformation = ({
   icon,
 }: CardInformationProps) => {
   return (
-    <div className="card-information bg-white p-4 rounded-md shadow-md sm:w-64 w-128 h-80">
+    <div className="card-information bg-white p-4 rounded-md shadow-md sm:w-64 w-3/4 h-80">
       <div className="text-2xl text-center mb-4">{icon}</div>
       <div className="font-bold text-lg text-center mb-2">{title}</div>
       <div className="text-center">{description}</div>
