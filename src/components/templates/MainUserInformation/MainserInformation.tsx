f59dd6db310c16a9e9eb4eec0331b4300bc3c45a
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 23:31:43 -0500

    :lipstick: Main user update with folder

--- src/components/templates/MainUserInformation/MainserInformation.tsx
+++ src/components/templates/MainUserInformation/MainserInformation.tsx
@@ -1,3 +1,4 @@
+import "./MainUserInformation.scss";
 import LandingImage from "@/components/atoms/Images/LandingImage/LandingImage";
 import Image from "next/image";
 
@@ -8,7 +9,7 @@ type MainUserInformationProps = {
 
 const MainUserInformation = ({ title, subtitle }: MainUserInformationProps) => {
   return (
-    <div className="flex justify-around flex-col items-center sm:flex-row">
+    <div className="main_information flex justify-around flex-col items-center sm:flex-row">
       <div className="flex flex-col justify-center items-center sm:items-start px-10 gap-5">
         <div className="font-bold text-5xl max-w-md">{title}</div>
         <div className="text-xl max-w-md">{subtitle}</div>
