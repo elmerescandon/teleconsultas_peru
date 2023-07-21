0a2e132becdd8b7b8e1f477c4b6326f72f5bed16
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 01:42:50 -0500

    :lipstick: Correct suscribe to newsletter container

--- src/components/templates/EmailSubscription/EmailSubscription.tsx
+++ src/components/templates/EmailSubscription/EmailSubscription.tsx
@@ -8,16 +8,13 @@ const EmailSubscription = () => {
   return (
     <div className="big-email-subscription">
       <div className="email-subscription py-32 flex justify-center">
-        <div className="w-max max-w-lg px-4">
+        <div className="email-subscription__data flex flex-col items-center justify-center px-16 sm:px-64 py-8 rounded-2xl">
           <h2 className="text-2xl md:text-5xl font-bold text-center py-8">
             Subscribe to our newsletter!
           </h2>
-          <div className="flex flex-col md:flex-row items-center">
+          <div className="flex flex-col md:flex-row items-center gap-10">
             <Input type="email" placeholder="Enter your email" />
-            <Button
-              content="SUBSCRIBE"
-              onChangeFunction={() => console.log("On Click")}
-            />
+            <Button content="SUBSCRIBE" />
           </div>
         </div>
       </div>
