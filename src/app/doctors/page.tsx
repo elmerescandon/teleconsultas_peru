653853865e31dd272076ca7ecfd7c247b39280b0
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 01:42:09 -0500

    :hammer: Create new pages

---
+++ src/app/doctors/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/organisms/Footer/Footer";
+import Header from "@/components/organisms/Header/Header";
+import Image from "next/image";
+import React from "react";
+
+const Doctors = () => {
+  return (
+    <main>
+      <Header />
+      <div className="doctors">Doctors</div>
+      <Footer />
+    </main>
+  );
+};
+
+export default Doctors;
---
+++ src/app/patients/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/organisms/Footer/Footer";
+import Header from "@/components/organisms/Header/Header";
+
+const Patients = () => {
+  return (
+    <main>
+      <Header />
+      <div className="patients">Patients</div>
+      <Footer />
+    </main>
+  );
+};
+
+export default Patients;
