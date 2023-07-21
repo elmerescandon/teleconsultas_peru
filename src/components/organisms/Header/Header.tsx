1099cf57d7da21b64416045079a5fc6699a011d2
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 01:48:39 -0500

    :sparkles: Add login bar to header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/atoms/Logo/Logo";
+import LoginBar from "@/components/molecules/LoginBar/LoginBar";
 import NavigationBar from "@/components/molecules/NavigationBar/NavigationBar";
 // import SearchBar from "@/components/molecules/SearchBar/SearchBar";
 
@@ -6,7 +7,10 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center px-10">
       <Logo />
-      <NavigationBar />
+      <div className="flex gap-8 items-center">
+        <NavigationBar />
+        <LoginBar />
+      </div>
       {/* <SearchBar/> */}
     </header>
   );
