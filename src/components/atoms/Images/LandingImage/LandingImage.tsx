5d91684712a5b4f16ad5a482e7d3848779ec3d46
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 03:27:53 -0500

    :sparkles: Update landing

--- src/components/atoms/Images/LandingImage/LandingImage.tsx
+++ src/components/atoms/Images/LandingImage/LandingImage.tsx
@@ -2,7 +2,15 @@
 import Image from "next/image";
 
 const LandingImage = () => {
-  return <Image src={"/human_1.svg"} alt="user" width={350} height={350} />;
+  return (
+    <Image
+      className="rounded-xl"
+      src={"/doctors_3.jpg"}
+      alt="user"
+      width={900}
+      height={600}
+    />
+  );
 };
 
 export default LandingImage;
