a5e245236060af882ad02862e437f89fc448357d
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 00:29:49 -0500

    :sparkles: Add new values to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Footer from "@/components/organisms/Footer/Footer";
 import Header from "@/components/organisms/Header/Header";
 import AspectsInformation from "@/components/templates/AspectsInformation/AspectsInformation";
+import EmailSubscription from "@/components/templates/EmailSubscription/EmailSubscription";
 import MainUserInformation from "@/components/templates/MainUserInformation/MainserInformation";
 import SecondaryUserInformation from "@/components/templates/SecondaryUserInformation/SecondaryUserInformation";
 
@@ -9,15 +11,20 @@ const Home = () => {
   return (
     <main>
       <Header />
-      <MainUserInformation
-        title="Welcome to MediConnect - Your Gateway to Convenient Healthcare."
-        subtitle="Connecting Patients and Doctors for Seamless Online Medical Consultation."
-      />
-      <SecondaryUserInformation
-        title="Your Health, Your Time, Your Way"
-        info={infoHome}
-      />
-      <AspectsInformation />
+      <div className="home">
+        <MainUserInformation
+          title="Welcome to MediConnect - Your Gateway to Convenient Healthcare."
+          subtitle="Connecting Patients and Doctors for Seamless Online Medical Consultation."
+        />
+        <SecondaryUserInformation
+          title="Your Health, Your Time, Your Way"
+          info={infoHome}
+        />
+        <AspectsInformation />
+        <EmailSubscription />
+      </div>
+
+      <Footer />
     </main>
   );
 };
