0267f7250af3378ef478ecbc292bb92760010d95
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 23:32:00 -0500

    :lipstick: Secondary user update with folder

--- src/components/templates/SecondaryUserInformation/SecondaryUserInformation.tsx
+++ src/components/templates/SecondaryUserInformation/SecondaryUserInformation.tsx
@@ -11,13 +11,15 @@ const SecondaryUserInformation = ({
   info,
 }: SecondaryUserInformationProps) => {
   return (
-    <div className="secondary-info flex flex-col sm:flex-row justify-center items-center gap-5 px-10 bg-sky-blue">
-      <div>
-        <LandingImage2 />
-      </div>
-      <div className="flex flex-col items-center sm:items-start gap-2">
-        <div className="font-bold text-3xl max-w-md">{title}</div>
-        <div className="text-lg max-w-md">{info}</div>
+    <div className="big-secondary-info bg-sky-blue">
+      <div className="secondary-info flex flex-col sm:flex-row justify-center items-center gap-5 px-10 bg-sky-blue">
+        <div>
+          <LandingImage2 />
+        </div>
+        <div className="flex flex-col items-center sm:items-start gap-2">
+          <div className="font-bold text-3xl max-w-md">{title}</div>
+          <div className="text-lg max-w-md">{info}</div>
+        </div>
       </div>
     </div>
   );
