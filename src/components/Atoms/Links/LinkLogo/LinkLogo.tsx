46fc3b8c47d951d00560123da2054f9f007006ee
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-08 21:16:52 -0500

    :sparkles: Create types of links

---
+++ src/components/Atoms/Links/LinkLogo/LinkLogo.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const LinkLogo = () => {
+  return (
+    <Link href={"/"} className="text-2xl font-semibold">
+      Aika Salud
+    </Link>
+  );
+};
+
+export default LinkLogo;
---
+++ src/components/Atoms/Links/LinkSecondary/LinkSecondary.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+type LinkSecondaryProps = {
+  children: React.ReactNode;
+  to: string;
+};
+
+const LinkSecondary = ({ children, to }: LinkSecondaryProps) => {
+  return (
+    <Link
+      className="text-brand-600 bg-classic-white rounded-lg p-2 text-base font-semibold"
+      href={to}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LinkSecondary;
