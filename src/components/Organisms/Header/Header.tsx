0d600321c8212333895a949986d65caa87ba68fc
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-08 21:17:36 -0500

    :hammer: Create header component

---
+++ src/components/Organisms/Header/Header.tsx
@@ -0,0 +1,18 @@
+import LinkLogo from "@/components/Atoms/Links/LinkLogo/LinkLogo";
+import LoginBar from "@/components/Molecules/LoginBar/LoginBar";
+import NavigationBar from "@/components/Molecules/NavigationBar/NavigationBar";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header flex flex-row justify-between items-center py-12">
+      <LinkLogo />
+      <div className="header-bar flex gap-28">
+        <NavigationBar />
+        <LoginBar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
