faaec9620b606d6cf1f1e2f6ab50c6b8d282d599
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-08 21:17:13 -0500

    :hammer: Implement first navigation bar

---
+++ src/components/Molecules/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,16 @@
+import LinkSecondary from "@/components/Atoms/Links/LinkSecondary/LinkSecondary";
+import Routes from "@/utils/routes/Routes";
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <div className="navigation-bar flex flex-row items-center gap-7">
+      <LinkSecondary to={Routes.RESERVE}>{"¡Reserva tu cita!"}</LinkSecondary>
+      <LinkSecondary to={Routes.PATIENTS}>{"Pacientes"}</LinkSecondary>
+      <LinkSecondary to={Routes.DOCTORS}>{"Doctores"}</LinkSecondary>
+      <LinkSecondary to={Routes.ESPECIALITY}>{"Especialidades"}</LinkSecondary>
+    </div>
+  );
+};
+
+export default NavigationBar;
