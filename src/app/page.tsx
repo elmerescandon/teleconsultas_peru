1d0c1fae199cbd4ee4e1c64d043d3b8f65baba48
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-08 19:38:54 -0500

    :fire: Eliminate components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Footer from "@/components/organisms/Footer/Footer";
-import Header from "@/components/organisms/Header/Header";
-import AspectsInformation from "@/components/templates/AspectsInformation/AspectsInformation";
-import EmailSubscription from "@/components/templates/EmailSubscription/EmailSubscription";
-import MainUserInformation from "@/components/templates/MainUserInformation/MainserInformation";
-import SecondaryUserInformation from "@/components/templates/SecondaryUserInformation/SecondaryUserInformation";
-
 const Home = () => {
 
 
