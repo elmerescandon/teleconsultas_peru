33bdb0d1aa0748a6e5653d844a3dc539919fc50e
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-08 21:16:32 -0500

    :sparkles: Add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import Header from "@/components/Organisms/Header/Header";
+
 const Home = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 };
 
 export default Home;
