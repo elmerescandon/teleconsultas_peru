9962546596d146e1154431b68c240a56f6008580
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 07:10:00 -0500

    :sparkles: Create Data Section

--- src/components/Organisms/CardSection/CardSection.tsx
+++ src/components/Organisms/CardSection/CardSection.tsx
@@ -1,9 +1,34 @@
 import CardFeature from "@/components/Molecules/CardFeature/CardFeature";
-import cardsContent from "@/utils/content/home/CardsContent";
+import ICardContent from "@/utils/Interfaces/ICardContent";
+
+import {
+    LockClosedIcon,
+    ClockIcon,
+    VideoCameraIcon,
+    ComputerDesktopIcon,
+} from "@heroicons/react/24/outline";
 
 const CardSection = () => {
+    const cardsContent: ICardContent[] = [
+        {
+            title: "A un click de distancia",
+            logo: <ComputerDesktopIcon className="w-16 h-16 text-brand-900" />,
+        },
+        {
+            title: "Privacidad y seguridad",
+            logo: <LockClosedIcon className="w-16 h-16 text-brand-900" />,
+        },
+        {
+            title: "Reserva de citas inmediata",
+            logo: <ClockIcon className="w-16 h-16 text-brand-900" />,
+        },
+        {
+            title: "Consultas por videollamada",
+            logo: <VideoCameraIcon className="w-16 h-16 text-brand-900" />,
+        },
+    ];
     return (
-        <div className="flex gap-8 justify-center">
+        <div className="flex gap-10 justify-center flex-wrap max-md:flex-col max-md:items-center py-20">
             {cardsContent.map((cardContent, index) => {
                 return (
                     <CardFeature
