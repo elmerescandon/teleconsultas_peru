2a48d350cfeefdf58a883e94ee503349ec05002c
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-12 23:29:41 -0500

    :sparkles: Create card section

--- src/components/Organisms/CardSection/CardSection.tsx
+++ src/components/Organisms/CardSection/CardSection.tsx
@@ -1,7 +1,20 @@
 import CardFeature from "@/components/Molecules/CardFeature/CardFeature";
+import cardsContent from "@/utils/content/home/CardsContent";
 
 const CardSection = () => {
-    return <div></div>;
+    return (
+        <div className="flex gap-8 justify-center">
+            {cardsContent.map((cardContent, index) => {
+                return (
+                    <CardFeature
+                        key={index}
+                        iconComponent={cardContent.logo}
+                        title={cardContent.title}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 
 export default CardSection;
