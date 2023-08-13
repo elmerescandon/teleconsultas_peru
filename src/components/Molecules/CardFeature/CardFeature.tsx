4d9c6631664f34816125c5d4a61230d7fdc6ca9c
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 07:44:35 -0500

    :lipstick: Modify card feature

--- src/components/Molecules/CardFeature/CardFeature.tsx
+++ src/components/Molecules/CardFeature/CardFeature.tsx
@@ -7,9 +7,9 @@ type CardFeatureProps = {
 
 const CardFeature = ({ iconComponent, title }: CardFeatureProps) => {
     return (
-        <div className="h-64 flex flex-col gap-5 items-center bg-brand-50 py-8 px-3 rounded-xl">
+        <div className="h-80 flex flex-col gap-5 items-center bg-brand-50 py-8 px-10 rounded-xl">
             <div className="rounded-full bg-brand-100 p-4">{iconComponent}</div>
-            <div className="text-center text-2xl font-semibold w-56">
+            <div className="text-center text-2xl font-semibold w-40">
                 {title}
             </div>
         </div>
