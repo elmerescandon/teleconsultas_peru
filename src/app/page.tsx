041196c7acc4eeb66ff14b8b15aeebef719ed1e1
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 07:11:05 -0500

    :sparkles: Add sections to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import CardSection from "@/components/Organisms/CardSection/CardSection";
+import DataSection from "@/components/Organisms/DataSection/DataSection";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import MainSection from "@/components/Organisms/MainSection/MainSection";
+import SecondarySection from "@/components/Organisms/SecondarySection/SecondarySection";
 import Routes from "@/utils/routes/Routes";
 
 const Home = () => {
@@ -22,6 +24,29 @@ const Home = () => {
                     linkName={"¡Reserva tu cita ya!"}
                 />
                 <CardSection />
+            </div>
+            <SecondarySection
+                buttonInfo="Ver más..."
+                content="Aika Salud te conecta con profesionales de acuerdo a tu disponibilidad."
+                miniHeader="Para Pacientes"
+                order="left"
+                pathImage="/doctors_1.jpg"
+                title="Agenda tu cita donde quieras cuando quieras"
+                to={Routes.PATIENTS}
+                sizeImage={500}
+            />
+            <SecondarySection
+                buttonInfo="Ver más..."
+                content="Nuestra plataforma contiene todas las herramientas necesarias para ti."
+                miniHeader="Para Doctores"
+                order="right"
+                pathImage="/doctors_2.jpg"
+                title="Atiende sin pensar en reportes y recetas"
+                to={Routes.DOCTORS}
+                sizeImage={500}
+            />
+            <div className="page-body">
+                <DataSection />
             </div>
             <Footer />
         </main>
