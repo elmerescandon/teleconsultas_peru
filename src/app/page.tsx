0a668110cfb8b1ae8fb59a04a60cdcc09cdfdee8
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-12 23:04:54 -0500

    :sparkles: Add card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import Footer from "@/components/Organisms/Footer/Footer";
+import CardSection from "@/components/Organisms/CardSection/CardSection";
 import Header from "@/components/Organisms/Header/Header";
+import MainSection from "@/components/Organisms/MainSection/MainSection";
+import Routes from "@/utils/routes/Routes";
 
 const Home = () => {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+    return (
+        <main>
+            <Header />
+            <MainSection
+                title={
+                    "Bienvenido a Aika Salud - Tu puerta a una atención de calidad"
+                }
+                subtitle={
+                    "Conectando pacientes y doctores para una atención médica personalizada en línea"
+                }
+                linkRef={Routes.RESERVE}
+                linkName={"¡Reserva tu cita ya!"}
+            />
+            {/* <CardSection /> */}
+        </main>
+    );
 };
 
 export default Home;
