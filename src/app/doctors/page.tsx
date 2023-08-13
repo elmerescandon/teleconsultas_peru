c05cb4b466b7be7d1c4c47774303a491fc6ef435
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 07:45:21 -0500

    :hammer: Add info to page doctors

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -1,5 +1,29 @@
+import Footer from "@/components/Organisms/Footer/Footer";
+import Header from "@/components/Organisms/Header/Header";
+import MainSection from "@/components/Organisms/MainSection/MainSection";
+import DoctorPersonsSection from "@/components/Templates/DoctorPersonsSection/DoctorPersonsSection";
+import DoctorsCardSection from "@/components/Templates/DoctorsCardSection/DoctorsCardSection";
+import Routes from "@/utils/routes/Routes";
+
 const Doctors = () => {
-  return <main>Doctors</main>;
+    return (
+        <main>
+            <div className="page-body">
+                <Header />
+                <MainSection
+                    title={"Expande tu práctica médica con MediConnect"}
+                    subtitle={
+                        "Llega y atiende a pacientes más allá de tu consultorio a través de tus consultas en línea"
+                    }
+                    linkRef={Routes.RESERVE}
+                    linkName={"¡Reserva tu cita ya!"}
+                />
+                <DoctorsCardSection />
+                <DoctorPersonsSection />
+            </div>
+            <Footer />
+        </main>
+    );
 };
 
 export default Doctors;
