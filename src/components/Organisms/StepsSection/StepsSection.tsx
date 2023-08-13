f8207e78b2c7bbd2b6b9bba9d6dbcc4200bc961d
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 10:55:35 -0500

    :sparkles: Add steps

---
+++ src/components/Organisms/StepsSection/StepsSection.tsx
@@ -0,0 +1,36 @@
+import CardCellPhone from "@/components/Molecules/CardCellPhone/CardCellPhone";
+import React from "react";
+
+const StepsSection = () => {
+    const steps = [
+        {
+            stepNumber: 1,
+            title: "Regístrate",
+            info: "Ingresa tu nombre completo, DNI y número telefónico.",
+        },
+        {
+            stepNumber: 2,
+            title: "Reserva tu cita",
+            info: "Selecciona la fecha y hora con 24 horas de anticipación.",
+        },
+        {
+            stepNumber: 3,
+            title: "Realiza el pago",
+            info: "Paga con tarjeta de crédito o débito.",
+        },
+    ];
+    return (
+        <div className="py-10 bg-brand-50">
+            <div className="text-5xl pb-12 font-semibold px-48">
+                ¡Empezar es sencillo!
+            </div>
+            <div className="flex justify-center gap-44">
+                {steps.map((step) => {
+                    return <CardCellPhone {...step} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default StepsSection;
