59471173889b3db6553da811858c14e4ff326b42
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 08:24:15 -0500

    :hammer: Create patients page

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
+import InformationForm from "@/components/Organisms/InformationForm/InformationForm";
 import MainSection from "@/components/Organisms/MainSection/MainSection";
 import DoctorPersonsSection from "@/components/Templates/DoctorPersonsSection/DoctorPersonsSection";
 import DoctorsCardSection from "@/components/Templates/DoctorsCardSection/DoctorsCardSection";
@@ -20,6 +21,9 @@ const Doctors = () => {
                 />
                 <DoctorsCardSection />
                 <DoctorPersonsSection />
+                <InformationForm
+                    title={"¡Pertenece a Aika Salud y únete al cambio!"}
+                />
             </div>
             <Footer />
         </main>
