b82f3b732c48fca8be88add4be4dffc12fb3d23e
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 07:10:28 -0500

    :sparkles: Create secondary section

---
+++ src/components/Organisms/SecondarySection/SecondarySection.tsx
@@ -0,0 +1,51 @@
+import InformationLanding from "@/components/Molecules/InformationLanding/InformationLanding";
+import Image from "next/image";
+import React from "react";
+
+type SecondarySectionProps = {
+    order: string;
+    miniHeader: string;
+    title: string;
+    content: string;
+    to: string;
+    pathImage: string;
+    buttonInfo: string;
+    sizeImage: number;
+};
+
+const SecondarySection = ({
+    order,
+    buttonInfo,
+    content,
+    miniHeader,
+    pathImage,
+    title,
+    to,
+    sizeImage,
+}: SecondarySectionProps) => {
+    return (
+        <div
+            className={`flex ${
+                order === "left" ? "flex-row" : "flex-row-reverse"
+            } items-center bg-brand-50 justify-center`}
+        >
+            <InformationLanding
+                buttonInfo={buttonInfo}
+                content={content}
+                miniHeader={miniHeader}
+                title={title}
+                to={to}
+            />
+
+            <Image
+                className="max-w-xl rounded-xl border-brand-600 border-2"
+                src={pathImage}
+                height={sizeImage ? sizeImage : "50"}
+                width={sizeImage ? sizeImage : "50"}
+                alt={`${miniHeader}-image`}
+            />
+        </div>
+    );
+};
+
+export default SecondarySection;
