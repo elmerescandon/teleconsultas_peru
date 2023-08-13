67c8189a227aaa57520ad33a78a84d16552f6576
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-12 23:04:39 -0500

    :sparkles: Modify the buttons and links

--- src/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary.tsx
+++ src/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 type ButtonPrimaryProps = {
-  children: React.ReactNode;
-  onClickFn: () => void;
+    children: React.ReactNode;
+    onClickFn: () => void;
 };
 
 const ButtonPrimary = ({ children, onClickFn }: ButtonPrimaryProps) => {
-  return (
-    <button
-      className="text-basic-white bg-brand-600 rounded-lg p-2 text-sm font-normal"
-      onChange={onClickFn}
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button
+            className="text-basic-white bg-brand-600 rounded-lg p-2 text-sm font-normal"
+            onChange={onClickFn}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default ButtonPrimary;
--- src/components/Atoms/Links/LinkPrimary/LinkPrimary.tsx
+++ src/components/Atoms/Links/LinkPrimary/LinkPrimary.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import React from "react";
 
 type LinkPrimaryProps = {
-  children: React.ReactNode;
-  to: string;
+    children: React.ReactNode;
+    to: string;
 };
 
 const LinkPrimary = ({ children, to }: LinkPrimaryProps) => {
-  return (
-    <Link
-      href={to}
-      className="text-basic-white bg-brand-600 rounded-lg p-2 text-sm font-normal max-h-9"
-    >
-      {children}
-    </Link>
-  );
+    return (
+        <Link
+            href={to}
+            className="text-basic-white bg-brand-600 rounded-lg p-2 text-sm font-normal max-h-9"
+        >
+            {children}
+        </Link>
+    );
 };
 
 export default LinkPrimary;
