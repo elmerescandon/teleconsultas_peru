9f003d4e66b95ca75b418f4dd88cc9327851e430
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-12 23:13:34 -0500

    :sparkle: Test with cards

---
+++ src/utils/content/home/CardsContent.tsx
@@ -0,0 +1,28 @@
+import ICardContent from "@/utils/Interfaces/ICardContent";
+import {
+    ComputerDesktopIcon,
+    LockClosedIcon,
+    ClockIcon,
+    VideoCameraIcon,
+} from "@heroicons/react/24/solid";
+
+const cardsContent: ICardContent[] = [
+    {
+        title: "A un click de distancia",
+        // logo: <ComputerDesktopIcon className="w-10 h-10 text-brand-900" />,
+    },
+    {
+        title: "Privacidad y seguridad",
+        // logo: <LockClosedIcon className="w-10 h-10 text-brand-900" />,
+    },
+    {
+        title: "Reserva de citas inmediata",
+        // logo: <ClockIcon className="w-10 h-10 text-brand-900" />,
+    },
+    {
+        title: "Consultas por videollamada",
+        // logo: <VideoCameraIcon className="w-10 h-10 text-brand-900" />,
+    },
+];
+
+export default cardsContent;
