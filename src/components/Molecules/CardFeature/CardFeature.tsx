9a6c808dbc871458ccee45406ece266c1275bd72
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-12 23:29:30 -0500

    :sparkles: Add icon to feature component

--- src/components/Molecules/CardFeature/CardFeature.tsx
+++ src/components/Molecules/CardFeature/CardFeature.tsx
@@ -1,10 +1,14 @@
+import React from "react";
+
 type CardFeatureProps = {
+    iconComponent: React.ReactNode;
     title: string;
 };
 
-const CardFeature = ({ title }: CardFeatureProps) => {
+const CardFeature = ({ iconComponent, title }: CardFeatureProps) => {
     return (
         <div className="w-44 h-44 flex flex-col gap-5">
+            {iconComponent}
             <div>{title}</div>
         </div>
     );
