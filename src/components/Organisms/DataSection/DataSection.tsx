c3bea1206b7b3e88968486101b6983a88c9ee991
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-13 07:10:41 -0500

    :sparkles: Create data section

---
+++ src/components/Organisms/DataSection/DataSection.tsx
@@ -0,0 +1,49 @@
+import CircleFeature from "@/components/Molecules/CircleFeature/CircleFeature";
+import IDataSection from "@/utils/Interfaces/IDataSection";
+import React from "react";
+
+const DataSection = () => {
+    const dataFeatures: IDataSection[] = [
+        {
+            title: "9.3/10",
+            subtitle: "Es la satisfaccion de nuestras consultas",
+            color: "light",
+            position: "bottom",
+        },
+        {
+            title: "85%",
+            subtitle: "de nuestros clientes nos recomiendan",
+            color: "normal",
+            position: "top",
+        },
+        {
+            title: "80%",
+            subtitle: "de nuestros clientes están satisfechos",
+            color: "light",
+            position: "bottom",
+        },
+        {
+            title: "76%",
+            subtitle: "consideran Aika Salud su primera opción",
+            color: "dark",
+            position: "top",
+        },
+    ];
+    return (
+        <div className="flex py-20 pt-36 justify-center">
+            {dataFeatures.map((dataFeature, index) => {
+                return (
+                    <CircleFeature
+                        title={dataFeature.title}
+                        subtitle={dataFeature.subtitle}
+                        color={dataFeature.color}
+                        position={dataFeature.position}
+                        key={index}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default DataSection;
