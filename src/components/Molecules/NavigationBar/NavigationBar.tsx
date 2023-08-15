5e21b144139529c0af50ecd24f27578deb15dc39
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-15 08:07:59 -0500

    :sparkles: Order navigation bar

--- src/components/Molecules/NavigationBar/NavigationBar.tsx
+++ src/components/Molecules/NavigationBar/NavigationBar.tsx
@@ -3,14 +3,18 @@ import Routes from "@/utils/routes/Routes";
 import React from "react";
 
 const NavigationBar = () => {
-  return (
-    <div className="navigation-bar flex flex-row items-center gap-7">
-      <LinkSecondary to={Routes.RESERVE}>{"¡Reserva tu cita!"}</LinkSecondary>
-      <LinkSecondary to={Routes.PATIENTS}>{"Pacientes"}</LinkSecondary>
-      <LinkSecondary to={Routes.DOCTORS}>{"Doctores"}</LinkSecondary>
-      <LinkSecondary to={Routes.ESPECIALITY}>{"Especialidades"}</LinkSecondary>
-    </div>
-  );
+    return (
+        <div className="navigation-bar flex flex-row items-center gap-7">
+            <LinkSecondary to={Routes.RESERVE}>
+                {"¡Reserva tu cita!"}
+            </LinkSecondary>
+            <LinkSecondary to={Routes.PATIENTS}>{"Pacientes"}</LinkSecondary>
+            <LinkSecondary to={Routes.DOCTORS}>{"Doctores"}</LinkSecondary>
+            <LinkSecondary to={Routes.ESPECIALITY}>
+                {"Especialidades"}
+            </LinkSecondary>
+        </div>
+    );
 };
 
 export default NavigationBar;
