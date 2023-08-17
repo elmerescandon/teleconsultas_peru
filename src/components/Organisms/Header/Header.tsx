ce65a8f2026155f0b80eebaf3775447b9519d27c
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-17 07:52:31 -0500

    :iphone: Add BurgerHeader menu

--- src/components/Organisms/Header/Header.tsx
+++ src/components/Organisms/Header/Header.tsx
@@ -2,16 +2,20 @@ import LinkLogo from "@/components/Atoms/Links/LinkLogo/LinkLogo";
 import LoginBar from "@/components/Molecules/LoginBar/LoginBar";
 import NavigationBar from "@/components/Molecules/NavigationBar/NavigationBar";
 import React from "react";
+import BurgerHeader from "../BurgerHeader/BurgerHeader";
 
 const Header = () => {
     return (
-        <div className="flex flex-row justify-between items-center py-12">
-            <LinkLogo />
-            <div className="header-bar flex gap-28">
-                <NavigationBar />
-                <LoginBar />
+        <header>
+            <BurgerHeader />
+            <div className="max-lg:hidden flex flex-row justify-between items-center py-12 px-48 max-2xl:px-10">
+                <LinkLogo />
+                <div className="flex gap-28 max-xl:gap-8">
+                    <NavigationBar />
+                    <LoginBar />
+                </div>
             </div>
-        </div>
+        </header>
     );
 };
 
