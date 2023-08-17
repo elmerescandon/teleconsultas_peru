e9d8762d02ef913d67f2fc99690c12e951608f33
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-17 07:52:21 -0500

    :iphone: Correct main section responsiveness

--- src/components/Organisms/MainSection/MainSection.tsx
+++ src/components/Organisms/MainSection/MainSection.tsx
@@ -16,11 +16,15 @@ const MainSection = ({
     linkName,
 }: MainSectionProps) => {
     return (
-        <div className="flex flex-col items-center px-56 py-20">
+        <div className="flex flex-col items-center px-56 py-20 max-lg:justify-center max-lg:pb-5 max-lg:px-20 max-lg:pt-36 max-sm:px-5">
             <div className="flex flex-col gap-4">
-                <h1 className="text-5xl font-semibold">{title}</h1>
-                <h5 className="text-2xl font-semibold">{subtitle}</h5>
-                <div className="py-5">
+                <h1 className="text-5xl font-semibold max-lg:text-center">
+                    {title}
+                </h1>
+                <h5 className="text-2xl font-semibold max-lg:text-center">
+                    {subtitle}
+                </h5>
+                <div className="py-5 text-center">
                     <LinkPrimary to={linkRef}>{linkName}</LinkPrimary>
                 </div>
             </div>
