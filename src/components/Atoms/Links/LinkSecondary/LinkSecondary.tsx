93d4370efbb748f6885a251f3a9eddcff075841f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-17 07:54:13 -0500

    :sparkles: Corret sizing

--- src/components/Atoms/Links/LinkSecondary/LinkSecondary.tsx
+++ src/components/Atoms/Links/LinkSecondary/LinkSecondary.tsx
@@ -2,19 +2,20 @@ import Link from "next/link";
 import React from "react";
 
 type LinkSecondaryProps = {
-  children: React.ReactNode;
-  to: string;
+    children: React.ReactNode;
+    to: string;
 };
 
 const LinkSecondary = ({ children, to }: LinkSecondaryProps) => {
-  return (
-    <Link
-      className="text-brand-600 bg-classic-white rounded-lg p-2 text-base font-semibold"
-      href={to}
-    >
-      {children}
-    </Link>
-  );
+    return (
+        <Link
+            className="text-brand-600 bg-classic-white rounded-lg p-2 text-base font-semibold
+       flex justify-between"
+            href={to}
+        >
+            {children}
+        </Link>
+    );
 };
 
 export default LinkSecondary;
