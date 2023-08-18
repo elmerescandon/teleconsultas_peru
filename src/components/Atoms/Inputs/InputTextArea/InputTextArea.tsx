d429034e35133b537c6edb7588d68fbf9b649d32
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-17 23:41:55 -0500

    :iphone: Make input textarea responsive

--- src/components/Atoms/Inputs/InputTextArea/InputTextArea.tsx
+++ src/components/Atoms/Inputs/InputTextArea/InputTextArea.tsx
@@ -7,9 +7,9 @@ type InputTextAreaProps = {
 
 const InputTextArea = ({ rows, cols }: InputTextAreaProps) => {
     return (
-        <div>
+        <div className="flex">
             <textarea
-                className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md px-4 resize-none"
+                className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md px-4 resize-none max-xl:w-full"
                 rows={rows}
                 cols={cols}
             />
