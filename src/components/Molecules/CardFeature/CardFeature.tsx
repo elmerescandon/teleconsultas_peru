3bb551c354df1d0e57e9920e8c159986c22815d1
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-17 23:42:04 -0500

    :iphone: Make card feature responsive

--- src/components/Molecules/CardFeature/CardFeature.tsx
+++ src/components/Molecules/CardFeature/CardFeature.tsx
@@ -7,7 +7,7 @@ type CardFeatureProps = {
 
 const CardFeature = ({ iconComponent, title }: CardFeatureProps) => {
     return (
-        <div className="h-80 flex flex-col gap-5 items-center bg-brand-50 py-8 px-10 rounded-xl">
+        <div className="h-80 flex flex-col gap-5 items-center bg-brand-50 py-8 px-10 rounded-xl max-xl:h-64 max-xl:px-5">
             <div className="rounded-full bg-brand-100 p-4">{iconComponent}</div>
             <div className="text-center text-2xl font-semibold w-40">
                 {title}
