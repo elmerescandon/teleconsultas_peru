67322fae8ff16903c36f280da39dc20b1380a4f1
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-17 23:41:49 -0500

    :iphone: Make input text responsive

--- src/components/Atoms/Inputs/InputText/InputText.tsx
+++ src/components/Atoms/Inputs/InputText/InputText.tsx
@@ -9,7 +9,7 @@ const InputText = ({ placeholder }: InputTextProps) => {
         <div>
             <input
                 type="text"
-                className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md h-12 px-4 pr-10"
+                className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md h-12 px-4 pr-10 max-xl:w-full"
                 placeholder={placeholder}
             />
         </div>
