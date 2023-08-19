f288a67f4a81c0d6af9968dce510712dbb269e2f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-19 18:06:21 -0500

    :sparkles: Add size prop in link logo

--- src/components/Atoms/Links/LinkLogo/LinkLogo.tsx
+++ src/components/Atoms/Links/LinkLogo/LinkLogo.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import React from "react";
 
-const LinkLogo = () => {
-  return (
-    <Link href={"/"} className="text-2xl font-semibold">
-      Aika Salud
-    </Link>
-  );
+type LinkLogoProps = {
+    size?: string;
+};
+
+const LinkLogo = ({ size }: LinkLogoProps) => {
+    return (
+        <Link
+            href={"/"}
+            className={`${
+                size === "big" ? "text-5xl" : "text-2xl"
+            } font-semibold `}
+        >
+            Aika Salud
+        </Link>
+    );
 };
 
 export default LinkLogo;
