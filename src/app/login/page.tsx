508f6a50d279d9287783c731fe95ecb959520631
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-19 18:05:09 -0500

    :sparkles: Create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import LoginSection from "@/components/Templates/LoginSection/LoginSection";
+import Image from "next/image";
+import React from "react";
+
+const Reserve = () => {
+    return (
+        <div className="flex w-full">
+            <LoginSection />
+            <Image
+                src="/telecom_landing.jpg"
+                alt="landing-telemedicine"
+                width={1000}
+                height={1000}
+                className="w-1/2 max-h-screen max-xl:hidden object-cover"
+            />
+        </div>
+    );
+};
+
+export default Reserve;
