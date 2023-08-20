5a9b5431ed27a4566e02dc8f71dc17b9797741e9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-20 12:55:58 -0500

    :lipstick: Correct input date

--- src/components/Atoms/Inputs/InputText/InputText.tsx
+++ src/components/Atoms/Inputs/InputText/InputText.tsx
@@ -3,6 +3,7 @@ import React from "react";
 type InputTextProps = {
     placeholder: string;
     type?: string;
+    size?: string;
 };
 
 const InputText = ({ placeholder, type }: InputTextProps) => {
@@ -10,7 +11,7 @@ const InputText = ({ placeholder, type }: InputTextProps) => {
         <input
             type={type ? type : "text"}
             className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md w-full 
-                py-4 px-4 pr-10 max-xl:w-full focus:outline-none focus:ring-2 focus:ring-brand-600 focus:border-transparent"
+                py-3 px-4 pr-10 max-xl:w-full focus:outline-none focus:ring-2 focus:ring-brand-600 focus:border-transparent"
             placeholder={placeholder}
         />
     );
