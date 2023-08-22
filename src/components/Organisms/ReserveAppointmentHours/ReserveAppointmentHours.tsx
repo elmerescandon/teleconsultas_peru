8006697fac6b88d742bd14abed113043b9adfef9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-21 19:28:26 -0500

    :sparkles: Create reserve hours section

--- src/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours.tsx
+++ src/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours.tsx
@@ -1,7 +1,26 @@
+import SlotAppointment from "@/components/Atoms/SlotAppointment/SlotAppointment";
+import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
 import React from "react";
 
-const ReserveAppointmentHours = () => {
-    return <div>ReserveAppointmentHours</div>;
+type ReserveAppointmentHoursProps = {
+    availableAppointments: IAvailableAppointment[];
+};
+
+const ReserveAppointmentHours = ({
+    availableAppointments,
+}: ReserveAppointmentHoursProps) => {
+    return (
+        <div className="w-1/3 px-5">
+            <div className="text-xl font-semibold py-4">
+                Horarios Disponibles
+            </div>
+            <div className="flex flex-wrap gap-5 justify-around">
+                {availableAppointments.map((appointment) => (
+                    <SlotAppointment availableAppointment={appointment} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default ReserveAppointmentHours;
