73056f1aad09462d3e5069ca0eb16f4eab3ea4b2
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-21 22:55:14 -0500

    :hammer :Create context

---
+++ src/utils/context/AppointmentContext/AppointmentContext.tsx
@@ -0,0 +1,66 @@
+import { Dispatch, createContext, useContext, useReducer } from "react";
+import IAppointment from "../../Interfaces/reducers/IAppointment";
+import IAppointmentActions from "../../Interfaces/reducers/IAppointmentActions";
+
+export const AppointmentContext = createContext<IAppointment>(undefined!);
+export const AppointmentDispatchContext = createContext<
+    Dispatch<IAppointmentActions>
+>(undefined!);
+
+type AppointmentProviderProps = {
+    children: React.ReactNode;
+};
+
+const AppointmentProvider = ({ children }: AppointmentProviderProps) => {
+    const [appointment, dispatch] = useReducer(appointmentReducer, {
+        specialityId: "",
+        doctorId: "",
+        reason: "",
+        symptoms: [],
+        details: "",
+        startDate: "",
+        endDate: "",
+    });
+
+    return (
+        <AppointmentContext.Provider value={appointment}>
+            <AppointmentDispatchContext.Provider value={dispatch}>
+                {children}
+            </AppointmentDispatchContext.Provider>
+        </AppointmentContext.Provider>
+    );
+};
+
+const appointmentReducer = (
+    appointment: IAppointment,
+    action: IAppointmentActions
+) => {
+    switch (action.type) {
+        case "SET_SPECIALITY":
+            return {
+                ...appointment,
+                specialityId: action.payload,
+                doctorId: "",
+            };
+        case "SET_DOCTOR":
+            return { ...appointment, doctorId: action.payload };
+        case "SET_REASON":
+            return { ...appointment, reason: action.payload };
+        case "SET_SYMPTOMS":
+            return { ...appointment, symptoms: action.payload };
+        case "SET_DETAILS":
+            return { ...appointment, details: action.payload };
+        default:
+            return appointment;
+    }
+};
+
+export const useAppointment = () => {
+    return useContext(AppointmentContext);
+};
+
+export const useAppointmentDispatch = () => {
+    return useContext(AppointmentDispatchContext);
+};
+
+export default AppointmentProvider;
