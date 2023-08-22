157a50196cde73d8cd0293a1be399d54e67d4812
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-21 19:32:35 -0500

    :lipstick: Add usability to select

--- src/components/Atoms/Inputs/InputSelect/InputSelect.tsx
+++ src/components/Atoms/Inputs/InputSelect/InputSelect.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ISelectOptions from "@/utils/Interfaces/ISelectOptions";
 import React from "react";
 
@@ -5,20 +6,31 @@ type InputSelectProps = {
     selectId: string;
     options: ISelectOptions[];
     placeholder: string;
+    onChange: (selectedValue: string) => void;
 };
 
-const InputSelect = ({ options, selectId, placeholder }: InputSelectProps) => {
+const InputSelect = ({
+    options,
+    selectId,
+    placeholder,
+    onChange,
+}: InputSelectProps) => {
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedValue = event.target.value;
+        onChange(selectedValue);
+    };
     return (
         <div>
             <select
                 id={selectId}
                 className="text-neutral-400 focus:text-classic-black border-neutral-300 border-2 rounded-md h-12 px-4 pr-10 max-xl:w-full w-full"
                 placeholder={placeholder}
+                onChange={handleChange}
             >
                 <option defaultValue={""}>{placeholder}</option>
-                {options.map((option) => {
+                {options.map((option, index) => {
                     return (
-                        <option key={option.value} value={option.value}>
+                        <option key={index} value={option.value}>
                             {option.label}
                         </option>
                     );
