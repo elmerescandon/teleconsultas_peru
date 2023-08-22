acb95353b7f54ea8186bfe57d796154cfa39d178
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-21 22:55:47 -0500

    :sparkles: Eliminate all context creation

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -1,38 +1,17 @@
-import React, { createContext } from "react";
+"use client";
+import React from "react";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import ReserveAppointmentSection from "@/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection";
-
-export const AppointmentContext = createContext({
-    appointmentInfo: {
-        specialityId: "",
-        doctorId: "",
-        reason: "",
-        symptoms: "",
-        details: "",
-        date: null,
-        hour: null,
-    },
-    setAppointmentInfo: () => {},
-});
-
-const initialAppointmentInfo = {
-    specialityId: "",
-    doctorId: "",
-    reason: "",
-    symptoms: "",
-    details: "",
-    date: null,
-    hour: null,
-};
+import AppointmentProvider from "@/utils/context/AppointmentContext/AppointmentContext";
 
 const page = () => {
     return (
         <div>
             <Header />
-            <AppointmentContext.Provider value={{initialAppointmentInfo}>
+            <AppointmentProvider>
                 <ReserveAppointmentSection />
-            </AppointmentContext.Provider>
+            </AppointmentProvider>
             <Footer />
         </div>
     );
