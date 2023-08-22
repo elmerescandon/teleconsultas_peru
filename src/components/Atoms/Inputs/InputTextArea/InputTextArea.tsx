8ec28de9048e29bd78f2873ad6dd098d76f7b9f7
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-21 19:32:01 -0500

    :lipstick: Correct text area

--- src/components/Atoms/Inputs/InputTextArea/InputTextArea.tsx
+++ src/components/Atoms/Inputs/InputTextArea/InputTextArea.tsx
@@ -3,15 +3,17 @@ import React from "react";
 type InputTextAreaProps = {
     rows: number;
     cols: number;
+    placeholder: string;
 };
 
-const InputTextArea = ({ rows, cols }: InputTextAreaProps) => {
+const InputTextArea = ({ rows, cols, placeholder }: InputTextAreaProps) => {
     return (
         <div className="flex">
             <textarea
-                className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md px-4 resize-none max-xl:w-full"
+                className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md px-4 py-4 resize-none max-xl:w-full"
                 rows={rows}
                 cols={cols}
+                placeholder={placeholder}
             />
         </div>
     );
