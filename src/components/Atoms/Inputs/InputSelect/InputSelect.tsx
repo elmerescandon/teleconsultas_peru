be95aa792c11540f68de4c1d4d380b088d54f46b
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-23 22:48:05 -0500

    :sparkles: Add interaction when change

--- src/components/Atoms/Inputs/InputSelect/InputSelect.tsx
+++ src/components/Atoms/Inputs/InputSelect/InputSelect.tsx
@@ -1,6 +1,6 @@
 "use client";
 import ISelectOptions from "@/utils/Interfaces/ISelectOptions";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 type InputSelectProps = {
     selectId: string;
@@ -15,28 +15,33 @@ const InputSelect = ({
     placeholder,
     onChange,
 }: InputSelectProps) => {
+    const [selectedValue, setSelectedValue] = useState<string>("");
+
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedValue = event.target.value;
-        onChange(selectedValue);
+        const newValue = event.target.value;
+        setSelectedValue(newValue);
+        onChange(newValue);
     };
+    useEffect(() => {
+        if (selectedValue === "" && options.length > 0) {
+            setSelectedValue(""); // Set the first option as selected initially
+        }
+    }, [options, selectedValue]);
+
     return (
         <div>
             <select
                 id={selectId}
                 className="text-neutral-400 focus:text-classic-black border-neutral-300 border-2 rounded-md h-12 px-4 pr-10 max-xl:w-full w-full"
-                placeholder={placeholder}
                 onChange={handleChange}
+                value={selectedValue}
             >
-                <option defaultValue={""} value="">
-                    {placeholder}
-                </option>
-                {options.map((option, index) => {
-                    return (
-                        <option key={index} value={option.value}>
-                            {option.label}
-                        </option>
-                    );
-                })}
+                <option value="">{placeholder}</option>
+                {options.map((option, index) => (
+                    <option key={index} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
             </select>
         </div>
     );
