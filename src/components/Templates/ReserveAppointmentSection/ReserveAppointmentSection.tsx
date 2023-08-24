f937a192e606d095352fca306fe8a1813188d822
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-23 22:49:29 -0500

    :hammer: Implement context details

--- src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
+++ src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
@@ -2,31 +2,29 @@ import ReserveAppointmentCalendar from "@/components/Organisms/ReserveAppointmen
 import ReserveAppointmentForms from "@/components/Organisms/ReserveAppointmentForms/ReserveAppointmentForms";
 import ReserveAppointmentHours from "@/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
-import React from "react";
+import { useAppointment } from "@/utils/context/AppointmentContext/AppointmentContext";
+import { getAvailableAppointments } from "@/utils/functions/utils";
+import React, { useEffect, useState } from "react";
 
 const ReserveAppointmentSection = () => {
-    const availableAppointments: IAvailableAppointment[] = [
-        {
-            startDate: new Date(2023, 7, 20, 10, 0),
-            endDate: new Date(2023, 7, 20, 10, 30),
-            available: true,
-        },
-        {
-            startDate: new Date(2023, 7, 20, 11, 0),
-            endDate: new Date(2023, 7, 20, 11, 30),
-            available: false,
-        },
-        {
-            startDate: new Date(2023, 7, 20, 10, 30),
-            endDate: new Date(2023, 7, 20, 11, 0),
-            available: true,
-        },
-        {
-            startDate: new Date(2023, 7, 20, 12, 0),
-            endDate: new Date(2023, 7, 20, 12, 30),
-            available: true,
-        },
-    ];
+    const [availableAppointments, setAvailableAppointments] = useState<
+        IAvailableAppointment[]
+    >([]);
+    const appointment = useAppointment();
+    const { date, doctorId, specialityId } = appointment;
+
+    useEffect(() => {
+        console.log(appointment);
+        if (date !== "") {
+            //  This getAvailable is a mock function that returns the available appointments for the date
+            const available = getAvailableAppointments(
+                date,
+                doctorId,
+                specialityId
+            );
+            setAvailableAppointments(available);
+        }
+    }, [appointment]);
 
     return (
         <div className="px-48 flex py-10 max-xl:flex-col max-xl:px-10 max-xl:items-center">
