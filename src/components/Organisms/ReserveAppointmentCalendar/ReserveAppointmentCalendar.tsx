d332acb34cd3e297f11f8128dd3f34543b23b567
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-23 22:48:32 -0500

    :sparkles: Add date to calendar

--- src/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar.tsx
+++ src/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar.tsx
@@ -1,18 +1,28 @@
 "use client";
+import { useAppointmentDispatch } from "@/utils/context/AppointmentContext/AppointmentContext";
 import { DateCalendar } from "@mui/x-date-pickers";
 import React, { useState } from "react";
 
-const ReserveAppointmentCalendar = () => {
-    const [value, setValue] = useState(null);
+const ReserveAppointmentCalendar = ({}) => {
+    const [dateJS, setDateJS] = useState<IDateJS | null>(null);
+
+    const onChangeDate = (date: IDateJS | null) => {
+        if (!date) return;
+        let dateTemp = new Date(date);
+        setDateJS(date);
+        dispatch({
+            type: "SET_DATE",
+            payload: dateTemp.toISOString().split("T")[0],
+        });
+    };
+
+    const dispatch = useAppointmentDispatch();
     return (
         <div className="px-5">
             <div className="text-xl font-semibold py-4">
                 Calendario Disponible
             </div>
-            <DateCalendar
-                value={value}
-                onChange={(newValue) => setValue(newValue)}
-            />
+            <DateCalendar value={dateJS} onChange={onChangeDate} />
         </div>
     );
 };
