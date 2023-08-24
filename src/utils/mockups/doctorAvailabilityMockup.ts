394a4fec5c469a9660c2c72c251ace8560c4e350
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-23 22:50:21 -0500

    :bug: Correct hours in dates

--- src/utils/mockups/doctorAvailabilityMockup.ts
+++ src/utils/mockups/doctorAvailabilityMockup.ts
@@ -1,6 +1,6 @@
 import IDoctorAvailability from "../Interfaces/dataModel/IDoctorAvailability";
 
-const doctorAvailabilityMockup: IDoctorAvailability = {
+const doctorAvailabilityMockup: IDoctorAvailability[] = [{
     _id: "availability456",
     doctor_id: "doctor123",
     specialty_id: "specialty789",
@@ -8,19 +8,20 @@ const doctorAvailabilityMockup: IDoctorAvailability = {
       {
         date: "2023-08-21",
         slots: [
-          { start_time: "09:00 AM", end_time: "09:30 AM", available: true },
-          { start_time: "09:30 AM", end_time: "10:00 AM", available: true }
+          { startDate: "2023-08-21T10:00:00.000", endDate: "2023-08-21T10:30:00.000", available: false },
+          { startDate: "2023-08-21T11:00:00.000", endDate: "2023-08-21T11:30:00.000", available: true }
         ]
       },
       {
         date: "2023-08-22",
         slots: [
-          { start_time: "09:00 AM", end_time: "09:30 AM", available: true },
-          { start_time: "09:30 AM", end_time: "10:00 AM", available: false }
+          { startDate: "2023-08-22T12:00:00.000", endDate: "2023-08-22T12:30:00.000", available: true },
+          { startDate: "2023-08-22T13:00:00.000", endDate: "2023-08-22T13:30:00.000", available: false }
         ]
       }
     ],
     other_fields: {}
-  };
+  }, 
+];
 
 export default doctorAvailabilityMockup;
