9a57745d72364fb571131254859d35128f3ced40
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-25 01:09:11 -0500

    :sparkles: Add functionality to the payment card

--- src/components/Molecules/PaymentCard/PaymentCard.tsx
+++ src/components/Molecules/PaymentCard/PaymentCard.tsx
@@ -3,11 +3,29 @@ import React from "react";
 type PaymentCardProps = {
     cardType: string;
     lastDigits: string;
+    index: number;
+    indexSelected: number;
+    onClickPayment: (index: number) => void;
 };
 
-const PaymentCard = ({ cardType, lastDigits }: PaymentCardProps) => {
+const PaymentCard = ({
+    cardType,
+    lastDigits,
+    index,
+    indexSelected,
+    onClickPayment,
+}: PaymentCardProps) => {
     return (
-        <div className="flex items-center justify-around bg-brand-50 py-3 rounded-2xl">
+        <button
+            className={`flex items-center justify-around bg-brand-50 py-3 rounded-2xl border-2 ${
+                index === indexSelected
+                    ? "border-basic-black"
+                    : "border-transparent"
+            } `}
+            onClick={() => {
+                onClickPayment(index);
+            }}
+        >
             <BanknotesIcon className="w-10 h-10 text-basic-black" />
             <div className="text-xl font-normal text-basic-black">
                 {cardType}
@@ -15,7 +33,7 @@ const PaymentCard = ({ cardType, lastDigits }: PaymentCardProps) => {
             <div className="text-xl font-normal text-basic-black">
                 **** {lastDigits}
             </div>
-        </div>
+        </button>
     );
 };
 
