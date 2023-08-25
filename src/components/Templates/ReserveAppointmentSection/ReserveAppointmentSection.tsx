3b70005bdad5e0477a72d47386e2bb1c39daa9cb
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-24 20:39:03 -0500

    :sparkles: Validate button reserve

--- src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
+++ src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
@@ -1,9 +1,15 @@
+import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
 import ReserveAppointmentCalendar from "@/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar";
 import ReserveAppointmentForms from "@/components/Organisms/ReserveAppointmentForms/ReserveAppointmentForms";
 import ReserveAppointmentHours from "@/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours";
+import ReserveSummary from "@/components/Organisms/ReserveSummary/ReserveSummary";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 import { useAppointment } from "@/utils/context/AppointmentContext/AppointmentContext";
-import { getAvailableAppointments } from "@/utils/functions/utils";
+import {
+    getAvailableAppointments,
+    validateAppointment,
+} from "@/utils/functions/utils";
 import React, { useEffect, useState } from "react";
 
 const ReserveAppointmentSection = () => {
@@ -12,6 +18,14 @@ const ReserveAppointmentSection = () => {
     >([]);
     const appointment = useAppointment();
     const { date, doctorId, specialityId } = appointment;
+
+    const onClickReserve = (appointment: IAppointment) => {
+        if (validateAppointment(appointment)) {
+            console.log("Appointment is valid");
+        } else {
+            console.log("Appointment is not valid");
+        }
+    };
 
     useEffect(() => {
         console.log(appointment);
@@ -27,12 +41,28 @@ const ReserveAppointmentSection = () => {
     }, [appointment]);
 
     return (
-        <div className="px-48 flex py-10 max-xl:flex-col max-xl:px-10 max-xl:items-center">
-            <ReserveAppointmentForms />
-            <ReserveAppointmentCalendar />
-            <ReserveAppointmentHours
-                availableAppointments={availableAppointments}
-            />
+        <div className="px-48  py-10  max-xl:px-10 ">
+            <div className="flex max-xl:flex-col max-xl:items-center justify-around">
+                <ReserveAppointmentForms />
+                <div>
+                    <ReserveAppointmentCalendar />
+                    <ReserveAppointmentHours
+                        availableAppointments={availableAppointments}
+                    />
+                </div>
+                <ReserveSummary />
+            </div>
+            <div className="max-xl:pt-10 flex justify-end w-full max-xl:justify-center">
+                <div className="max-xl:w-1/2">
+                    <ButtonPrimary
+                        onClickFn={() => {
+                            onClickReserve(appointment);
+                        }}
+                    >
+                        Agendar
+                    </ButtonPrimary>
+                </div>
+            </div>
         </div>
     );
 };
