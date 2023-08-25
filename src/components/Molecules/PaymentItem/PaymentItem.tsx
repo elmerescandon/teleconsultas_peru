2b41dce4165d575a14ca2332c8aef7dfd4cc191f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-25 00:49:04 -0500

    :sparkles: Item de detalle

---
+++ src/components/Molecules/PaymentItem/PaymentItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type PaymentItemProps = {
+    label: string;
+    name: string;
+};
+
+const PaymentItem = ({ label, name }: PaymentItemProps) => {
+    return (
+        <div className="flex justify-between">
+            <p className="text-neutral-400 text-lg">{`${label}:`}</p>
+            <p className="text-basic-black text-lg">{name}</p>
+        </div>
+    );
+};
+
+export default PaymentItem;
