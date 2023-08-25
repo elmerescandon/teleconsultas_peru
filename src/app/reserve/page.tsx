2db3699d082eea7c742891ce2d2cd85a7adf7b02
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-24 21:53:17 -0500

    :sparkles: Remove reserve appointment

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -3,15 +3,12 @@ import React from "react";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import ReserveAppointmentSection from "@/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection";
-import AppointmentProvider from "@/utils/context/AppointmentContext/AppointmentContext";
 
 const page = () => {
     return (
         <div>
             <Header />
-            <AppointmentProvider>
-                <ReserveAppointmentSection />
-            </AppointmentProvider>
+            <ReserveAppointmentSection />
             <Footer />
         </div>
     );
