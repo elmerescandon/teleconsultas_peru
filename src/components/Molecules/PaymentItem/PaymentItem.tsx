8c3ab3e5bc4c2542a946cdc2c5c7331a04e70c9e
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-26 11:25:01 -0500

    :sparkls: Create page item

--- src/components/Molecules/PaymentItem/PaymentItem.tsx
+++ src/components/Molecules/PaymentItem/PaymentItem.tsx
@@ -7,7 +7,7 @@ type PaymentItemProps = {
 
 const PaymentItem = ({ label, name }: PaymentItemProps) => {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between py-2">
             <p className="text-neutral-400 text-lg">{`${label}:`}</p>
             <p className="text-basic-black text-lg">{name}</p>
         </div>
