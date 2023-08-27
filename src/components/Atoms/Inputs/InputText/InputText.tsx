c4725915e33434e2b7b2366e400520878ecafb1d
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-27 10:58:04 -0500

    :sparkles: Include input text functionality

--- src/components/Atoms/Inputs/InputText/InputText.tsx
+++ src/components/Atoms/Inputs/InputText/InputText.tsx
@@ -4,15 +4,24 @@ type InputTextProps = {
     placeholder: string;
     type?: string;
     size?: string;
+    onChangeFn: (newValue: string) => void;
+    value: string;
 };
 
-const InputText = ({ placeholder, type }: InputTextProps) => {
+const InputText = ({
+    placeholder,
+    type,
+    onChangeFn,
+    value,
+}: InputTextProps) => {
     return (
         <input
             type={type ? type : "text"}
             className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md w-full 
                 py-3 px-4 pr-10 max-xl:w-full focus:outline-none focus:ring-2 focus:ring-brand-600 focus:border-transparent"
             placeholder={placeholder}
+            value={value}
+            onChange={(e) => onChangeFn(e.target.value)}
         />
     );
 };
