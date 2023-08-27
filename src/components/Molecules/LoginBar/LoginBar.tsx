e7e720bf4a47b254e2dd5f9d61b93f428a4fdcdc
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-27 12:11:46 -0500

    :sparkles: Add login bar functionality

--- src/components/Molecules/LoginBar/LoginBar.tsx
+++ src/components/Molecules/LoginBar/LoginBar.tsx
@@ -1,14 +1,53 @@
+"use client";
+import ButtonSecondary from "@/components/Atoms/Buttons/ButtonSecondary/ButtonSecondary";
 import LinkPrimary from "@/components/Atoms/Links/LinkPrimary/LinkPrimary";
+import { userLogOut } from "@/redux/action-creators/UserActionCreators";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import Routes from "@/utils/routes/Routes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const LoginBar = () => {
-  return (
-    <div className="login-bar flex flex-row items-center gap-7">
-      <LinkPrimary to={Routes.LOGIN}>{"Iniciar Sesión"}</LinkPrimary>
-      <LinkPrimary to={Routes.REGISTER}>{"Regístrate"}</LinkPrimary>
-    </div>
-  );
+    const state = useAppSelector((state) => state.user);
+    const { logged } = state;
+    const router = useRouter();
+    const dispatch = useAppDispatch();
+    return (
+        <div>
+            {logged ? (
+                <div className="login-bar flex flex-row items-center gap-7">
+                    <button
+                        className="text-lg font-semibold"
+                        onClick={() => {
+                            router.push(Routes.PATIENT_PROFILE);
+                        }}
+                    >
+                        <img
+                            className="w-14 h-14 rounded-full"
+                            src="/girl_smiling_v2.jpg"
+                        />
+                    </button>
+                    <ButtonSecondary
+                        onClickFn={() => {
+                            dispatch(userLogOut());
+                            router.push(Routes.HOME);
+                        }}
+                    >
+                        Cerrar Sesión
+                    </ButtonSecondary>
+                </div>
+            ) : (
+                <div className="login-bar flex flex-row items-center gap-7">
+                    <LinkPrimary to={Routes.LOGIN}>
+                        {"Iniciar Sesión"}
+                    </LinkPrimary>
+                    <LinkPrimary to={Routes.REGISTER}>
+                        {"Regístrate"}
+                    </LinkPrimary>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default LoginBar;
