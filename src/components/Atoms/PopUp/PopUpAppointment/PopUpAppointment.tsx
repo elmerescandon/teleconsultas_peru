3f12ae569d5b93e467448e155cdd93743a23721f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-27 12:18:37 -0500

    :sparkles: Update dating

--- src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
+++ src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
@@ -10,20 +10,16 @@ const PopUpAppointment = ({ onClose, appointment }: PopUpAppointmentProps) => (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
         <div className="bg-white p-4 rounded">
             <h2 className="text-3xl font-semibold mb-2">
-                {appointment.specialty}
+                {appointment.specialityId}
             </h2>
-            <p>
-                Doctor:{" "}
-                {`${appointment.doctorLastName} ${appointment.doctorName}`}
-            </p>
-            <p>
-                Patient:{" "}
-                {`${appointment.patientLastName} ${appointment.patientName}`}
-            </p>
-            <p>Date: {`${appointment.startDate.toDateString()}`}</p>
+            <p>Doctor: {`${appointment.doctorId}`}</p>
+            <p>Razón: {`${appointment.reason}`}</p>
+            <p>Date: {`${new Date(appointment.startDate).toDateString()}`}</p>
             <p>
                 Schedule:{" "}
-                {`${appointment.startDate.getHours()} - ${appointment.endDate.getHours()}`}
+                {`${new Date(appointment.startDate).getHours()} - ${new Date(
+                    appointment.endDate
+                ).getHours()}`}
             </p>
 
             <button className="text-blue-500 hover:underline" onClick={onClose}>
