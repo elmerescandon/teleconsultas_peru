352856c44bce6fbffe0fb89127b0b1b87f936607
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-27 12:32:28 -0500

    :sparkles: Set appointmnent

--- src/utils/mockups/AppointmentsTest.ts
+++ src/utils/mockups/AppointmentsTest.ts
@@ -2,36 +2,15 @@ import IAppointment from "../Interfaces/reducers/IAppointment";
 
 const AppointmentTest : IAppointment[] = [
     {
-        id: 1,
-        specialty: "Cardiología",
-        startDate: new Date(2023, 7, 10, 10), // 2021-03-03 10:00:00
-        endDate: new Date(2023, 7, 10, 11), // 2021-03-03 10:00:00
-        doctorName: "Juan",
-        doctorLastName: "Perez",
-        patientName: "Pedro",
-        patientLastName: "Gomez"
-    },
-    // Create new IAppointment objects here
-    {
-        id: 2,
-        specialty: "Medicina General",
-        startDate: new Date(2023, 7, 16, 19), // 2021-03-03 10:00:00
-        endDate: new Date(2023, 7, 16, 11), // 2021-03-03 10:00:00
-        doctorName: "Maria",
-        doctorLastName: "Cortes",
-        patientName: "Pedro",
-        patientLastName: "Gomez"
-    },
-    {
-        id: 3,
-        specialty: "Otorrinolaringología",
-        startDate: new Date(2023, 7, 25, 14), // 2021-03-03 10:00:00
-        endDate: new Date(2023, 7, 25, 15), // 2021-03-03 10:00:00
-        doctorName: "Pedro",
-        doctorLastName: "Fernandez",
-        patientName: "Pedro",
-        patientLastName: "Gomez"
-    },     
+        specialityId: "Cardiología",
+        startDate: "2023-08-21T10:00:00.000", // 2021-03-03 10:00:00
+        endDate: "2023-08-21T10:30:00.000", // 2021-03-03 10:00:00
+        doctorId: "Juan",
+        date: "2023-08-21",
+        details: "Dolor de pecho",
+        reason: "Dolor de pecho",
+        symptoms: ["Dolor de pecho", "Dolor de cabeza"],
+    },    
 ]
 
 export default AppointmentTest;
