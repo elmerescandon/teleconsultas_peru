3534ba3008e32a13e34f3d769a5a1d7a045cc17c
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-01 22:49:55 -0500

    :sparkles: Add interface register

---
+++ src/utils/Interfaces/reducers/IRegister.ts
@@ -0,0 +1,24 @@
+interface IRegister{
+    // general
+    name: string,
+    lastname: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
+    id: string,
+
+    // location
+    region: string,
+    province: string,
+    district: string,
+    address: string,
+    refference: string,
+    interiorNumber: string,
+
+    // Info
+    age: string,
+    height: string,
+    weight: string,
+    phone: string,
+    bornDate: string,
+}
