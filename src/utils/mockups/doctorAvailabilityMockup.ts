9d93bbd9bdfe79717d5e0ed854474b94f6d07187
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-02 17:05:24 -0500

    :sparkles: Add more availability

--- src/utils/mockups/doctorAvailabilityMockup.ts
+++ src/utils/mockups/doctorAvailabilityMockup.ts
@@ -6,10 +6,13 @@ const doctorAvailabilityMockup: IDoctorAvailability[] = [{
     specialty_id: "specialty789",
     availability_slots: [
       {
-        date: "2023-08-21",
+        date: "2023-08-24",
         slots: [
           { startDate: "2023-08-21T10:00:00.000", endDate: "2023-08-21T10:30:00.000", available: false },
-          { startDate: "2023-08-21T11:00:00.000", endDate: "2023-08-21T11:30:00.000", available: true }
+          { startDate: "2023-08-21T11:00:00.000", endDate: "2023-08-21T11:30:00.000", available: true },
+          { startDate: "2023-08-21T13:00:00.000", endDate: "2023-08-21T13:30:00.000", available: true },
+          { startDate: "2023-08-21T14:00:00.000", endDate: "2023-08-21T14:30:00.000", available: true },
+          { startDate: "2023-08-21T15:00:00.000", endDate: "2023-08-21T15:30:00.000", available: true }
         ]
       },
       {
