5051d46441e676a293e8d95b4c4eafbb53ae4ff8
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-01 22:49:12 -0500

    :sparkles: Add functionality

--- src/components/Organisms/RegisterGeneral/RegisterGeneral.tsx
+++ src/components/Organisms/RegisterGeneral/RegisterGeneral.tsx
@@ -1,35 +1,53 @@
-import Register from "@/app/register/page";
 import InputText from "@/components/Atoms/Inputs/InputText/InputText";
 import RegisterField from "@/components/Molecules/RegisterField/RegisterField";
-import React from "react";
+import React, { useState } from "react";
 import RegisterRow from "../RegisterRow/RegisterRow";
+import useRegister from "@/utils/hooks/useRegister";
 
 const RegisterGeneral = () => {
+    // TODO: Make it responsive
+    // TODO: Add validation functions
+    // TODO: Add error messages
+
+    const { formFields, handleChange } = useRegister();
+
     return (
         <div className="flex flex-row justify-around max-xl:flex-col max-xl:items-center">
             <RegisterRow>
-                <RegisterField title="Nombre">
+                <RegisterField title="Nombre" error={formFields.name.error}>
                     <InputText
-                        onChangeFn={() => {}}
-                        value=""
+                        onChangeFn={(name) => {
+                            handleChange("name", name);
+                        }}
+                        value={formFields.name.value}
                         type="text"
                         placeholder="Ej. Pedro Alberto"
                     />
                 </RegisterField>
 
-                <RegisterField title="Apellido (s)">
+                <RegisterField
+                    title="Apellido (s)"
+                    error={formFields.lastname.error}
+                >
                     <InputText
-                        onChangeFn={() => {}}
-                        value=""
+                        onChangeFn={(lastname) => {
+                            handleChange("lastname", lastname);
+                        }}
+                        value={formFields.lastname.value}
                         type="text"
                         placeholder="Ej. Álvarez Rodriguez "
                     />
                 </RegisterField>
 
-                <RegisterField title="Correo Electrónico">
+                <RegisterField
+                    title="Correo Electrónico"
+                    error={formFields.email.error}
+                >
                     <InputText
-                        onChangeFn={() => {}}
-                        value=""
+                        onChangeFn={(email) => {
+                            handleChange("email", email);
+                        }}
+                        value={formFields.email.value}
                         type="mail"
                         placeholder="Ej. pedro.alva@aikasalud.com"
                     />
@@ -37,30 +55,45 @@ const RegisterGeneral = () => {
             </RegisterRow>
 
             <RegisterRow>
-                <RegisterField title="Documento de Identidad">
+                <RegisterField
+                    title="Documento de Identidad"
+                    error={formFields.id.error}
+                >
                     <InputText
-                        onChangeFn={() => {}}
-                        value=""
+                        onChangeFn={(id) => {
+                            handleChange("id", id);
+                        }}
+                        value={formFields.id.value}
                         type="number"
                         placeholder="Ej. 4589812"
                     />
                 </RegisterField>
 
-                <RegisterField title="Contraseña">
+                <RegisterField
+                    title="Contraseña"
+                    error={formFields.password.error}
+                >
                     <InputText
-                        onChangeFn={() => {}}
-                        value=""
+                        onChangeFn={() => {
+                            handleChange("password", "");
+                        }}
+                        value={formFields.password.value}
                         type="password"
-                        placeholder="Ej. Almenos 8 caractéres, una mayúscula y números"
+                        placeholder="8 caractéres, una mayúscula y números"
                     />
                 </RegisterField>
 
-                <RegisterField title="Validar Contraseña">
+                <RegisterField
+                    title="Validar Contraseña"
+                    error={formFields.repeatPassword.error}
+                >
                     <InputText
-                        onChangeFn={() => {}}
-                        value=""
+                        onChangeFn={(repeat) => {
+                            handleChange("repeatPassword", repeat);
+                        }}
+                        value={formFields.repeatPassword.value}
                         type="password"
-                        placeholder="Ej. pedro.alva@aikasalud.com"
+                        placeholder=""
                     />
                 </RegisterField>
             </RegisterRow>
