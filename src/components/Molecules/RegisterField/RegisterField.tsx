ad45631f19f0d7b723cfd375ace44448226292e0
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-01 22:48:56 -0500

    :sparkles: Add  error message

--- src/components/Molecules/RegisterField/RegisterField.tsx
+++ src/components/Molecules/RegisterField/RegisterField.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 type RegisterFieldProps = {
     title: string;
+    error: string;
     children: React.ReactNode;
 };
 
-const RegisterField = ({ title, children }: RegisterFieldProps) => {
+const RegisterField = ({ title, children, error }: RegisterFieldProps) => {
     return (
         <div>
             <p className="text-lg">{title}</p>
             {children}
+            <p>{error}</p>
         </div>
     );
 };
