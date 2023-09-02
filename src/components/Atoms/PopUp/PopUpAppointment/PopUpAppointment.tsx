20b3a85b3fc6573feae8c4a561aff17ecf1858c9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-02 17:30:30 -0500

    :sparkles: Update values

--- src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
+++ src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
@@ -8,7 +8,7 @@ type PopUpAppointmentProps = {
 
 const PopUpAppointment = ({ onClose, appointment }: PopUpAppointmentProps) => (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-4 rounded">
+        <div className="bg-white p-4 rounded w-[70vh] h-[30vh] flex flex-col items-center">
             <h2 className="text-3xl font-semibold mb-2">
                 {appointment.specialityId}
             </h2>
