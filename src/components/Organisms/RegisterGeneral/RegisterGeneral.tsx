55abda564fb24ced37cb6edea1060f60a3055f17
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-02 11:32:41 -0500

    :sparkles: Implement register general pages

--- src/components/Organisms/RegisterGeneral/RegisterGeneral.tsx
+++ src/components/Organisms/RegisterGeneral/RegisterGeneral.tsx
@@ -1,109 +1,129 @@
 import InputText from "@/components/Atoms/Inputs/InputText/InputText";
 import RegisterField from "@/components/Molecules/RegisterField/RegisterField";
-import React, { useEffect, useState } from "react";
 import RegisterRow from "../RegisterRow/RegisterRow";
 import useRegister from "@/utils/hooks/useRegister";
 import { useRegisterDispatch } from "@/utils/context/RegisterContext/RegisterContext";
+import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
+import { useEffect, useState } from "react";
 
-const RegisterGeneral = () => {
-    // TODO: Make it responsive
-    // TODO: Add validation functions
-    // TODO: Add error messages
+type RegisterGeneralProps = {
+    nextFn: () => void;
+};
 
-    const { formFields, handleChange } = useRegister();
-
-    const dispatch = useRegisterDispatch();
+const RegisterGeneral = ({ nextFn }: RegisterGeneralProps) => {
+    const { formFields, handleChange, handleValidations, handleRegister } =
+        useRegister();
+    const [checkForms, setCheckForms] = useState<boolean>(false);
 
     useEffect(() => {
         console.log(formFields);
+        if (checkForms && handleRegister("general")) {
+            nextFn();
+        } else {
+            setCheckForms(false);
+        }
     }, [formFields]);
 
     return (
-        <div className="flex flex-row justify-around max-xl:flex-col max-xl:items-center">
-            <RegisterRow>
-                <RegisterField title="Nombre" error={formFields.name.error}>
-                    <InputText
-                        onChangeFn={(name) => {
-                            handleChange("name", name);
-                        }}
-                        value={formFields.name.value}
-                        type="text"
-                        placeholder="Ej. Pedro Alberto"
-                    />
-                </RegisterField>
+        <div>
+            <div className="flex flex-row justify-around max-xl:flex-col max-xl:items-center gap-10 max-xl:gap-0 max-xl:px-10">
+                <RegisterRow>
+                    <RegisterField title="Nombre" error={formFields.name.error}>
+                        <InputText
+                            onChangeFn={(name) => {
+                                handleChange("name", name);
+                            }}
+                            value={formFields.name.value}
+                            type="text"
+                            placeholder="Ej. Pedro Alberto"
+                        />
+                    </RegisterField>
 
-                <RegisterField
-                    title="Apellido (s)"
-                    error={formFields.lastname.error}
-                >
-                    <InputText
-                        onChangeFn={(lastname) => {
-                            handleChange("lastname", lastname);
-                        }}
-                        value={formFields.lastname.value}
-                        type="text"
-                        placeholder="Ej. Álvarez Rodriguez "
-                    />
-                </RegisterField>
+                    <RegisterField
+                        title="Apellido (s)"
+                        error={formFields.lastname.error}
+                    >
+                        <InputText
+                            onChangeFn={(lastname) => {
+                                handleChange("lastname", lastname);
+                            }}
+                            value={formFields.lastname.value}
+                            type="text"
+                            placeholder="Ej. Álvarez Rodriguez "
+                        />
+                    </RegisterField>
 
-                <RegisterField
-                    title="Correo Electrónico"
-                    error={formFields.email.error}
-                >
-                    <InputText
-                        onChangeFn={(email) => {
-                            handleChange("email", email);
-                        }}
-                        value={formFields.email.value}
-                        type="mail"
-                        placeholder="Ej. pedro.alva@aikasalud.com"
-                    />
-                </RegisterField>
-            </RegisterRow>
+                    <RegisterField
+                        title="Correo Electrónico"
+                        error={formFields.email.error}
+                    >
+                        <InputText
+                            onChangeFn={(email) => {
+                                handleChange("email", email);
+                            }}
+                            value={formFields.email.value}
+                            type="mail"
+                            placeholder="Ej. pedro.alva@aikasalud.com"
+                        />
+                    </RegisterField>
+                </RegisterRow>
 
-            <RegisterRow>
-                <RegisterField
-                    title="Documento de Identidad"
-                    error={formFields.id.error}
-                >
-                    <InputText
-                        onChangeFn={(id) => {
-                            handleChange("id", id);
-                        }}
-                        value={formFields.id.value}
-                        type="number"
-                        placeholder="Ej. 4589812"
-                    />
-                </RegisterField>
+                <RegisterRow>
+                    <RegisterField
+                        title="Documento de Identidad"
+                        error={formFields.id.error}
+                    >
+                        <InputText
+                            onChangeFn={(id) => {
+                                handleChange("id", id);
+                            }}
+                            value={formFields.id.value}
+                            type="number"
+                            placeholder="Ej. 4589812"
+                        />
+                    </RegisterField>
 
-                <RegisterField
-                    title="Contraseña"
-                    error={formFields.password.error}
-                >
-                    <InputText
-                        onChangeFn={(pass) => {
-                            handleChange("password", pass);
-                        }}
-                        value={formFields.password.value}
-                        type="password"
-                        placeholder="8 caractéres, una mayúscula y números"
-                    />
-                </RegisterField>
+                    <RegisterField
+                        title="Contraseña"
+                        error={formFields.password.error}
+                    >
+                        <InputText
+                            onChangeFn={(pass) => {
+                                handleChange("password", pass);
+                            }}
+                            value={formFields.password.value}
+                            type="password"
+                            placeholder="8 caractéres, una mayúscula y números"
+                        />
+                    </RegisterField>
 
-                <RegisterField
-                    title="Validar Contraseña"
-                    error={formFields.repeatPassword.error}
+                    <RegisterField
+                        title="Validar Contraseña"
+                        error={formFields.repeatPassword.error}
+                    >
+                        <InputText
+                            onChangeFn={(repeat) => {
+                                handleChange("repeatPassword", repeat);
+                            }}
+                            value={formFields.repeatPassword.value}
+                            type="password"
+                            placeholder=""
+                        />
+                    </RegisterField>
+                </RegisterRow>
+            </div>
+
+            <div className="flex py-5 max-xl:px-10">
+                <ButtonPrimary
+                    type="button"
+                    onClickFn={() => {
+                        handleValidations("general");
+                        setCheckForms(true);
+                    }}
                 >
-                    <InputText
-                        onChangeFn={(repeat) => {
-                            handleChange("repeatPassword", repeat);
-                        }}
-                        value={formFields.repeatPassword.value}
-                        type="password"
-                        placeholder=""
-                    />
-                </RegisterField>
-            </RegisterRow>
+                    Siguiente
+                </ButtonPrimary>
+            </div>
         </div>
     );
 };
