470c5e34a0dcc80c615065be21ad0585f6d87b6d
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-03 18:36:12 -0500

    :sparkles: Include new values

--- src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
+++ src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
@@ -45,10 +45,10 @@ const ReserveAppointmentSection = () => {
     }, [appointment]);
 
     return (
-        <div className="px-48  py-10  max-xl:px-10 h-[90vh] max-xl:h-full">
-            <div className="flex max-xl:flex-col max-xl:items-center justify-around">
+        <div className="px-48  py-10  max-xl:px-20 h-[90vh] max-xl:h-full max-md:px-5">
+            <div className="flex max-xl:flex-col max-xl:items-center max-xl:justify-around ">
                 <ReserveAppointmentForms />
-                <div>
+                <div className="w-1/3 max-xl:w-full max-xl:flex max-md:flex-col max-xl:justify-around">
                     <ReserveAppointmentCalendar />
                     <ReserveAppointmentHours
                         availableAppointments={availableAppointments}
