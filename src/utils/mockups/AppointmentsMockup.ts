d33331b435011dce0a1f96d117ab11815a99c359
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-03 22:35:45 -0500

    :sparkles: Add more appointments to check on them

--- src/utils/mockups/AppointmentsMockup.ts
+++ src/utils/mockups/AppointmentsMockup.ts
@@ -40,6 +40,19 @@ const AppointmentsMockup : IAppointment[] = [
         symptoms: ["Dolor de pecho", "Dolor de cabeza"],
         status: "scheduled",
     },  
+    {
+        _id: "appointment4",
+        patientId: "patient1",
+        specialityId: "speciality2",
+        startDate: "2023-08-25T10:00:00.000",
+        endDate: "2023-08-25T10:30:00.000", 
+        doctorId: "doctor3",
+        date: "2023-08-24",
+        details: "Dolor de pecho",
+        reason: "Dolor de pecho",
+        symptoms: ["Dolor de pecho", "Dolor de cabeza"],
+        status: "scheduled",
+    },  
 ]
 
 export default AppointmentsMockup;
