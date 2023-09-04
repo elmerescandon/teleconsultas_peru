b23a696d233a9261ae4e9aba3d371e7867a820eb
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-03 22:15:44 -0500

    :sparkles: Add organized information

--- src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
+++ src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
@@ -1,6 +1,16 @@
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 import React from "react";
 import ButtonPrimary from "../../Buttons/ButtonPrimary/ButtonPrimary";
+import LabelInformation from "../../Labels/LabelInformation/LabelInformation";
+import {
+    getAppointmentHours,
+    getDoctorName,
+    getSpecialityName,
+    statusToSpanish,
+    stringToDate,
+} from "@/utils/functions/utils";
+import doctorsMockup from "@/utils/mockups/doctorsMockup";
+import specialitiesMockup from "@/utils/mockups/specialitiesMockup";
 
 type PopUpAppointmentProps = {
     onClose: () => void;
@@ -9,20 +19,46 @@ type PopUpAppointmentProps = {
 
 const PopUpAppointment = ({ onClose, appointment }: PopUpAppointmentProps) => (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-4 rounded w-[70vh] h-[30vh] flex flex-col items-center">
+        <div className="bg-white p-28 rounded-3xl flex flex-col gap-5">
             <h2 className="text-3xl font-semibold mb-2">
-                {appointment.specialityId}
+                {getSpecialityName(
+                    specialitiesMockup,
+                    appointment.specialityId
+                )}
             </h2>
-            <p>Doctor: {`${appointment.doctorId}`}</p>
-            <p>Razón: {`${appointment.reason}`}</p>
-            <p>Date: {`${new Date(appointment.startDate).toDateString()}`}</p>
-            <p>
-                Schedule:{" "}
-                {`${new Date(appointment.startDate).getHours()} - ${new Date(
-                    appointment.endDate
-                ).getHours()}`}
-            </p>
-            <ButtonPrimary onClickFn={onClose}>Cerrar</ButtonPrimary>
+            <div className="flex flex-col gap-2">
+                <LabelInformation
+                    label="Doctor"
+                    value={`${getDoctorName(
+                        doctorsMockup,
+                        appointment.doctorId
+                    )}`}
+                />
+
+                <LabelInformation
+                    label="Fecha"
+                    value={stringToDate(appointment.date)}
+                />
+                <LabelInformation
+                    label="Estado"
+                    value={`${statusToSpanish(appointment.status)}`}
+                />
+                <LabelInformation
+                    label="Horario"
+                    value={getAppointmentHours(
+                        appointment.startDate,
+                        appointment.endDate
+                    )}
+                />
+                <LabelInformation
+                    label="Razón"
+                    value={`${appointment.reason}`}
+                />
+            </div>
+
+            <div>
+                <ButtonPrimary onClickFn={onClose}>Cerrar</ButtonPrimary>
+            </div>
         </div>
     </div>
 );
