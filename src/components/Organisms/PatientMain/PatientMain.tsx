7578a32919ed7e224b2fc151ada19c7a300b3ade
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-03 19:54:41 -0500

    :sparkles: Add time display in section

--- src/components/Organisms/PatientMain/PatientMain.tsx
+++ src/components/Organisms/PatientMain/PatientMain.tsx
@@ -1,14 +1,19 @@
+"use client";
+import TimeDisplay from "@/components/Atoms/Time/TimeDisplay";
+import { useAppSelector } from "@/redux/hooks";
+import IUserState from "@/redux/state-interfaces/User/IUserState";
+import { IState } from "@/redux/store";
 import Image from "next/image";
 import React from "react";
 
 const PatientMain = () => {
+    const user: IUserState = useAppSelector((state: IState) => state.user);
+    const { userInfo } = user;
     return (
         <div className="flex justify-between w-full items-center rounded-3xl border-neutral-300 border-2 py-10 px-24 max-xl:px-5 max-xl:flex-col max-xl:gap-5">
             <div className="flex flex-col gap-5">
-                <p className="text-sm font-normal">
-                    Lunes, 01 de Agosto 2023 | 2:23PM
-                </p>
-                <p className="text-3xl font-semibold">¡Buenos días, Alvaro!</p>
+                <TimeDisplay />
+                <p className="text-3xl font-semibold">{`¡Buenos días, ${userInfo.name}!`}</p>
                 <p className="text-base font-normal">
                     Tienes 1 cita agendada para hoy
                 </p>
