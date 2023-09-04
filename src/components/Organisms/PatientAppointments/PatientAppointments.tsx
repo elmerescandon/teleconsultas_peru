e64d21fc506eb18e8875ff8f7c94e010a60c5a42
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-03 22:35:05 -0500

    :sparkles: Add pagination items

--- src/components/Organisms/PatientAppointments/PatientAppointments.tsx
+++ src/components/Organisms/PatientAppointments/PatientAppointments.tsx
@@ -5,6 +5,8 @@ import IUserState from "@/redux/state-interfaces/User/IUserState";
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 import AppointmentsMockup from "@/utils/mockups/AppointmentsMockup";
 import React, { useEffect, useState } from "react";
+import Carrousel from "../Carrousel/Carrousel";
+import Pagination from "../Pagination/Pagination";
 
 const PatientAppointments = () => {
     const state: IUserState = useAppSelector((state) => state.user);
@@ -25,15 +27,22 @@ const PatientAppointments = () => {
         // Get appointments from user
         getAppointments();
     }, []);
+
     return (
         <div className="w-2/3 rounded-3xl border-neutral-300 border-2 py-5 px-10 h-96 max-xl:h-full max-xl:w-full">
             <div className="text-xl font-semibold">Citas Agendadas</div>
             <div className="flex flex-col items-center py-5 gap-4">
-                {patientAppointments.map((appointment, index) => {
-                    return (
-                        <PatientDate key={index} appointment={appointment} />
-                    );
-                })}
+                <Pagination
+                    itemsPerPage={3}
+                    items={patientAppointments.map((appointment, index) => {
+                        return (
+                            <PatientDate
+                                key={index}
+                                appointment={appointment}
+                            />
+                        );
+                    })}
+                />
             </div>
         </div>
     );
