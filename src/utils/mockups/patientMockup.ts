fdfe0587af131b87a1319cc1fc9206d5a6d4fa8b
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-03 20:04:21 -0500

    :sparkles: Create patient mockup

---
+++ src/utils/mockups/patientMockup.ts
@@ -0,0 +1,19 @@
+import IUser from "../Interfaces/dataModel/IUser";
+
+const doctorUserMockup: IUser = {
+    _id: "patient1",
+    role: "patient",
+    name: "Pedro Navaja",
+    email: "pedro_navaja@aika.com",
+    password: "hashed_password",
+    phone: "943186710",
+    address: "Av. César Vallejo 350",
+    profile_picture: "https://i.imgur.com/1OJ9Q3x.png",
+    other_fields: {},
+    reserved_appointments: {
+        ["2023-09-3"]: ["appointment1", "appointment2"],
+        ["2023-09-4"]: ["appointment3", "appointment4"],
+    },
+    active_prescriptions: ["prescription1", "prescription2"],
+};
+export default doctorUserMockup;
