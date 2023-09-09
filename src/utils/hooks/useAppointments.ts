4b33cb5dd7f1c70df1534a3f25742649775a4ebb
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-09 16:05:16 -0500

    :hammer: Create custom hook appointment

---
+++ src/utils/hooks/useAppointments.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import IAppointment from "../Interfaces/reducers/IAppointment";
+import IDateRangeAppointment from "../Interfaces/IDateRangeAppointment";
+import AppointmentsMockup from "../mockups/AppointmentsMockup";
+
+const useAppointments = (date : IDateRangeAppointment, setSelectedAppointment: (value: string) => void ) : IAppointment[] => {
+
+
+    const getAppointments = () => {
+        const data = AppointmentsMockup.filter((appointment) => {
+            if (date.init === null || date.finish === null) {
+                return appointment.status === "completed";
+            }
+
+            console.log(new Date(date.init));
+            console.log(new Date(date.finish));
+            console.log(new Date(appointment.date));
+            return (
+                appointment.status === "completed" &&
+                new Date(appointment.date) >= new Date(date.init) &&
+                new Date(appointment.date) <= new Date(date.finish)
+            );
+        });
+        return data;
+    };
+
+    const [appointments, setAppointments] = useState<IAppointment[]>(getAppointments());
+
+
+
+
+
+    useEffect(() => {
+        const newAppointments = getAppointments();
+        setAppointments(newAppointments);
+        setSelectedAppointment("");
+    }, [date]);
+
+    return appointments;
+
+}
+
+export default useAppointments;
