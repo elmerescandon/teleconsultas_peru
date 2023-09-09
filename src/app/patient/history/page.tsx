20cd392c14d42d19f79cbfb8bd9cc7271029e3a5
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-09 16:03:16 -0500

    :sparkles: Add patient history section

--- src/app/patient/history/page.tsx
+++ src/app/patient/history/page.tsx
@@ -1,7 +1,16 @@
+import Footer from "@/components/Organisms/Footer/Footer";
+import Header from "@/components/Organisms/Header/Header";
+import PatientHistorySection from "@/components/Templates/PatientHistorySection/PatientHistorySection";
 import React from "react";
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div>
+            <Header />
+            <PatientHistorySection />
+            <Footer />
+        </div>
+    );
 };
 
 export default page;
