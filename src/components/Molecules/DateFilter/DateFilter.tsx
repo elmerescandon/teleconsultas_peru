95e0552d6b9ba8ba0b597a06055dbf5445eddc42
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-09 16:04:13 -0500

    :sparkles: Update some values in the data filter component

---
+++ src/components/Molecules/DateFilter/DateFilter.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { DatePicker } from "@mui/x-date-pickers";
+
+type DateFilterProps = {
+    date: { init: string | null; finish: string | null };
+    setDate: (date: { init: string | null; finish: string | null }) => void;
+};
+
+const DateFilter = ({ date, setDate }: DateFilterProps) => {
+    return (
+        <div className="flex gap-5 justify-end">
+            <DatePicker
+                maxDate={date.finish}
+                label="Fecha Inicio"
+                value={date.init}
+                onChange={(newDate) => {
+                    setDate({ ...date, init: newDate });
+                }}
+            />
+            <DatePicker
+                minDate={date.init}
+                label="Fecha Final"
+                value={date.finish}
+                onChange={(newDate) => {
+                    setDate({ ...date, finish: newDate });
+                }}
+            />
+        </div>
+    );
+};
+
+export default DateFilter;
