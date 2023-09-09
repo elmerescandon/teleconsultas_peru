e605d67742a4c427956376794f0cd95fc3e95d54
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-09 16:03:31 -0500

    :sparkles: Create button history

---
+++ src/components/Atoms/Buttons/ButtonHistory/ButtonHistory.tsx
@@ -0,0 +1,30 @@
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import { stringToDate } from "@/utils/functions/utils";
+import React from "react";
+
+type ButtonHistoryProps = {
+    appointment: IAppointment;
+    onClickFn: (id: string) => void;
+    idSelect: string;
+};
+
+const ButtonHistory = ({
+    appointment,
+    onClickFn,
+    idSelect,
+}: ButtonHistoryProps) => {
+    return (
+        <button
+            className={`px-2 py-1 rounded-md hover:bg-gray-300 transition duration-300 ease-in-out ${
+                idSelect === appointment._id
+                    ? " bg-brand-600 text-basic-white pb-3"
+                    : "bg-gray-200"
+            }`}
+            onClick={() => {
+                onClickFn(appointment._id);
+            }}
+        >{`Cita ${stringToDate(appointment.date)}`}</button>
+    );
+};
+
+export default ButtonHistory;
