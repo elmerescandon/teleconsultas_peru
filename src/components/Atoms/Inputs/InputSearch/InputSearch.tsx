179f17cad2a52cf10fc40a53075ec13a9c9004df
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-16 14:35:54 -0500

    :sparkles: Initiate value

---
+++ src/components/Atoms/Inputs/InputSearch/InputSearch.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputSearch = () => {
+    return (
+        <div className="h-14 px-4 pr-10 border-b border-neutral-300 w-72">
+            InputSearch
+        </div>
+    );
+};
+
+export default InputSearch;
