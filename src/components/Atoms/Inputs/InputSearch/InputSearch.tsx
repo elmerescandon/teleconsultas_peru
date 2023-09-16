e20bc8f5172b3e6bba6c4aaef18075bd25aa6c63
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-16 16:37:05 -0500

    :sparkles: Implement debouncing in search

--- src/components/Atoms/Inputs/InputSearch/InputSearch.tsx
+++ src/components/Atoms/Inputs/InputSearch/InputSearch.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import React, { useEffect } from "react";
+import { debounce } from "lodash";
 
-const InputSearch = () => {
+type InputSearchProps = {
+    placeholder?: string;
+    onInputChange: (e: string) => void;
+};
+
+const InputSearch = ({ placeholder, onInputChange }: InputSearchProps) => {
+    const [inputValue, setInputValue] = React.useState<string>("");
+    const debounceSearch = debounce((value: string) => {
+        onInputChange(value);
+    }, 300);
+
+    useEffect(() => {
+        return () => {
+            debounceSearch.cancel();
+        };
+    }, []);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+        debounceSearch(e.target.value);
+    };
+
     return (
-        <div className="h-14 px-4 pr-10 border-b border-neutral-300 w-72">
-            InputSearch
+        <div className="flex items-center gap-4">
+            <input
+                className="h-14 pt-4 pr-5 border-b-2 w-60 pb-3 active:b-0 focus:ring-0 focus:border-primary-500 focus:outline-none text-lg"
+                type="text"
+                id="name"
+                name="name"
+                placeholder={placeholder}
+                value={inputValue}
+                onChange={handleInputChange}
+                required
+            />
+            <MagnifyingGlassIcon className="h-6 w-6  text-neutral-400 -mb-5" />
         </div>
     );
 };
