5a00e74a0cbdb48c82a1030a9623f061f368597a
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-17 11:34:38 -0500

    :sparkles: Create items

---
+++ src/components/Molecules/PatientItem/PatientItem.tsx
@@ -0,0 +1,12 @@
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import React from "react";
+
+type PatientItemProps = {
+    appointment: IAppointment;
+};
+
+const PatientItem = ({ appointment }: PatientItemProps) => {
+    return <div></div>;
+};
+
+export default PatientItem;
