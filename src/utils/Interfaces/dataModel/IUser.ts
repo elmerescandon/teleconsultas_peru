50dab5b7bc3453e2b0e8448a9351002ee7fa1c5a
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-17 11:34:17 -0500

    :sparkles: Update user interface pagination

--- src/utils/Interfaces/dataModel/IUser.ts
+++ src/utils/Interfaces/dataModel/IUser.ts
@@ -1,13 +1,31 @@
 interface IUser {
+  // TODO: Add terms and conditions as a required field
     _id: string;
     role: "patient" | "doctor";
     name: string;
     email: string;
     password: string;
-    specialties?: string[];
+    termsAndConditions?: boolean;
     phone: string;
     address: string;
-    profile_picture: string;
+    profile_picture: string; // TODO: Make Optional
+
+    // Patient fields
+    allergies?: string[];
+    medicalHistory?: string[];
+
+    // Doctor fields
+
+    specialties?: string[];
+    studies?: string[];
+    curriculum?: string;
+    cmpNumber?: string;
+    cmpPicture?: string;
+    availableHours?: {
+      date: string,
+      hours: string[]
+    }[];
+
     other_fields?: any;
     reserved_appointments?: {
         date: string, appointments: string[]; 
