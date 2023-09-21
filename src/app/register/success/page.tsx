f37b6c738b0710396796abd8d780294c69132f2a
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-20 23:25:25 -0500

    :sparkles: Success new data into the values

--- src/app/register/success/page.tsx
+++ src/app/register/success/page.tsx
@@ -1,5 +1,7 @@
 import LinkLogo from "@/components/Atoms/Links/LinkLogo/LinkLogo";
+import LinkPrimary from "@/components/Atoms/Links/LinkPrimary/LinkPrimary";
 import Footer from "@/components/Organisms/Footer/Footer";
+import Routes from "@/utils/routes/Routes";
 import React from "react";
 
 const RegisterSuccess = () => {
@@ -8,14 +10,13 @@ const RegisterSuccess = () => {
             <div className="flex flex-row justify-between items-center py-12 px-48 max-2xl:px-10">
                 <LinkLogo />
             </div>
-            <div className="flex-grow w-1/2 flex flex-col items-center h-[85vh] justify-center mx-auto gap-7">
-                <div className="text-4xl font-semibold text-center">
+            <div className="flex-grow w-1/2 flex flex-col items-center h-[60vh] justify-center mx-auto gap-7">
+                <div className="text-4xl font-semibold text-center py-8">
                     ¡Gracias por registrarte a Salufy Salud!
                 </div>
-                <div className="text-xl font-normal text-center">
-                    Por favor, completa tu registro mediante el enlace de
-                    confirmación enviado a tu correo electrónico:
-                    pedro.alvarez@Salufysalud.com
+                <div className="flex flex-col gap-5 w-72">
+                    <LinkPrimary to={Routes.HOME}>Volver al incio</LinkPrimary>
+                    <LinkPrimary to={Routes.LOGIN}>Inicia Sesión</LinkPrimary>
                 </div>
             </div>
             <Footer />
