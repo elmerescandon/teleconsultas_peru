3fadcb7939e13c1d988a716130ccb61d956d7e80
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-23 21:07:53 -0500

    :sparkles: Update lastname

--- src/utils/mockups/doctorReduxMockup.ts
+++ src/utils/mockups/doctorReduxMockup.ts
@@ -6,12 +6,22 @@ const doctorReduxMockup : IUserInfo = {
     _id: doctorMockup._id,
     role: doctorMockup.role,
     name: doctorMockup.name,
+    lastName: doctorMockup.lastName,
     email: doctorMockup.email,
     phone: doctorMockup.phone,
     address: doctorMockup.address,
     profile_picture: doctorMockup.profile_picture,
     reserved_appointments: doctorMockup.reserved_appointments,
     ingresos: doctorMockup.ingresos,
+    district: doctorMockup.district,
+    province: doctorMockup.province,
+    region: doctorMockup.region,
+    reference: doctorMockup.reference,
+    interiorNumber: doctorMockup.interiorNumber,
+    specialties: doctorMockup.specialties,
+    active_prescriptions: doctorMockup.active_prescriptions,
+    termsAndConditions: doctorMockup.termsAndConditions,
+    age: doctorMockup.age,
 };    
 
 export default doctorReduxMockup;
