2afae14ae5e57945fea2111fffb9bd805b2c4e82
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-30 15:45:12 -0500

    :fire: Eliminate time display

--- src/components/Organisms/PatientMain/PatientMain.tsx
+++ src/components/Organisms/PatientMain/PatientMain.tsx
@@ -12,7 +12,7 @@ const PatientMain = () => {
     return (
         <div className="flex justify-between w-full items-center rounded-3xl border-neutral-300 border-2 py-10 px-24 max-xl:px-5 max-xl:flex-col max-xl:gap-5">
             <div className="flex flex-col gap-5">
-                <TimeDisplay />
+                {/* <TimeDisplay /> */}
                 <p className="text-3xl font-semibold">{`¡Buenos días, ${userInfo.name}!`}</p>
                 <p className="text-base font-normal">
                     Tienes 1 cita agendada para hoy
