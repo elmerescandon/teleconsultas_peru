7a3b680b957d944e662f0eedfe720f4aa071e695
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-30 15:07:58 -0500

    :fire: Eliminate utils

--- src/utils/functions/utils.ts
+++ src/utils/functions/utils.ts
@@ -101,27 +101,3 @@ export const areDatesEqual = (dateA : Date, dateB: Date) => {
     dateA.getDate() === dateB.getDate()
   );
 }
-
-export const dbToUser = (userDb: IUser) => {
-
-  const user : IUserInfo = {
-    _id: userDb._id,
-    role: userDb.role,
-    name: userDb.name,
-    lastName: userDb.lastName,
-    email: userDb.email,
-    phone: userDb.phone,
-    address: userDb.address!,
-    profile_picture: userDb.profile_picture!,
-    reserved_appointments: userDb.reserved_appointments,
-    ingresos: userDb.ingresos,
-    district: userDb.district!,
-    province: userDb.province!,
-    region: userDb.region!,
-    reference: userDb.reference!,
-    interiorNumber: userDb.interiorNumber!,
-  }; 
-
-  return user;
-
-}
