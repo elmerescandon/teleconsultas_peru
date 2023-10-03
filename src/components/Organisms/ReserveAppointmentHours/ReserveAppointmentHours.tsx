03a723111ad1fc738a2f71675b836b664db96df6
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-02 23:22:43 -0500

    :sparkles: Add loader to reserve appointment hourse

--- src/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours.tsx
+++ src/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours.tsx
@@ -1,13 +1,16 @@
 import SlotAppointment from "@/components/Atoms/SlotAppointment/SlotAppointment";
+import Loading from "@/components/Molecules/Loading/Loading";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
 import React from "react";
 
 type ReserveAppointmentHoursProps = {
     availableAppointments: IAvailableAppointment[];
+    loading: boolean;
 };
 
 const ReserveAppointmentHours = ({
     availableAppointments,
+    loading,
 }: ReserveAppointmentHoursProps) => {
     const slots = availableAppointments.map((appointment, index) => (
         <SlotAppointment key={index} availableAppointment={appointment} />
@@ -15,16 +18,22 @@ const ReserveAppointmentHours = ({
 
     return (
         <div className="px-5 max-">
-            <div className="text-xl font-semibold py-4">
-                Horarios Disponibles
+            <div className="flex items-center gap-5">
+                <div className="text-xl font-semibold py-4">
+                    Horarios Disponibles
+                </div>
+                {loading && <Loading size={5} />}
             </div>
-            <div className="flex flex-wrap gap-5 justify-start max-xl:justify-start max-md:justify-center">
-                {slots.length > 0 ? (
-                    slots.map((slot) => slot)
-                ) : (
-                    <p className="italic">No hay horarios disponibles</p>
-                )}
-            </div>
+
+            {!loading && (
+                <div className="flex flex-wrap gap-5 justify-start max-xl:justify-start max-md:justify-center">
+                    {slots.length > 0 ? (
+                        slots.map((slot) => slot)
+                    ) : (
+                        <p className="italic">No hay horarios disponibles</p>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
