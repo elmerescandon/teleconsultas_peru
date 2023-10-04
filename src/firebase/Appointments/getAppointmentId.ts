d946064cb641f03265d3ce26f06b063639e9d163
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-04 08:15:42 -0500

    :sparkles: Get appointment Id

---
+++ src/firebase/Appointments/getAppointmentId.ts
@@ -0,0 +1,11 @@
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import { readAppointmentSize } from "./readAppointmentSize";
+
+const getAppointmentId = async (appointment: IAppointment) => {
+    const sizeAppointment = await readAppointmentSize();
+    const userId = sizeAppointment === undefined ? 1 : sizeAppointment + 1;
+    const newAppointment = {...appointment, _id: `appointment${userId}`}
+    return newAppointment;
+}
+
+export default getAppointmentId;
