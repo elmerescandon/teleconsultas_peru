7999091ce7b01c7da1ce0a2134f208e7c3cc00ea
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-04 09:06:56 -0500

    :sparkles: Set info in popup appointment

--- src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
+++ src/components/Atoms/PopUp/PopUpAppointment/PopUpAppointment.tsx
@@ -1,66 +1,87 @@
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ButtonPrimary from "../../Buttons/ButtonPrimary/ButtonPrimary";
 import LabelInformation from "../../Labels/LabelInformation/LabelInformation";
 import {
     getAppointmentHours,
-    getDoctorNameMockup as getDoctorName,
-    getSpecialityName,
     statusToSpanish,
     stringToDate,
 } from "@/utils/functions/utils";
-import doctorsMockup from "@/utils/mockups/doctorsMockup";
-import specialitiesMockup from "@/utils/mockups/specialitiesMockup";
+import { getDoctorName } from "@/firebase/Doctor/getDoctorName";
+import { getSpecialityName } from "@/firebase/Speciality/getSpecialityName";
 
 type PopUpAppointmentProps = {
     onClose: () => void;
     appointment: IAppointment;
 };
 
-const PopUpAppointment = ({ onClose, appointment }: PopUpAppointmentProps) => (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-28 rounded-3xl flex flex-col gap-5">
-            <h2 className="text-3xl font-semibold mb-2">
-                {getSpecialityName(
-                    specialitiesMockup,
-                    appointment.specialityId
-                )}
-            </h2>
-            <div className="flex flex-col gap-2">
-                <LabelInformation
-                    label="Doctor"
-                    value={`${getDoctorName(
-                        doctorsMockup,
-                        appointment.doctorId
-                    )}`}
-                />
+const PopUpAppointment = ({ onClose, appointment }: PopUpAppointmentProps) => {
+    const [summary, setSummary] = useState<{
+        doctorName: string;
+        specialityName: string;
+    }>({ doctorName: "", specialityName: "" });
 
-                <LabelInformation
-                    label="Fecha"
-                    value={stringToDate(appointment.date)}
-                />
-                <LabelInformation
-                    label="Estado"
-                    value={`${statusToSpanish(appointment.status)}`}
-                />
-                <LabelInformation
-                    label="Horario"
-                    value={getAppointmentHours(
-                        appointment.startDate,
-                        appointment.endDate
-                    )}
-                />
-                <LabelInformation
-                    label="Razón"
-                    value={`${appointment.reason}`}
-                />
-            </div>
+    useEffect(() => {
+        const getInfoFromDb = async (
+            doctorId: string,
+            specialityId: string
+        ) => {
+            if (doctorId === "" || specialityId === "") return;
+            const doctor = await getDoctorName(doctorId);
+            const speciality = await getSpecialityName(specialityId);
 
-            <div>
-                <ButtonPrimary onClickFn={onClose}>Cerrar</ButtonPrimary>
+            if (doctor && speciality) {
+                setSummary({
+                    specialityName: speciality.name,
+                    doctorName: `Dr. ${doctor.name} ${doctor.lastName}`,
+                });
+            } else {
+                setSummary({ doctorName: "", specialityName: "" });
+            }
+        };
+
+        getInfoFromDb(appointment.doctorId, appointment.specialityId);
+    }, []);
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-28 rounded-3xl flex flex-col gap-5">
+                <h2 className="text-3xl font-semibold mb-2">
+                    {summary.specialityName}{" "}
+                </h2>
+                <div className="flex flex-col gap-2">
+                    <LabelInformation
+                        label="Doctor"
+                        value={summary.doctorName}
+                    />
+
+                    <LabelInformation
+                        label="Fecha"
+                        value={stringToDate(appointment.date)}
+                    />
+                    <LabelInformation
+                        label="Estado"
+                        value={`${statusToSpanish(appointment.status)}`}
+                    />
+                    <LabelInformation
+                        label="Horario"
+                        value={getAppointmentHours(
+                            appointment.startDate,
+                            appointment.endDate
+                        )}
+                    />
+                    <LabelInformation
+                        label="Razón"
+                        value={`${appointment.reason}`}
+                    />
+                </div>
+
+                <div>
+                    <ButtonPrimary onClickFn={onClose}>Cerrar</ButtonPrimary>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default PopUpAppointment;
