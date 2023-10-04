0c034b62e4813f7f6fa130e355ce6eb4ccaa46c7
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-04 08:31:44 -0500

    :sparkles: Get user appointments

---
+++ src/firebase/Appointments/getUserAppointments.ts
@@ -0,0 +1,23 @@
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import { and, collection, getDocs, query, where } from "firebase/firestore";
+import dbFirestore from "../config";
+
+const getUserAppointments = async (userId: string, status: string) => {
+    const q = query(collection(dbFirestore, "appointments"), and(where("patientId", "==", userId),where("status", "==", status)));
+    const snapShot = await getDocs(q);
+
+    const appointments: IAppointment[] = [];
+    if (snapShot.empty) {
+        return appointments;
+    }
+    snapShot.docs.forEach((doc) => {
+        const appointment = doc.data() as IAppointment;
+        appointments.push(appointment);
+    });
+    return appointments;
+    
+
+
+}
+
+export default getUserAppointments;
