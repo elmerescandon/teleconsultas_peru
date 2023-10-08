0a663010590019ade26960333a814893fd31a347
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-08 14:15:49 -0500

    :sparkles: Update text area handling

--- src/components/Atoms/Inputs/InputTextArea/InputTextArea.tsx
+++ src/components/Atoms/Inputs/InputTextArea/InputTextArea.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import { debounce } from "lodash";
+import React, { ChangeEvent, useRef } from "react";
 
 type InputTextAreaProps = {
     rows: number;
     cols: number;
     placeholder: string;
-    message: string;
     onChange: (value: string) => void;
 };
 
@@ -12,23 +12,27 @@ const InputTextArea = ({
     rows,
     cols,
     placeholder,
-    message,
     onChange,
 }: InputTextAreaProps) => {
-    const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        onChange(e.target.value);
+    const inputRef = useRef<HTMLTextAreaElement>(null);
+
+    const dedebonceHandleChange = debounce((value: string) => {
+        onChange(value);
+    }, 1000);
+
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        dedebonceHandleChange(e.target.value);
     };
 
-    // TODO: Add debounce
     return (
         <div className="flex">
             <textarea
+                ref={inputRef}
                 className="text-classic-black placeholder-neutral-400 border-neutral-300 border-2 rounded-md px-4 py-4 resize-none max-xl:w-full"
                 rows={rows}
                 cols={cols}
                 placeholder={placeholder}
-                value={message}
-                onChange={handleMessageChange}
+                onChange={handleChange}
             />
         </div>
     );
