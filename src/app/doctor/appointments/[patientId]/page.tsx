367f0ac947d4441fa76043b2ffbb79b3ab737553
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-08 11:05:29 -0500

    :sparkles: Get only patientId

--- src/app/doctor/appointments/[patientId]/page.tsx
+++ src/app/doctor/appointments/[patientId]/page.tsx
@@ -1,18 +1,14 @@
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import DoctorPatientSection from "@/components/Templates/DoctorPatientSection/DoctorPatientSection";
-import patientsMockup from "@/utils/mockups/patientsMockup";
-import React from "react";
 
 const page = ({ params }: { params: { patientId: string } }) => {
     const { patientId } = params;
-    const patient = patientsMockup.find((patient) => patient._id === patientId);
-
     return (
         <div>
             <Header />
-            {patient ? (
-                <DoctorPatientSection patient={patient} />
+            {patientId ? (
+                <DoctorPatientSection patientId={patientId} />
             ) : (
                 <p>No existe el paciente</p>
             )}
