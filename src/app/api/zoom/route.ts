8aee8b72c40db7e6b025dc4cd22e0539ea87fa96
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-08 10:03:04 -0500

    :sparkles: Update route values

---
+++ src/app/api/zoom/route.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+// };
+
+// export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
