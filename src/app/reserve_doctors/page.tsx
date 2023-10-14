33ddf8c6fc5704ab1dbd83b4b41fa2daf34ba459
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-14 12:25:49 -0500

    :hammer: Create reserve doctors

---
+++ src/app/reserve_doctors/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Organisms/Footer/Footer";
+import Header from "@/components/Organisms/Header/Header";
+import React from "react";
+
+const page = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+};
+
+export default page;
