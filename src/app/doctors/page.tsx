364ae86afdcf10e37ea79bf024005d42d8f062aa
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-14 18:31:31 -0500

    :sparkles: Update doctors page

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -1,26 +1,33 @@
+import LinkPrimary from "@/components/Atoms/Links/LinkPrimary/LinkPrimary";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
-import InformationForm from "@/components/Organisms/InformationForm/InformationForm";
-import MainSection from "@/components/Organisms/MainSection/MainSection";
-import DoctorPersonsSection from "@/components/Templates/DoctorPersonsSection/DoctorPersonsSection";
 import DoctorsCardSection from "@/components/Templates/DoctorsCardSection/DoctorsCardSection";
+import Routes from "@/utils/routes/Routes";
 
 const Doctors = () => {
     return (
         <main>
             <Header />
             <div className="page-body">
-                <MainSection
-                    title={"Expande tu práctica médica con MediConnect"}
-                    subtitle={
-                        "Llega y atiende a pacientes más allá de tu consultorio a través de tus consultas en línea"
-                    }
-                />
+                <div className="flex flex-col items-center px-56 py-10 max-lg:justify-center max-lg:pb-5 max-lg:px-20 max-lg:pt-36 max-sm:px-5">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="text-5xl font-semibold max-lg:text-center">
+                            Expande tu práctica médica con Salufy
+                        </h1>
+                        <h5 className="text-2xl font-semibold max-lg:text-center">
+                            Atiende a pacientes más allá de tu consultorio a
+                            través de tus consultas en línea
+                        </h5>
+                        <div className="flex justify-center py-5 text-center gap-10">
+                            <LinkPrimary to={Routes.REGISTER}>
+                                <div className="w-48 flex items-center justify-center h-full text-xl">
+                                    Soy un profesional de la salud
+                                </div>
+                            </LinkPrimary>
+                        </div>
+                    </div>
+                </div>
                 <DoctorsCardSection />
-                <DoctorPersonsSection />
-                <InformationForm
-                    title={"¡Pertenece a Salufy Salud y únete al cambio!"}
-                />
             </div>
             <Footer />
         </main>
