0e295ca81a606059f251d676f669b7085e16842a
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 16:30:34 -0500

    :sparkles: Add props to login section

--- src/components/Templates/LoginSection/LoginSection.tsx
+++ src/components/Templates/LoginSection/LoginSection.tsx
@@ -4,19 +4,20 @@ import LoginOptions from "@/components/Organisms/LoginOptions/LoginOptions";
 import React from "react";
 
 type LoginSectionProps = {
-    type: string;
+    role: string;
+    label: string;
 };
 
-const LoginSection = ({ type }: LoginSectionProps) => {
+const LoginSection = ({ role, label }: LoginSectionProps) => {
     return (
         <div className="flex flex-col gap-12 justify-center w-1/3 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
             <div className="flex flex-col text-center items-center w-full gap-3">
                 <LinkLogo size="big" />
             </div>
             <p className="text-center text-3xl font-semibold text-brand-900">
-                {type}
+                {label}
             </p>
-            <LoginForms />
+            <LoginForms role={role} />
             <LoginOptions />
         </div>
     );
