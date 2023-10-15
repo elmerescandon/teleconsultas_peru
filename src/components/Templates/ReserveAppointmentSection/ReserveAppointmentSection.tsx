a3bb1181641c30851db59e61d8424cb1bdfa31a9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 17:52:29 -0500

    :fire: Eliminate unsued code

--- src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
+++ src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
@@ -1,10 +1,9 @@
 "use client";
-import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
+import ButtonReserve from "@/components/Atoms/Buttons/ButtonReserve/ButtonReserve";
 import PopUpReservation from "@/components/Atoms/PopUp/PopUpReservation/PopUpReservation";
 import ReserveAppointmentCalendar from "@/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar";
 import ReserveAppointmentForms from "@/components/Organisms/ReserveAppointmentForms/ReserveAppointmentForms";
 import ReserveAppointmentHours from "@/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours";
-import ReserveSummary from "@/components/Organisms/ReserveSummary/ReserveSummary";
 import { getAvailableDates } from "@/firebase/Availability/getAvailableDates";
 import { useAppSelector } from "@/redux/hooks";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
@@ -20,14 +19,15 @@ const ReserveAppointmentSection = () => {
     const userState: IUserState = useAppSelector((state) => state.user);
     const { logged } = userState;
     const router = useRouter();
+
     const [popUp, setPopUp] = useState<boolean>(false);
     const [popUpRegister, setPopUpRegister] = useState<boolean>(false);
     const [availableAppointments, setAvailableAppointments] = useState<
         IAvailableAppointment[]
     >([]);
+
     const appointment = useAppointment();
     const { date, doctorId, specialityId } = appointment;
-
     const [loadingDates, setLoadingDates] = useState<boolean>(false);
 
     const onClickReserve = (appointment: IAppointment) => {
@@ -73,26 +73,15 @@ const ReserveAppointmentSection = () => {
         <div className="px-48  py-10  max-xl:px-20 max-md:px-5">
             <div className="flex max-xl:flex-col max-xl:items-center max-xl:justify-around ">
                 <ReserveAppointmentForms />
-                <div className="w-1/3 max-xl:w-full max-xl:flex max-md:flex-col max-xl:justify-around">
+                <div className="w-full max-xl:w-full max-xl:flex max-md:flex-col max-xl:justify-around">
                     <ReserveAppointmentCalendar />
                     <ReserveAppointmentHours
                         loading={loadingDates}
                         availableAppointments={availableAppointments}
                     />
                 </div>
-                <ReserveSummary />
             </div>
-            <div className="max-xl:pt-10 flex justify-end w-full max-xl:justify-center">
-                <div className="max-xl:w-1/2">
-                    <ButtonPrimary
-                        onClickFn={() => {
-                            onClickReserve(appointment);
-                        }}
-                    >
-                        Agendar
-                    </ButtonPrimary>
-                </div>
-            </div>
+            <ButtonReserve onClickFn={() => onClickReserve(appointment)} />
             {popUp && (
                 <PopUpReservation
                     title="¡Lo sentimos!"
