0525a852c7426f63806aa7c0ea815bf6ee4e6c18
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 13:06:47 -0500

    :sparkles: Create bigger template

--- src/components/Templates/LoginSection/LoginSection.tsx
+++ src/components/Templates/LoginSection/LoginSection.tsx
@@ -3,16 +3,19 @@ import LoginForms from "@/components/Organisms/LoginForms/LoginForms";
 import LoginOptions from "@/components/Organisms/LoginOptions/LoginOptions";
 import React from "react";
 
-const LoginSection = () => {
+type LoginSectionProps = {
+    type: string;
+};
+
+const LoginSection = ({ type }: LoginSectionProps) => {
     return (
-        <div className="flex flex-col gap-12 justify-center w-1/4 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
+        <div className="flex flex-col gap-12 justify-center w-1/3 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
             <div className="flex flex-col text-center items-center w-full gap-3">
                 <LinkLogo size="big" />
-                <div className="text-lg font-normal">
-                    Conectando pacientes y doctores para una atención médica
-                    personalizada en línea
-                </div>
             </div>
+            <p className="text-center text-3xl font-semibold text-brand-900">
+                {type}
+            </p>
             <LoginForms />
             <LoginOptions />
         </div>
