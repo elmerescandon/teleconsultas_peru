e0c240e04bc3083cdcc985043df20795ce0dd051
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 17:52:54 -0500

    :sparkles: Update query reserve variable

--- src/utils/constants/queryReserve.ts
+++ src/utils/constants/queryReserve.ts
@@ -1,7 +1,7 @@
 enum QueryReserve {
-    NUTRITION = 'nutri',
-    GENERAL = 'gen',
-    PSYCHOLOGY = 'psy'
+    NUTRITION = 'speciality3',
+    GENERAL = 'speciality2',
+    PSYCHOLOGY = 'speciality1'
 }
 
 export default QueryReserve;
