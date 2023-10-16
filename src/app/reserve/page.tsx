07d21a8b668267953e230a6897355ad735344c45
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 20:16:59 -0500

    :sparkles: Select type of something

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -1,14 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import ReserveAppointmentSection from "@/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection";
+import SelectReservation from "@/components/Organisms/SelectReservation/SelectReservation";
 
 const page = () => {
+    const [type, setType] = useState(0);
     return (
         <div>
             <Header />
-            <ReserveAppointmentSection />
+            <SelectReservation type={type} setType={setType} />
+            {type === 0 ? <ReserveAppointmentSection /> : null}
             <Footer />
         </div>
     );
