e5c42a5ded0a1f616323ff35bffa814da636944b
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 23:29:35 -0500

    :sparkles: Set create meeting

--- src/app/api/zoom/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-// export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-// };
-
-// export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
