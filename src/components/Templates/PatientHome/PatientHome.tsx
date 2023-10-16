6178aa500c0c90340194a68b4b4badd2a075caff
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 21:38:38 -0500

    :sparkles: Update patient history

--- src/components/Templates/PatientHome/PatientHome.tsx
+++ src/components/Templates/PatientHome/PatientHome.tsx
@@ -1,7 +1,7 @@
 "use client";
 import PatientAppointments from "@/components/Organisms/PatientAppointments/PatientAppointments";
+import PatientHistory from "@/components/Organisms/PatientHistory/PatientHistory";
 import PatientMain from "@/components/Organisms/PatientMain/PatientMain";
-import PatientTreatments from "@/components/Organisms/PatientTreatments/PatientTreatments";
 import React from "react";
 
 const PatientHome = () => {
@@ -9,7 +9,7 @@ const PatientHome = () => {
         <div className="flex flex-col items-center px-48 h-full py-10 max-xl:pt-36 gap-5 max-xl:px-5">
             <PatientMain />
             <div className="flex gap-5 w-full max-xl:flex-col">
-                <PatientTreatments />
+                <PatientHistory />
                 <PatientAppointments />
             </div>
         </div>
