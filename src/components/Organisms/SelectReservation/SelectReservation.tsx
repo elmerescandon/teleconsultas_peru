a1c6206998034a5e277a17c011edb76605a55eb9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 20:17:20 -0500

    :sparkles: Select reservation

---
+++ src/components/Organisms/SelectReservation/SelectReservation.tsx
@@ -0,0 +1,32 @@
+import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
+import React from "react";
+
+type SelectReservationProps = {
+    type: number;
+    setType: (type: number) => void;
+};
+
+const SelectReservation = ({ type, setType }: SelectReservationProps) => {
+    return (
+        <div className="flex px-64 gap-52">
+            <ButtonPrimary
+                onClickFn={() => {
+                    setType(0);
+                }}
+                selected={type === 0}
+            >
+                Reserva según especialista
+            </ButtonPrimary>
+            <ButtonPrimary
+                onClickFn={() => {
+                    setType(1);
+                }}
+                selected={type === 1}
+            >
+                Reserva según disponibilidad horaria
+            </ButtonPrimary>
+        </div>
+    );
+};
+
+export default SelectReservation;
