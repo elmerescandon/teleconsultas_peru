ba8bd0912f7a85337ccb938cf653ac9b4e75c978
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 23:04:13 -0500

    :sparkles: Update doctor patients

---
+++ src/components/Molecules/DoctorPatients/DoctorPatients.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorPatients = () => {
+    return (
+        <div className="w-full">
+            <p className="text-xl font-semibold pb-5"> Mis pacientes</p>
+        </div>
+    );
+};
+
+export default DoctorPatients;
