33b7c3baef7a6c7402e46ecb489849f954cc30f7
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-15 20:17:09 -0500

    :sparkles: Create selected button

--- src/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary.tsx
+++ src/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary.tsx
@@ -5,12 +5,19 @@ type ButtonPrimaryProps = {
     children: React.ReactNode;
     onClickFn: () => void;
     type?: string;
+    selected?: boolean;
 };
 
-const ButtonPrimary = ({ children, onClickFn }: ButtonPrimaryProps) => {
+const ButtonPrimary = ({
+    children,
+    onClickFn,
+    selected,
+}: ButtonPrimaryProps) => {
     return (
         <button
-            className="text-basic-white bg-brand-600 rounded-lg text-lg font-normal border-2 border-brand-600 py-4 px-10 w-full active:bg-basic-white active:text-brand-600 active:border-2"
+            className={`text-basic-white bg-brand-600 rounded-lg text-lg font-normal border-2 border-brand-600 py-4 px-10 w-full active:bg-basic-white active:text-brand-600 active:border-2 ${
+                selected ? "bg-white text-brand-600" : ""
+            }`}
             onClick={onClickFn}
         >
             {children}
