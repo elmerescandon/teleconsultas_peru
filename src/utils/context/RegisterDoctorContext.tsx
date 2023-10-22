c4c66af807451c0c0cc97c442c2bf56c0ce63d24
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-22 09:44:47 -0500

    :sparkles: Create register doctor context

---
+++ src/utils/context/RegisterDoctorContext.tsx
@@ -0,0 +1,79 @@
+import { Dispatch, createContext, useContext, useReducer } from "react";
+import { IRegisterDoctorActions } from "../Interfaces/reducers/IRegisterAction";
+
+enum RegisterActions {
+    SET_GENERAL = "SET_GENERAL",
+    SET_LOCATION = "SET_LOCATION",
+    SET_DOCINFO = "SET_DOCINFO",
+}
+
+export const RegisterDoctorContext = createContext<IRegisterDoctor>(undefined!);
+export const RegisterDispatchContext = createContext<
+    Dispatch<IRegisterDoctorActions>
+>(undefined!);
+
+type RegisterProviderProps = {
+    children: React.ReactNode;
+};
+
+const RegisterProvider = ({ children }: RegisterProviderProps) => {
+    const [register, dispatch] = useReducer(registerReducer, {
+        name: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        id: "",
+
+        // location
+        region: "",
+        province: "",
+        district: "",
+        address: "",
+        reference: "",
+        interiorNumber: "",
+
+        // Doc Info
+        age: "",
+        sex: "",
+        specialities: [],
+        cmpNumber: "",
+        curriculum: "",
+        phone: "",
+        termsAndConditions: "",
+    });
+
+    return (
+        <RegisterDoctorContext.Provider value={register}>
+            <RegisterDispatchContext.Provider value={dispatch}>
+                {children}
+            </RegisterDispatchContext.Provider>
+        </RegisterDoctorContext.Provider>
+    );
+};
+
+const registerReducer = (
+    register: IRegisterDoctor,
+    action: IRegisterDoctorActions
+) => {
+    switch (action.type) {
+        case RegisterActions.SET_GENERAL:
+            return { ...register, ...action.payload };
+        case RegisterActions.SET_LOCATION:
+            return { ...register, ...action.payload };
+        case RegisterActions.SET_DOCINFO:
+            return { ...register, ...action.payload };
+        default:
+            return register;
+    }
+};
+
+export const useRegisterState = () => {
+    return useContext(RegisterDoctorContext);
+};
+
+export const useRegisterDispatch = () => {
+    return useContext(RegisterDispatchContext);
+};
+
+export default RegisterProvider;
