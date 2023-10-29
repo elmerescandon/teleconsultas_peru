bd5c50a6e5280b3fc12ac1e9fe3a04214e1022a0
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-29 18:41:20 -0500

    :sparkles: Update page doctors general

--- src/app/doctors/general/page.tsx
+++ src/app/doctors/general/page.tsx
@@ -1,75 +1,61 @@
 import CardInfo from "@/components/Atoms/Cards/CardInfo/CardInfo";
 import LabelPrimary from "@/components/Atoms/Labels/LabelPrimary/LabelPrimary";
 import LinkPrimary from "@/components/Atoms/Links/LinkPrimary/LinkPrimary";
+import EspecialityMain from "@/components/Organisms/EspecialityMain/EspecialityMain";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
+import PhoneSteps from "@/components/Organisms/PhoneSteps/PhoneSteps";
+import ReviewDoctorSection from "@/components/Organisms/ReviewDoctorSection/ReviewDoctorSection";
 import Routes from "@/utils/routes/Routes";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
     const specialityPhysical = [
-        "Medicina General",
+        "Gastroenterología",
+        "Neurología",
         "Endrocrinología",
-        "Nutrición",
         "Cardiología",
         "Otorrinolaringología",
-        "Ginecología",
         "Pediatría",
         "Dermatología",
         "Traumatología",
+        "Oftalmología",
+        "Reumatología",
+        "Hematología",
+        "Infectología",
+        "Nefrología",
+        "Neumología",
     ];
     return (
         <div>
             <Header />
-            <div className="flex flex-col px-48">
-                <h1 className="text-5xl font-semibold text-center py-10">
-                    Doctores
-                </h1>
-                <div className="flex flex-col gap-5">
-                    <div className="flex flex-wrap gap-5 items-center justify-center">
-                        {specialityPhysical.map((speciality, index) => (
-                            <LabelPrimary content={speciality} key={index} />
-                        ))}
-                    </div>
-                    <div className="flex gap-5">
-                        <CardInfo
-                            text="Para el año 2011, el 70% de los médicos especialistas
-                        registrados en Perú se encontraban laborando en Lima y
-                        solo el 30% en otras regiones. Hasta el momento la
-                        centralización del sistema de salud no ha cambiado
-                        significativamente"
-                        />
-
-                        <CardInfo
-                            text="Según declaraciones de Janice Sanfield, Directora
-                        Ejecutiva de Videnza Consultores, en el CADE Ejecutivos
-                        del 2021, “hay 14 médicos por cada 10 mil habitantes,
-                        una de las cifras más bajas de Latinoamérica"
-                        />
-
-                        <CardInfo
-                            text="En Salufy estamos comprometidos en 
-                        revolucionar 
-                        la forma en que los doctores 
-                        se conectan con pacientes para brindarles una atención médica de calidad."
-                        />
-
-                        <CardInfo
-                            text="Nuestra misión es facilitar el acceso a la atención
-                        médica personalizada, brindando una plataforma en la que
-                        los pacientes puedan encontrar doctores y horarios según
-                        sus necesidades"
-                        />
-                    </div>
-                </div>
-                <div className="flex py-5">
-                    <LinkPrimary to={Routes.RESERVE}>
-                        {" "}
-                        ¡Reserva tu cita!
-                    </LinkPrimary>
+            <EspecialityMain
+                title="Medicina General"
+                subtitle="En Salufy sabemos que la salud mental es un componente esencial en la salud general, por eso nuestro compromiso y esfuerzo en impulsarlo."
+                to={Routes.RESERVE}
+                image={
+                    <Image
+                        src="/PSYCHO_IMAGE.png"
+                        alt="salufy-nutri"
+                        width={500}
+                        height={500}
+                    />
+                }
+                type="medicine"
+            />
+            <div className="px-72 flex flex-col justify-center items-center">
+                <p className="text-4xl text-brand-600 font-semibold py-5">
+                    Especialidades
+                </p>
+                <div className="flex gap-5 py-10 px-48  flex-wrap items-center justify-center">
+                    {specialityPhysical.map((speciality, index) => (
+                        <LabelPrimary content={speciality} key={index} />
+                    ))}
                 </div>
             </div>
-
+            <PhoneSteps color="medicine" />
+            <ReviewDoctorSection />
             <Footer />
         </div>
     );
