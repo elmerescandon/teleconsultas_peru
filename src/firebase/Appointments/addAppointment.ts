e022339d6e5de164999c9c3f92a8ac501f34bb20
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-28 21:15:09 -0500

    :sparkles: Add URL join

--- src/firebase/Appointments/addAppointment.ts
+++ src/firebase/Appointments/addAppointment.ts
@@ -2,10 +2,13 @@ import {collection, addDoc} from 'firebase/firestore';
 import dbFirestore from '../config';
 import IAppointment from '@/utils/Interfaces/reducers/IAppointment';
 import getAppointmentId from './getAppointmentId';
+import createZoomAppointment from '@/lib/zoom/createZoomAppointment';
 
 const addAppointment = async (appointment: IAppointment) => {
     try {
         const newAppointment = await getAppointmentId(appointment);
+        const joinURL = await createZoomAppointment(newAppointment._id, newAppointment.startDate);
+        newAppointment.joinURL = joinURL;
         await addDoc(collection(dbFirestore, "appointments"), newAppointment);        
     } catch (e) {
         throw new Error("No se pudo agendar la cita, inténtelo nuevamente luego. Error:" + e);
