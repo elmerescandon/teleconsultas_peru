600a2a0379cc000ca8521db8e08c5940fa9a8520
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-29 18:41:59 -0500

    :lipstick: Check on detail colors

--- src/components/Organisms/EspecialityDetails /EspecialityDetails.tsx
+++ src/components/Organisms/EspecialityDetails /EspecialityDetails.tsx
@@ -1,5 +1,7 @@
-import { PsychologyContent } from "@/utils/constants/images/EspecialityImages";
-import Image from "next/image";
+import {
+    NutritionContent,
+    PsychologyContent,
+} from "@/utils/constants/images/EspecialityImages";
 import React from "react";
 
 type EspecialityDetailsProps = {
@@ -15,15 +17,19 @@ const EspecialityDetails = ({
     risks,
 }: EspecialityDetailsProps) => {
     let colorValue = "bg-salufy-blue";
+    let colorText = "text-salufy-blue";
     switch (type) {
         case "psychology":
             colorValue = "bg-salufy-psycho-light";
+            colorText = "text-salufy-psycho";
             break;
         case "nutrition":
             colorValue = "bg-salufy-nutri-light";
+            colorText = "text-salufy-nutri";
             break;
         case "general":
             colorValue = "bg-salufy-blue";
+            colorText = "text-salufy-blue";
             break;
         default:
             break;
@@ -31,28 +37,62 @@ const EspecialityDetails = ({
 
     return (
         <div
-            className={`flex items-center mx-48 my-10 ${colorValue} rounded-3xl`}
+            className={`flex items-center mx-48 mb-10 ${colorValue} rounded-3xl justify-between`}
         >
             {type === "psychology" ? PsychologyContent.banner : null}
-            <div>
-                {type === "psychology" ? PsychologyContent.ratio : null}
-                <p>Síntomas más comúnes</p>
-                {symptoms.map((symptom, index) => (
-                    <div key={index}>
-                        {type === "psychology" ? PsychologyContent.item : null}
-                        <p>{symptom}</p>
+            {type === "nutrition" ? NutritionContent.banner : null}
+            <div className="flex pr-32 gap-20 w-full">
+                <div className="bg-basic-white flex flex-col w-1/2 rounded-3xl px-10 min-h-fit pb-10">
+                    {type === "psychology" ? PsychologyContent.ratio : null}
+                    {type === "nutrition" ? NutritionContent.ratio : null}
+                    <p
+                        className={`${colorText} text-3xl -mt-10 font-semibold pb-5`}
+                    >
+                        Síntomas más comúnes
+                    </p>
+                    <div className="flex flex-col gap-3">
+                        {symptoms.map((symptom, index) => (
+                            <div
+                                key={index}
+                                className="flex gap-5 items-center"
+                            >
+                                {type === "psychology"
+                                    ? PsychologyContent.item
+                                    : null}
+                                {type === "nutrition"
+                                    ? NutritionContent.item
+                                    : null}
+                                <p className="text-lg">{symptom}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div>
-                {type === "psychology" ? PsychologyContent.risk : null}
-                <p>Factores de riesgo</p>
-                {risks.map((risk, index) => (
-                    <div key={index}>
-                        {type === "psychology" ? PsychologyContent.item : null}
-                        <p>{risk} </p>
+                </div>
+                <div className="bg-basic-white flex flex-col w-1/2 rounded-3xl px-10 min-h-fit pb-10">
+                    {type === "psychology" ? PsychologyContent.risk : null}
+                    {type === "nutrition" ? NutritionContent.risk : null}
+
+                    <p
+                        className={`${colorText} text-3xl -mt-10 font-semibold pb-5`}
+                    >
+                        Factores de riesgo
+                    </p>
+                    <div className="flex flex-col gap-3">
+                        {risks.map((risk, index) => (
+                            <div
+                                key={index}
+                                className="flex gap-5 items-center"
+                            >
+                                {type === "psychology"
+                                    ? PsychologyContent.item
+                                    : null}
+                                {type === "nutrition"
+                                    ? NutritionContent.item
+                                    : null}
+                                <p className="text-lg">{risk} </p>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
