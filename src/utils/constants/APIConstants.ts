5633f88b3986781929a0b5a9b917f5d2b1802128
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-28 21:22:13 -0500

    :hammer: Create constants

---
+++ src/utils/constants/APIConstants.ts
@@ -0,0 +1,16 @@
+export const zoomAuthURL = 'https://zoom.us/oauth/token?grant_type=account_credentials&account_id=';
+export const zoomURL = 'https://api.zoom.us/v2';
+export const zoomMeetingURL = 'https://api.zoom.us/v2/users/me/meetings';
+
+
+
+
+export const sessionDuration = 30;
+export const sessionTimeZone = 'America/Lima';
+export const sessionSettings = {
+    host_video: true,
+    participant_video: false,
+    join_before_host: true,
+    waiting_room: true,
+    mute_upon_entry: true,
+}
