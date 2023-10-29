44020128e1f8ff4914598f4ac076f45ef9358c4c
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-29 14:40:16 -0500

    :sparkles: Avoid colors

--- src/components/Atoms/Links/LinkPrimary/LinkPrimary.tsx
+++ src/components/Atoms/Links/LinkPrimary/LinkPrimary.tsx
@@ -5,17 +5,32 @@ type LinkPrimaryProps = {
     children: React.ReactNode;
     to: string;
     type?: string;
+    color?: string;
 };
 
-const LinkPrimary = ({ children, to, type }: LinkPrimaryProps) => {
+const LinkPrimary = ({ children, to, type, color }: LinkPrimaryProps) => {
+    let colorValue = "text-basic-white bg-brand-600";
+    switch (color) {
+        case "normal":
+            colorValue = "text-basic-white bg-brand-200";
+            break;
+        case "psychology":
+            colorValue = "text-basic-white bg-salufy-psycho";
+            break;
+        case "nutrition":
+            colorValue = "text-basic-white bg-salufy-nutri";
+            break;
+        default:
+            colorValue = "text-basic-white bg-brand-600";
+            break;
+    }
+
     return (
         <Link
             href={to}
             className={`${
-                type
-                    ? "text-basic-black bg-basic-white"
-                    : "text-basic-white bg-brand-600"
-            } rounded-lg px-6 text-md font-normal py-4 text-center`}
+                type ? "text-basic-black bg-basic-white" : `${colorValue}`
+            } rounded-lg px-6 text-md font-semibold py-4 text-center`}
         >
             {children}
         </Link>
