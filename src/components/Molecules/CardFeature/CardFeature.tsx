08f92f388739f94215e4d522dd315d5d1658a39a
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-29 12:04:00 -0500

    :sparkles: Add card feature

--- src/components/Molecules/CardFeature/CardFeature.tsx
+++ src/components/Molecules/CardFeature/CardFeature.tsx
@@ -7,9 +7,11 @@ type CardFeatureProps = {
 
 const CardFeature = ({ iconComponent, title }: CardFeatureProps) => {
     return (
-        <div className="h-80 flex flex-col gap-5 items-center bg-brand-50 py-8 px-10 rounded-xl max-xl:h-64 max-xl:px-5">
-            <div className="rounded-full bg-brand-100 p-4">{iconComponent}</div>
-            <div className="text-center text-2xl font-semibold w-40">
+        <div className="h-72 flex flex-col gap-1 items-center bg-brand-200 py-3 px-10 rounded-xl max-xl:h-64 max-xl:px-5">
+            <div className="rounded-full bg-brand-100 px-4 py-2">
+                {iconComponent}
+            </div>
+            <div className="text-center text-2xl font-semibold w-56 text-salufy-blue">
                 {title}
             </div>
         </div>
