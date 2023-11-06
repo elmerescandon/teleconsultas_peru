66df0b0cab4610f154af5c9ef5eda5a2bd4687d0
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-06 07:05:47 -0500

    Update DoctorAvailabilityHours component to include PopUpMyAvailability and Button to view availability

--- src/components/Organisms/DoctorAvailabilityHours/DoctorAvailabilityHours.tsx
+++ src/components/Organisms/DoctorAvailabilityHours/DoctorAvailabilityHours.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
 import PopUpAddAvailability from "@/components/Atoms/PopUp/PopUpAddAvailability/PopUpAddAvailability";
-import DayAvailability from "@/components/Molecules/DayAvailability/DayAvailability";
+import PopUpMyAvailability from "@/components/Atoms/PopUp/PopUpMyAvailability/PopUpMyAvailability";
 import { useAppSelector } from "@/redux/hooks";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
 import React, { useEffect, useState } from "react";
@@ -13,8 +13,16 @@ const DoctorAvailabilityHours = () => {
     const [showAddAvailability, setShowAddAvailability] =
         useState<boolean>(false);
 
+    const [myAvaialability, setMyAvailability] = useState<boolean>(false);
     return (
-        <div className="flex flex-col items-center gap-1">
+        <div className="flex  items-center gap-1">
+            <ButtonPrimary
+                onClickFn={() => {
+                    setMyAvailability(true);
+                }}
+            >
+                Ver mi disponibilidad
+            </ButtonPrimary>
             <ButtonPrimary
                 onClickFn={() => {
                     setShowAddAvailability(true);
@@ -22,18 +30,16 @@ const DoctorAvailabilityHours = () => {
             >
                 Agregar disponibilidad
             </ButtonPrimary>
-            {/* <div className="flex items-center gap-5">
-                <div>
-                    <DayAvailability day="Lunes" />
-                    <DayAvailability day="Martes" />
-                    <DayAvailability day="Miercoles" />
-                </div>
-                <div>
-                    <DayAvailability day="Jueves" />
-                    <DayAvailability day="Viernes" />
-                    <DayAvailability day="Sábado" />
-                </div>
-            </div> */}
+
+            {myAvaialability && (
+                <PopUpMyAvailability
+                    doctorId={_id}
+                    specialityId={specialities ? specialities[0] : ""}
+                    onClose={() => {
+                        setMyAvailability(false);
+                    }}
+                />
+            )}
             {showAddAvailability && (
                 <PopUpAddAvailability
                     specialityId={specialities ? specialities[0] : ""}
