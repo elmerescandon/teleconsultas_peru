4b1214e33366560d234980f8e6108dad517af416
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-11 16:42:43 -0500

    Update page title to "Medicina General"

--- src/app/doctors/general/page.tsx
+++ src/app/doctors/general/page.tsx
@@ -31,7 +31,7 @@ const page = () => {
         <div>
             <Header />
             <EspecialityMain
-                title="Psicología"
+                title="Medicina General"
                 subtitle="En Salufy sabemos que la salud mental es un componente esencial en la salud general, por eso nuestro compromiso y esfuerzo en impulsarlo."
                 to={Routes.RESERVE}
                 image={
