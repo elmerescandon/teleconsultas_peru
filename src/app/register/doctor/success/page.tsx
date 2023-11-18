82645a2b80f6cdc43f2a785e25fd4a9231339481
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-18 17:07:41 -0500

    Update styling and layout in doctor registration success page

--- src/app/register/doctor/success/page.tsx
+++ src/app/register/doctor/success/page.tsx
@@ -6,11 +6,11 @@ import React from "react";
 
 const RegisterSuccess = () => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col h-screen">
             <div className="flex flex-row justify-between items-center py-12 px-48 max-2xl:px-10">
                 <LinkLogo />
             </div>
-            <div className="flex-grow w-1/2 flex flex-col items-center h-[63vh] justify-center mx-auto gap-7">
+            <div className="flex-grow w-1/2 flex flex-col items-center justify-center mx-auto gap-7">
                 <div className="text-4xl font-semibold text-center py-8">
                     <div>¡Gracias por registrarte a Salufy!</div>
                     <div>Estamos validando tu información.</div>
@@ -20,7 +20,7 @@ const RegisterSuccess = () => {
                     inscripción. Si tienes alguna consulta, comunícate con
                     nostros a soporte@salufyorg.com
                 </p>
-                <div className="flex flex-col gap-5 w-72">
+                <div className="flex flex-col gap-5 w-72 py-10">
                     <LinkPrimary to={Routes.HOME}>Volver al incio</LinkPrimary>
                 </div>
             </div>
