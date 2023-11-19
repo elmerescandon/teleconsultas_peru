f7b3b6b1faa2a631073752d58b951028966c53bb
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-18 21:59:08 -0500

    Refactor PopUpAddAvailability component

--- src/components/Atoms/PopUp/PopUpAddAvailability/PopUpAddAvailability.tsx
+++ src/components/Atoms/PopUp/PopUpAddAvailability/PopUpAddAvailability.tsx
@@ -1,9 +1,6 @@
 "use client";
-import AddAvailabilityByDate from "@/components/Molecules/AddAvailabilityByDate/AddAvailabilityByDate";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import InputSelect from "../../Inputs/InputSelect/InputSelect";
-import AddAvailabilityByPrevDay from "@/components/Molecules/AddAvailabilityByPrevDay/AddAvailabilityByPrevDay";
+import AddAvailabilityGeneral from "@/components/Molecules/AddAvailabilityGeneral/AddAvailabilityGeneral";
 
 type PopUpAddAvailabilityProps = {
     onClose: () => void;
@@ -16,57 +13,20 @@ const PopUpAddAvailability = ({
     doctorId,
     specialityId,
 }: PopUpAddAvailabilityProps) => {
-    const [state, setState] = useState<string>("1");
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-10 rounded-3xl flex flex-col gap-5">
+            <div className="bg-white p-10 rounded-3xl flex flex-col gap-5 w-1/3 max-xl:w-5/6">
                 <button onClick={onClose}>
                     <XMarkIcon className="w-10 h-10 ml-auto" />
                 </button>
-
                 <div className="px-10 pb-10 h-full">
                     <h2 className="text-3xl font-semibold mb-2">
                         Agregar disponibilidad
                     </h2>
-
-                    <div className="px-5 py-5 bg-brand-600 rounded-2xl my-5">
-                        <InputSelect
-                            selectId="select-availability-type"
-                            placeholder="Tipo de disponibilidad"
-                            onChange={(value) => {
-                                setState(value);
-                            }}
-                            fistValue="1"
-                            options={[
-                                {
-                                    label: "Agregar disponibilidad",
-                                    value: "1",
-                                },
-                                {
-                                    label: "Replicar disponibilidad",
-                                    value: "2",
-                                },
-                            ]}
-                        />
-                    </div>
-
-                    {state !== "2" && state !== "1" && (
-                        <p className="h-full text-xl w-72 text-center text-gray-500">
-                            Selecciona cómo desea agregar su disponibilidad
-                        </p>
-                    )}
-                    {state === "2" && (
-                        <AddAvailabilityByPrevDay
-                            doctorId={doctorId}
-                            specialityId={specialityId}
-                        />
-                    )}
-                    {state === "1" && (
-                        <AddAvailabilityByDate
-                            doctorId={doctorId}
-                            specialityId={specialityId}
-                        />
-                    )}
+                    <AddAvailabilityGeneral
+                        doctorId={doctorId}
+                        specialityId={specialityId}
+                    />
                 </div>
             </div>
         </div>
