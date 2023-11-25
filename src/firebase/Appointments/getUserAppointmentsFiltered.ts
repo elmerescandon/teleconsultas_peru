5ccacbe526ea6e96b13e0228aa707e5a379fede5
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-25 14:48:23 -0500

    Add getUserAppointmentsFiltered function to fetch user appointments with filters

---
+++ src/firebase/Appointments/getUserAppointmentsFiltered.ts
@@ -0,0 +1,36 @@
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import { and, collection, getDocs, query, where } from "firebase/firestore";
+import dbFirestore from "../config";
+import IDateRangeAppointment from "@/utils/Interfaces/IDateRangeAppointment";
+
+const getUserAppointmentsFiltered = async (userId: string, status: string, date: IDateRangeAppointment , specialityId: string) => {
+
+    if (date.init === null || date.finish === null) {
+        throw Error("Date range is not valid");
+    }
+    const initDate = new Date(date.init);
+    const finishDate = new Date(date.finish);
+
+    const q = query(collection(dbFirestore, "appointments"), and(where("patientId", "==", userId),
+                                                                 where("status", "==", status), 
+                                                                 where("specialityId", "==", specialityId),
+                                                                 and(where("date", ">=",initDate), where("date", "<=", finishDate))
+                                                                 ));
+    
+    const snapShot = await getDocs(q);
+
+    const appointments: IAppointment[] = [];
+    if (snapShot.empty) {
+        return appointments;
+    }
+    snapShot.docs.forEach((doc) => {
+        const appointment = doc.data() as IAppointment;
+        appointments.push(appointment);
+    });
+    return appointments;
+    
+
+
+}
+
+export default getUserAppointmentsFiltered;
