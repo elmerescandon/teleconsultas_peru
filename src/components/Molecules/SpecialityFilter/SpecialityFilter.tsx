e770448fc62db2da360c3fbc1c04a534500e9839
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-25 15:52:42 -0500

    Refactor SpecialityFilter component to use InputSelect and fetch specialities from database

--- src/components/Molecules/SpecialityFilter/SpecialityFilter.tsx
+++ src/components/Molecules/SpecialityFilter/SpecialityFilter.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import InputSelect from "@/components/Atoms/Inputs/InputSelect/InputSelect";
+import { getSpecialities } from "@/firebase/Speciality/getSpecialities";
+import ISelectOptions from "@/utils/Interfaces/ISelectOptions";
+import { getSpecialitiesOptions } from "@/utils/functions/utils";
+import React, { useEffect, useState } from "react";
 
-const SpecialityFilter = () => {
-    return <div>SpecialityFilter</div>;
+type SpecialityFilterProps = {
+    setSpeciality: (value: string) => void;
+};
+
+const SpecialityFilter = ({ setSpeciality }: SpecialityFilterProps) => {
+    const [specialitiesOptions, setSpecialitiesOptions] = useState<
+        ISelectOptions[]
+    >([]);
+
+    useEffect(() => {
+        const getSpecialitiesFromDb = async () => {
+            const specialities = await getSpecialities();
+            if (specialities) {
+                setSpecialitiesOptions(getSpecialitiesOptions(specialities));
+            }
+        };
+        getSpecialitiesFromDb();
+    }, []);
+    return (
+        <div>
+            <InputSelect
+                onChange={(speciality) => {
+                    setSpeciality(speciality);
+                }}
+                selectId="specialities"
+                options={specialitiesOptions}
+                placeholder="Selecciona la especialidad"
+            />
+        </div>
+    );
 };
 
 export default SpecialityFilter;
