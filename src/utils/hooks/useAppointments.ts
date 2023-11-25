f59d37af981e8392e809175aac80f8fb584d022f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-25 14:48:53 -0500

    Refactor useAppointments hook to filter appointments by date and speciality

--- src/utils/hooks/useAppointments.ts
+++ src/utils/hooks/useAppointments.ts
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import IAppointment from "../Interfaces/reducers/IAppointment";
 import IDateRangeAppointment from "../Interfaces/IDateRangeAppointment";
-import getUserAppointments from "@/firebase/Appointments/getUserAppointments";
 import IUserInfo from "@/redux/state-interfaces/User/IUserInfo";
+import getUserAppointmentsFiltered from "@/firebase/Appointments/getUserAppointmentsFiltered";
+import getUserAppointments from "@/firebase/Appointments/getUserAppointments";
 
 const useAppointments = (date : IDateRangeAppointment, user: IUserInfo, setSelectedAppointment: (value: string) => void ) : IAppointment[] => {
 
     const [appointments, setAppointments] = useState<IAppointment[]>([]);
 
     const getAppointments = async () => {
-        const appointments = await getUserAppointments(user._id, "pending");
+        let appointments: IAppointment[] = [];
+        if (date.init === null || date.finish === null) {
+            appointments = await getUserAppointments(user._id, "pending");
+        }else{
+            appointments = await getUserAppointmentsFiltered(user._id, "pending", date, "speciality1");
+        }
         setAppointments(appointments);
     };
 
