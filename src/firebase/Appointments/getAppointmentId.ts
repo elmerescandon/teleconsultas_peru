b9f1abf4f1551f10f9ba74556badfb58d44e5689
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-28 20:42:12 -0500

    Add date property to newAppointment object

--- src/firebase/Appointments/getAppointmentId.ts
+++ src/firebase/Appointments/getAppointmentId.ts
@@ -4,7 +4,7 @@ import { readAppointmentSize } from "./readAppointmentSize";
 const getAppointmentId = async (appointment: IAppointment) => {
     const sizeAppointment = await readAppointmentSize();
     const userId = sizeAppointment === undefined ? 1 : sizeAppointment + 1;
-    const newAppointment = {...appointment, _id: `appointment${userId}`}
+    const newAppointment = {...appointment, _id: `appointment${userId}`, date: new Date(appointment.date)}
     return newAppointment;
 }
 
