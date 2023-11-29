4d9c47e6353327d36bd020854ef65c46c084cd33
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-28 20:40:40 -0500

    Add ProfileDocs component

---
+++ src/components/Organisms/ProfileDocs/ProfileDocs.tsx
@@ -0,0 +1,18 @@
+import DocumentItem from "@/components/Molecules/DocumentItem/DocumentItem";
+import React from "react";
+
+const ProfileDocs = () => {
+    return (
+        <div className="w-full pt-5">
+            <div className="text-xl font-semibold">Documentos</div>
+            <div className="flex flex-wrap items-center justify-center w-full mt-5 gap-14">
+                <DocumentItem document="DNI" type="id" />
+                <DocumentItem document="Colegiatura" type="cmp" />
+                <DocumentItem document="Curriculum" type="curriculum" />
+                <DocumentItem document="Título" type="titulo" />
+            </div>
+        </div>
+    );
+};
+
+export default ProfileDocs;
