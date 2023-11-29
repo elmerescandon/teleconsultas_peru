566fdbbb327a6533dbd683e3aa56a8dbe0c62b44
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-28 20:40:47 -0500

    Refactor DoctorProfileSection component

--- src/components/Templates/DoctorProfileSection/DoctorProfileSection.tsx
+++ src/components/Templates/DoctorProfileSection/DoctorProfileSection.tsx
@@ -1,15 +1,25 @@
 import DoctorAvailabilityHours from "@/components/Organisms/DoctorAvailabilityHours/DoctorAvailabilityHours";
 import ProfileDetailed from "@/components/Organisms/ProfileDetailed/ProfileDetailed";
+import ProfileDocs from "@/components/Organisms/ProfileDocs/ProfileDocs";
 import ProfileMain from "@/components/Organisms/ProfileMain/ProfileMain";
 import React from "react";
 
 const DoctorProfileSection = () => {
     return (
-        <div className="flex px-48 py-10 flex-wrap max-xl:px-4 max-xl:py-40 max-xl:flex-col h-[90vh]">
+        <div
+            className="flex px-48 py-10 flex-wrap 
+                        max-2xl:px-10 max-2xl:flex-col
+                        max-xl:py-40
+                        max-md:px-5"
+        >
             <ProfileMain />
-            <div className="w-2/3">
+            <div
+                className="w-2/3 
+                           max-2xl:flex max-2xl:flex-col max-2xl:items-center max-2xl:justify-center max-2xl:w-full"
+            >
                 <DoctorAvailabilityHours />
                 <ProfileDetailed />
+                <ProfileDocs />
             </div>
         </div>
     );
