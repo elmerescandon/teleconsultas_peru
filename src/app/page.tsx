61ed0b39b3230c3462224e9bbbcb8b228003d135
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-02 14:55:06 -0500

    Remove unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import CardSection from "@/components/Organisms/CardSection/CardSection";
 import CardSectionMobile from "@/components/Organisms/CardSection/CardSectionMobile";
-import DataSection from "@/components/Organisms/DataSection/DataSection";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import MainHomeSection from "@/components/Organisms/MainHomeSection/MainHomeSection";
 import PhoneSteps from "@/components/Organisms/PhoneSteps/PhoneSteps";
-import PhoneStepsMobile from "@/components/Organisms/PhoneSteps/PhoneStepsMobile";
 import SecondaryHomeSection from "@/components/Organisms/SecondaryHomeSection/SecondaryHomeSection";
 
 const Home = () => {
@@ -17,11 +15,7 @@ const Home = () => {
             <CardSection />
             <CardSectionMobile />
             <PhoneSteps color="normal" />
-            <PhoneStepsMobile color="normal" />
-            {/* <div className="page-body"> */}
             <SecondaryHomeSection />
-            {/* <DataSection /> */}
-            {/* </div> */}
             <Footer />
         </main>
     );
