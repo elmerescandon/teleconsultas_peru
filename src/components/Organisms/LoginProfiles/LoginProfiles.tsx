9b7beb1ad4f0e9b88f51628cd69d487aeb1a9468
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-02 20:53:46 -0500

    Refactor login profiles and login selection components

--- src/components/Organisms/LoginProfiles/LoginProfiles.tsx
+++ src/components/Organisms/LoginProfiles/LoginProfiles.tsx
@@ -4,14 +4,26 @@ import React from "react";
 
 const LoginProfiles = () => {
     return (
-        <div className="flex gap-5 justify-between">
-            <button className="text-basic-white bg-brand-600 rounded-lg text-2xl font-normal border-2 border-brand-600 py-4 px-10 w-full active:bg-basic-white active:text-brand-600 active:border-2">
-                <Link href={Routes.LOGIN_DOCTOR}>Soy doctor</Link>
-            </button>
+        <div
+            className="flex gap-5 justify-between
+                        max-xl:flex-col max-xl:w-full max-xl:px-4 max-xl:h-auto max-xl:gap-10 max-xl:justify-center max-xl:items-center"
+        >
+            <Link
+                //Animate a button to transition color when active
+                className="text-basic-white bg-brand-600 rounded-lg text-2xl font-normal border-2 border-brand-600 py-4 px-10 w-full text-center
+                active:bg-basic-white active:text-brand-600 active:border-2"
+                href={Routes.LOGIN_DOCTOR}
+            >
+                Soy doctor
+            </Link>
 
-            <button className="text-basic-white bg-brand-600 rounded-lg text-2xl font-normal border-2 border-brand-600 py-4 px-10 w-full active:bg-basic-white active:text-brand-600 active:border-2">
-                <Link href={Routes.LOGIN_PATIENT}>Soy paciente</Link>
-            </button>
+            <Link
+                className="text-basic-white bg-brand-600 rounded-lg text-2xl font-normal border-2 border-brand-600 py-4 px-10 w-full text-center
+                active:bg-basic-white active:text-brand-600 active:border-2"
+                href={Routes.LOGIN_PATIENT}
+            >
+                Soy paciente
+            </Link>
         </div>
     );
 };
--- src/components/Templates/LoginSelection/LoginSelection.tsx
+++ src/components/Templates/LoginSelection/LoginSelection.tsx
@@ -4,7 +4,10 @@ import React from "react";
 
 const LoginSelection = () => {
     return (
-        <div className="flex flex-col gap-12 justify-center w-1/4 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
+        <div
+            className="flex flex-col gap-12 justify-center w-1/4 h-full m-auto 
+                        max-xl:w-full max-xl:px-0 max-xl:py-28 max-xl:items-center"
+        >
             <div className="flex flex-col text-center items-center w-full gap-3">
                 <LinkLogo size="big" />
             </div>
