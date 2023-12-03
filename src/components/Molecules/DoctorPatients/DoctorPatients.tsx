f3f3664f33b9ad09da02ca959421dc0a85764af3
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-02 21:45:34 -0500

    Update DoctorPatients and DoctorHome components

--- src/components/Molecules/DoctorPatients/DoctorPatients.tsx
+++ src/components/Molecules/DoctorPatients/DoctorPatients.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const DoctorPatients = () => {
     return (
-        <div className="w-full">
-            <p className="text-xl font-semibold pb-5"> Mis pacientes</p>
+        <div className="w-full py-5">
+            <p className="text-xl font-semibold pb-3"> Mis pacientes</p>
+            <p className="italic w-full text-base text-center">
+                No hay ningún paciente en revisión
+            </p>
         </div>
     );
 };
--- src/components/Templates/DoctorHome/DoctorHome.tsx
+++ src/components/Templates/DoctorHome/DoctorHome.tsx
@@ -1,6 +1,5 @@
 import DoctorAppointments from "@/components/Organisms/DoctorAppointments/DoctorAppointments";
 import DoctorDashboard from "@/components/Organisms/DoctorDashboard/DoctorDashboard";
-import DoctorInitial from "@/components/Organisms/DoctorInitial/DoctorInitial";
 import PatientMain from "@/components/Organisms/PatientMain/PatientMain";
 import React from "react";
 
@@ -8,7 +7,10 @@ const DoctorHome = () => {
     return (
         <div className="flex flex-col items-center px-48 h-full py-10 max-xl:pt-36 gap-5 max-xl:px-5 w-full">
             <PatientMain />
-            <div className="w-full flex">
+            <div
+                className="w-full flex
+                            max-xl:flex-col"
+            >
                 <DoctorDashboard />
                 <DoctorAppointments />
             </div>
