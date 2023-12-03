0af777b0cdc93a9f2c66f30014e6d4ee7da074b2
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-02 19:08:41 -0500

    Add ElementSlider and EspecialitiesMedicine components

--- src/app/doctors/general/page.tsx
+++ src/app/doctors/general/page.tsx
@@ -1,4 +1,6 @@
 import LabelPrimary from "@/components/Atoms/Labels/LabelPrimary/LabelPrimary";
+import ElementSlider from "@/components/Organisms/ElementSlider/ElementSlider";
+import EspecialitiesMedicine from "@/components/Organisms/EspecialitiesMedicine/EspecialitiesMedicine";
 import EspecialityMain from "@/components/Organisms/EspecialityMain/EspecialityMain";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
@@ -8,22 +10,6 @@ import Image from "next/image";
 import React from "react";
 
 const page = () => {
-    const specialityPhysical = [
-        "Gastroenterología",
-        "Neurología",
-        "Endrocrinología",
-        "Cardiología",
-        "Otorrinolaringología",
-        "Pediatría",
-        "Dermatología",
-        "Traumatología",
-        "Oftalmología",
-        "Reumatología",
-        "Hematología",
-        "Infectología",
-        "Nefrología",
-        "Neumología",
-    ];
     return (
         <div>
             <Header />
@@ -41,22 +27,7 @@ const page = () => {
                 }
                 type="medicine"
             />
-            <div
-                className="px-72 flex flex-col justify-center items-center
-                            max-2xl:px-10"
-            >
-                <p className="text-4xl text-brand-600 font-semibold py-5">
-                    Especialidades
-                </p>
-                <div
-                    className="flex gap-5 py-10 px-48 flex-wrap items-center justify-center
-                                max-2xl:px-2 max-2xl:gap-4 max-2xl:flex-row max-2xl:justify-center"
-                >
-                    {specialityPhysical.map((speciality, index) => (
-                        <LabelPrimary content={speciality} key={index} />
-                    ))}
-                </div>
-            </div>
+            <EspecialitiesMedicine />
             <PhoneSteps color="medicine" />
             {/* <ReviewDoctorSection /> */}
             <Footer />
