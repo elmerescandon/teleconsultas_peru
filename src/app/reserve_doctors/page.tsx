381cd1d21fd14c561979c9f367a1a9af16febe59
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-08 12:49:03 -0500

    Update feature descriptions in reserve_doctors page

--- src/app/reserve_doctors/page.tsx
+++ src/app/reserve_doctors/page.tsx
@@ -1,33 +1,29 @@
 "use client";
-import CardSpeciality from "@/components/Molecules/CardSpeciality/CardSpeciality";
 import EspecialityInformation from "@/components/Organisms/EspecialityInformation/EspecialityInformation";
 import Footer from "@/components/Organisms/Footer/Footer";
 import Header from "@/components/Organisms/Header/Header";
 import PhoneSteps from "@/components/Organisms/PhoneSteps/PhoneSteps";
-import ReviewDoctorSection from "@/components/Organisms/ReviewDoctorSection/ReviewDoctorSection";
-import QueryReserve from "@/utils/constants/queryReserve";
 import Routes from "@/utils/routes/Routes";
-import { LightBulbIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import React from "react";
 
 const page = () => {
     const featureMedicine = [
-        "Encuentra la especialidad que buscas",
-        "Te brindamos todas las disponibilidades horarias",
-        "Elige el horario y doctor según tus necesidades",
+        "Encuentra la especialidad que buscas y reserva tu cita médica.",
+        "Te brindamos todas las disponibilidades horarias de los doctores para que puedas elegir.",
+        "Elige el horario y doctor según tus necesidades, y reserva tu cita médica.",
     ];
 
     const featureMental = [
-        "Tu salud mental es nuestra prioridad",
-        "Disponemos de psicólogos especializados",
-        "Te brindamos el apoyo necesario para tu bienestar mental y emocional",
+        "Nuestra variedad de psicólogos te apoyarán hacia tu bienestar psicológico y emocional.",
+        "Encuentra el psicólogo que más se adapte a tus necesidades.",
+        "Somos conscientes de la importancia de la salud mental. Nos preocupamos por ti.",
     ];
 
     const featureNutrition = [
-        "Somos conscientes de la importancia de una buena alimentación",
-        "Nuestros nutricionistas te ayudarán a lograr tus objetivos",
-        "Te guíámos durante todo el proceso de tu alimentación",
+        "Nuestros expertos te orientarán hacia un plan de alimentación saludable.",
+        "Te ofrecemos una variedad de nutricionistas para que elijas el que más se adapte a tus necesidades.",
+        "Llega a la cita con todas las consultas que tengas y necesites resolver.",
     ];
 
     return (
@@ -60,7 +56,7 @@ const page = () => {
                             height={500}
                         />
                     }
-                    items={featureMedicine}
+                    items={featureMental}
                     more={Routes.DOCTORS_PSYCHOLOGISTS}
                     reserve={Routes.RESERVE}
                     title="Psicología"
@@ -76,14 +72,14 @@ const page = () => {
                             height={500}
                         />
                     }
-                    items={featureMedicine}
+                    items={featureNutrition}
                     more={Routes.DOCTORS_NUTRITIONISTS}
                     reserve={Routes.RESERVE}
                     title="Nutrición"
                 />
             </div>
             <PhoneSteps color="normal" />
-            <ReviewDoctorSection />
+            {/* <ReviewDoctorSection /> */}
             <Footer />
         </div>
     );
