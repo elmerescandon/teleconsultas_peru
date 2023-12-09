f0387c89c4a244afeb7edf086d2f47e6ccaa5e07
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-09 15:10:56 -0500

    Remove empty line in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -39,7 +39,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      
     })
   ],
 };
