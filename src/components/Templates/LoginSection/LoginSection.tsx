7ab1312e65bd12774c7683f64f3e5ef7e8ba18a4
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-09 15:10:48 -0500

    Add login status alert component

--- src/components/Templates/LoginSection/LoginSection.tsx
+++ src/components/Templates/LoginSection/LoginSection.tsx
@@ -1,6 +1,9 @@
+"use client";
 import LinkLogo from "@/components/Atoms/Links/LinkLogo/LinkLogo";
 import LoginForms from "@/components/Organisms/LoginForms/LoginForms";
 import LoginOptions from "@/components/Organisms/LoginOptions/LoginOptions";
+import { BellAlertIcon } from "@heroicons/react/24/solid";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 type LoginSectionProps = {
@@ -9,6 +12,8 @@ type LoginSectionProps = {
 };
 
 const LoginSection = ({ role, label }: LoginSectionProps) => {
+    const params = useSearchParams();
+    const loginStatus = params.get("signin");
     return (
         <div className="flex flex-col gap-12 justify-center w-1/3 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
             <div className="flex flex-col text-center items-center w-full gap-3">
@@ -17,8 +22,17 @@ const LoginSection = ({ role, label }: LoginSectionProps) => {
             <p className="text-center text-3xl font-semibold text-brand-900">
                 {label}
             </p>
+            {loginStatus === "failed" && (
+                <div className="flex gap-5 text-center text-red-500 p-5 bg-red-200 border-red-500 border-2 rounded-3xl items-center justify-center">
+                    <BellAlertIcon className="w-5 h-5 inline-block" />
+                    <p>
+                        La cuenta de Google no está conectada a Salufy, por
+                        favor regístrese.
+                    </p>
+                </div>
+            )}
             <LoginForms role={role} />
-            <LoginOptions />
+            <LoginOptions role={role} />
         </div>
     );
 };
