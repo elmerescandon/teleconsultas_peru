edb983c234ec449bdee08c379f1791d9ffd0299b
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-09 15:10:44 -0500

    Fix login options and add role parameter

--- src/components/Organisms/LoginOptions/LoginOptions.tsx
+++ src/components/Organisms/LoginOptions/LoginOptions.tsx
@@ -1,12 +1,17 @@
 "use client";
 import GoogleColoredIcon from "@/components/Atoms/Icons/GoogleColoredIcon";
 import React from "react";
-import AppleColoredIcon from "@/components/Atoms/Icons/AppleColoredIcon";
-import FacebookColoredIcon from "@/components/Atoms/Icons/FacebookColoredIcon";
-import OutlookColoredIcon from "@/components/Atoms/Icons/OutlookColoredIcon";
+// import AppleColoredIcon from "@/components/Atoms/Icons/AppleColoredIcon";
+// import FacebookColoredIcon from "@/components/Atoms/Icons/FacebookColoredIcon";
+// import OutlookColoredIcon from "@/components/Atoms/Icons/OutlookColoredIcon";
 import { signIn } from "next-auth/react";
+import Routes from "@/utils/routes/Routes";
 
-const LoginOptions = () => {
+type LoginOptionsProps = {
+    role: string;
+};
+
+const LoginOptions = ({ role }: LoginOptionsProps) => {
     return (
         <div>
             <div className="flex items-center py-4">
@@ -17,7 +22,13 @@ const LoginOptions = () => {
                 <div className="flex-grow h-px bg-neutral-400"></div>
             </div>
             <div className="flex gap-10 justify-center">
-                <button onClick={() => signIn("google")}>
+                <button
+                    onClick={() =>
+                        signIn("google", {
+                            callbackUrl: `${Routes.LOGIN_GOOGLE}?role=${role}`,
+                        })
+                    }
+                >
                     <GoogleColoredIcon />
                 </button>
                 {/* <button>
