0918a2ec2b9333e36948c472992ce84b0ae5e2bc
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-09 11:15:19 -0500

    Update PatientHistory component styling

--- src/components/Organisms/PatientHistory/PatientHistory.tsx
+++ src/components/Organisms/PatientHistory/PatientHistory.tsx
@@ -51,7 +51,7 @@ const PatientHistory = () => {
     );
 
     return (
-        <div className="w-1/3 rounded-3xl border-neutral-300 border-2 py-5 px-10 h-96 max-xl:h-full max-xl:w-full">
+        <div className="w-1/3 rounded-3xl border-neutral-300 border-2 py-5 px-10 h-auto max-xl:h-full max-xl:w-full">
             <div className="text-xl font-semibold">Historial Médico</div>
             <div className="flex flex-col items-center py-5">
                 {patientAppointments.length > 0 &&
