f63d49889b4bb42b79d0611e15aaf5d28862c512
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-10 12:26:12 -0500

    Fix bug in login functionality

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,195 @@
+import React from "react";
+import Header from "@/components/Organisms/Header/Header";
+import Footer from "@/components/Organisms/Footer/Footer";
+
+const page = () => {
+    return (
+        <div>
+            <Header />
+            <div className="px-48 max-xl:px-10">
+                <h1 className="text-3xl text-center font-bold mb-8">
+                    Política de privacidad
+                </h1>
+                <p className="text-lg text-justify font-normal mb-8">
+                    Por medio del presente documento, Aika Corp SAC pone a
+                    disposición de los usuarios su Política de Privacidad, la
+                    cual se encuentra en armonía con la normativa vigente sobre
+                    protección de Datos Personales (Ley N° 29733 y Decreto
+                    Supremo 003-2013-JUS). A continuación, se señalan las
+                    finalidades de tratamiento de la información que usted nos
+                    ha suministrado, quiénes son los encargados de tratarla, el
+                    tiempo de almacenamiento en nuestras Bases de Datos, las
+                    medidas de seguridad, y los medios para ejercer sus derechos
+                    como titular de los Datos Personales.
+                </p>
+
+                <ol>
+                    <li>
+                        <h1 className="text-xl font-semibold mb-8">
+                            ¿CÓMO PROTEGEMOS SU INFORMACIÓN?
+                        </h1>
+                        <p className="text-lg text-justify font-normal mb-8">
+                            AIKA Corp SAC, ha adoptado todas las medidas
+                            técnicas, legales y organizacionales necesarias para
+                            garantizar la seguridad, la Asimetría de la
+                            información y el adecuado procesamiento de sus datos
+                            personales; así como para evitar cualquier
+                            manipulación indebida, pérdida accidental,
+                            destrucción o acceso no autorizado de terceros.
+                            Hacemos de su conocimiento que su información no
+                            será comercializada, transferida ni compartida sin
+                            su autorización y para fines ajenos a los que se
+                            describen a continuación.
+                        </p>
+                    </li>
+                    <li>
+                        <h1 className="text-xl font-semibold mb-8">
+                            CÓMO ALMACENAMOS Y PARA QUÉ FINALIDAD UTILIZAMOS SU
+                            INFORMACIÓN?
+                        </h1>
+                        <p className="text-lg text-justify font-normal mb-8">
+                            Aika Corp SAC recopila datos personales de las
+                            diversas secciones de su Sitio Web, formularios
+                            físicos, fuentes de acceso público y privado. A
+                            continuación, se precisan las características de
+                            cada procesamiento de información:
+                        </p>
+                        <ol>
+                            <li className="ml-8 text-base mb-4">
+                                1. Para el caso de las secciones “Crear Cuenta”
+                                tanto para profesionales de la salud como para
+                                las personas que adquieran los servicios; los
+                                datos personales serán almacenados en el banco
+                                de datos de “Clientes” (Código N° 2849). Así
+                                mismo, las finalidades de tratamiento son: (i)
+                                Procesar la adquisición del servicio realizado
+                                en virtud del contrato de consumo suscrito con;
+                                (ii) Establecer un canal de comunicación con
+                                para informarle respecto al servicio adquirido u
+                                otras condiciones relacionadas a la relación
+                                contractual que subsiste con ; (iii) Realizar
+                                acciones de cobranza; (iv) Incorpóralo en el
+                                registro de para fines estadísticos e
+                                históricos; (v) Establecer un medio de
+                                comunicación para atender sus consultas,
+                                preguntas, quejas y reclamos; (vi) en caso lo
+                                autorice expresamente, remitir promociones,
+                                beneficios, concursos, ofertas y, en general,
+                                publicidad sobre productos y servicios, así como
+                                obtener información de fuentes de acceso
+                                públicas y privadas para fines de perfilamiento.
+                                Para poder remitirle información de tipo
+                                publicitaria y realizar acciones de
+                                perfilamiento, usted deberá otorgarnos su
+                                autorización expresa por medio de los check box
+                                implementados en nuestras secciones web, dado
+                                que, sin su autorización, se abstendrá de
+                                enviarle este tipo de comunicación.
+                            </li>
+                            <li className="ml-8 text-base mb-4">
+                                2. Para el caso de la sección de atención médica
+                                y agendar una cita los datos personales serán
+                                almacenados en el banco de datos de “historial
+                                de atención” (Código N° 2848). Así mismo, las
+                                finalidades de tratamiento son: (i) Almacenar
+                                los datos provisionados por los profesionales de
+                                la salud por cada consulta médica realizada a
+                                través de la plataforma; así como los datos
+                                provisionados por la web, según el registro de
+                                la cita.
+                            </li>
+                            <li className="ml-8 text-base mb-8">
+                                3. Para el caso de la sección “Libro de
+                                Reclamaciones”, los datos personales serán
+                                almacenados en el banco de datos de “Reclamos”
+                                (Código N°2849). Así mismo, las finalidades de
+                                tratamiento son: (i) Establecer un medio de
+                                comunicación para atender sus consultas,
+                                preguntas, quejas y reclamos; y, (ii) llevar un
+                                registro de las quejas y reclamos con el
+                                propósito de cumplir con las normas de
+                                protección al consumidor.
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h1 className="text-xl font-semibold mb-8">
+                            ¿POR CUÁNTO TIEMPO ALMACENAMOS LA INFORMACIÓN?
+                        </h1>
+                        <p className="text-lg text-justify font-normal mb-8">
+                            Es importante señalar que el plazo de almacenamiento
+                            de todos los bancos de datos es indeterminado o
+                            hasta que el titular de datos personales ejerza
+                            algunos de los derechos previstos en la normativa de
+                            protección de datos personales.
+                        </p>
+                    </li>
+                    <li>
+                        <h1 className="text-xl font-semibold mb-8">
+                            ¿QUIÉN TIENE ACCESO A SU INFORMACIÓN?
+                        </h1>
+                        <p className="text-lg text-justify font-normal mb-8">
+                            Usted nos autoriza a que podamos compartir y
+                            encargar el tratamiento de su información personal
+                            con terceros que nos prestan servicios para mejorar
+                            nuestras actividades. En estos casos, garantizará
+                            que el tratamiento de sus datos se limite a las
+                            finalidades antes autorizadas y que se mantenga
+                            confidencialidad y se implementen las medidas de
+                            seguridad adecuadas. Una vez agende una cita con un
+                            profesional de la salud, está exprensando su
+                            consentimiento para que el profesional de la salud
+                            tenga acceso a su historial de atención de citas
+                            previas realizadas por www.salufy.com.pe; con la
+                            finalidad de analizarlos y realizar un mejor
+                            diagnóstico y tratamiento.
+                        </p>
+                    </li>
+                    <li>
+                        <h1 className="text-xl font-semibold mb-8">
+                            ¿CÓMO PUEDE EJERCER SUS DERECHOS COMO TITULAR DE
+                            DATOS PERSONALES?
+                        </h1>
+                        <p className="text-lg text-justify font-normal mb-8">
+                            Poner a disposición del cliente la solicitud de
+                            Derechos ARCO(acceso, rectificación, cancelación,
+                            oposición, información o revocación) a fin de que el
+                            cliente pueda ejercer cualquiera de estos derechos
+                            que la ley le asisten. Para ejercer los derechos
+                            antes mencionados el cliente deberá de presentar una
+                            solicitud al correo electrónico con el asunto
+                            “Solicitud Arco” Se responderá la presente solicitud
+                            en un plazo de 15 días hábiles contados desde la
+                            recepción del mismo, donde procederá a detallar,
+                            rectificar, cancelar u oponer los datos personales
+                            solicitados. Cabe precisar que, En caso, el cliente
+                            considere que no ha sido atendido, podrá presentar
+                            un reclamo ante la Dirección de Protección de Datos
+                            Personales, dirigiéndose a la Mesa de Partes del
+                            Ministerio de Justicia y Derechos Humanos ubicado en
+                            Calle Scipión Llona N° 350, Miraflores, Lima, Perú.
+                        </p>
+                    </li>
+                    <li>
+                        <h1 className="text-xl font-semibold mb-8">
+                            ACTUALIZACIONES DE LA PRESENTE POLÍTICA DE
+                            PRIVACIDAD
+                        </h1>
+                        <p className="text-lg text-justify font-normal mb-8">
+                            Con motivo de la mejora continua de nuestros
+                            procesos, AIKA CORP SAC podrá realizar correcciones
+                            y actualizaciones a esta Política de Privacidad. Por
+                            favor, verifique estos términos regularmente para
+                            consultar los cambios que puedan haber existido y de
+                            qué manera le pueden afectar.
+                        </p>
+                    </li>
+                </ol>
+            </div>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default page;
