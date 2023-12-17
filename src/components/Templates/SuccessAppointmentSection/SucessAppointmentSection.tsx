280293b87e709e27063c989405b73b0daf0d7738
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-17 18:52:03 -0500

    Add appointment retrieval and update functionality

--- src/components/Templates/SuccessAppointmentSection/SucessAppointmentSection.tsx
+++ src/components/Templates/SuccessAppointmentSection/SucessAppointmentSection.tsx
@@ -2,46 +2,51 @@
 import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
 import PaymentItem from "@/components/Molecules/PaymentItem/PaymentItem";
 import SuccessRecommendations from "@/components/Molecules/SuccessRecommendations/SuccessRecommendations";
+import getAppointment from "@/firebase/Appointments/getAppointment";
+import updateAppointmentField from "@/firebase/Appointments/updateAppointmentField";
 import { getDoctorName } from "@/firebase/Doctor/getDoctorName";
 import { getSpecialityName } from "@/firebase/Speciality/getSpecialityName";
-import { useAppointment } from "@/utils/context/AppointmentContext/AppointmentContext";
-import { getAppointmentHours, stringToDate } from "@/utils/functions/utils";
-import doctorsMockup from "@/utils/mockups/doctorsMockup";
-import specialitiesMockup from "@/utils/mockups/specialitiesMockup";
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import { getAppointmentHours } from "@/utils/functions/utils";
 import Routes from "@/utils/routes/Routes";
-import { useRouter } from "next/navigation";
+import { set } from "lodash";
+import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const SucessAppointmentSection = () => {
+    const [appoinment, setAppoinment] = useState<IAppointment | null>(null);
     const router = useRouter();
-    const appointment = useAppointment();
-    const { specialityId, doctorId, date, startDate, endDate } = appointment;
+    const searchParams = useSearchParams();
 
-    const [summary, setSummary] = useState<{
-        doctorName: string;
-        specialityName: string;
-    }>({ doctorName: "", specialityName: "" });
+    const paymentStatus = searchParams.get("collection_status");
+    const paymentReference = searchParams.get("external_reference");
+    const paymentId = searchParams.get("preference_id");
+
+    // const [summary, setSummary] = useState<{
+    //     doctorName: string;
+    //     specialityName: string;
+    // }>({ doctorName: "", specialityName: "" });
 
     useEffect(() => {
-        const getInfoFromDb = async (
-            doctorId: string,
-            specialityId: string
+        const getAppointmentInfo = async (
+            paymentReference: string | null,
+            paymentStatus: string | null
         ) => {
-            if (doctorId === "" || specialityId === "") return;
-            const doctor = await getDoctorName(doctorId);
-            const speciality = await getSpecialityName(specialityId);
-
-            if (doctor && speciality) {
-                setSummary({
-                    specialityName: speciality.name,
-                    doctorName: `Dr. ${doctor.name} ${doctor.lastName}`,
-                });
-            } else {
-                setSummary({ doctorName: "", specialityName: "" });
+            if (paymentReference === null) return;
+            const newAppointment: IAppointment | null = await getAppointment(
+                paymentReference
+            );
+            if (newAppointment !== null) {
+                setAppoinment(newAppointment);
+                appoinment;
+                updateAppointmentField(
+                    newAppointment._id,
+                    "status",
+                    "scheduled"
+                );
             }
         };
-
-        getInfoFromDb(doctorId, specialityId);
+        getAppointmentInfo(paymentReference, paymentStatus);
     }, []);
     return (
         <div
@@ -74,14 +79,14 @@ const SucessAppointmentSection = () => {
             <div className="pb-5">
                 <PaymentItem
                     label="Especialidad"
-                    name={summary.specialityName}
+                    name={appoinment?.specialityId!}
                 />
-                <PaymentItem label="Profesional" name={summary.doctorName} />
-                <PaymentItem label="Fecha" name={date} />
+                <PaymentItem label="Profesional" name={appoinment?.doctorId!} />
+                {/* <PaymentItem label="Fecha" name={date} />
                 <PaymentItem
                     label="Horario"
                     name={getAppointmentHours(startDate, endDate)}
-                />
+                /> */}
             </div>
             <SuccessRecommendations />
         </div>
