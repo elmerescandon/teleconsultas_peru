e928864af5725208037f19179290a3a2f5932ca1
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-17 18:51:51 -0500

    Refactor MercadoPagoPayment component

--- src/components/Organisms/MercadoPagoPayment/MercadoPagoPayment.tsx
+++ src/components/Organisms/MercadoPagoPayment/MercadoPagoPayment.tsx
@@ -1,13 +1,36 @@
-import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
-
-import React, { useEffect } from "react";
+"use client";
+import IPreference from "@/utils/Interfaces/API/MercadoPago/IPreference";
+import { Wallet, initMercadoPago } from "@mercadopago/sdk-react";
+import { reject } from "lodash";
+import React, { useEffect, useState } from "react";
 
 const MercadoPagoPayment = () => {
+    const onSumbit = async () => {
+        try {
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_MYPAGE_URL}/api/checkout`
+            );
+            const data: IPreference = await res.json();
+            if (data.init_point) {
+                return data.id;
+            }
+            throw new Error("No se pudo obtener el link de pago");
+        } catch (err) {
+            reject(err as Error);
+        }
+    };
+
     useEffect(() => {
-        initMercadoPago("TEST-ebc14502-8889-4ddb-b4ff-1d6cd21ec1f1");
+        if (process.env.NEXT_PUBLIC_MERCADO_PAGO_KEY) {
+            initMercadoPago(process.env.NEXT_PUBLIC_MERCADO_PAGO_KEY);
+        }
     }, []);
 
-    return <Wallet initialization={{ preferenceId: "1234" }} />;
+    return (
+        <div>
+            <Wallet locale="es-PE" onSubmit={onSumbit} />
+        </div>
+    );
 };
 
 export default MercadoPagoPayment;
