aba99503dadbf9c1e53f2e0681af236db6d1efff
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-12-17 23:14:34 -0500

    Update menu animation translation

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,8 +58,8 @@ module.exports = {
             },
             keyframes: {
                 "open-menu": {
-                    "0%": { transform: "translateX(-100%)" },
-                    "100%": { transform: "translateX(0%)" },
+                    "0%": { transform: "translateY(-100%)" },
+                    "100%": { transform: "translateY(0%)" },
                 },
             },
             animation: {
