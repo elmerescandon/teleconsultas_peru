d3690dbe0c48c4a0f1c998ce6a76f185bb485b1b
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-05 07:36:05 -0500

    Add forgot password functionality

--- src/components/Templates/LoginSection/LoginSection.tsx
+++ src/components/Templates/LoginSection/LoginSection.tsx
@@ -4,7 +4,7 @@ import LoginForms from "@/components/Organisms/LoginForms/LoginForms";
 import LoginOptions from "@/components/Organisms/LoginOptions/LoginOptions";
 import { BellAlertIcon } from "@heroicons/react/24/solid";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 type LoginSectionProps = {
     role: string;
@@ -14,6 +14,7 @@ type LoginSectionProps = {
 const LoginSection = ({ role, label }: LoginSectionProps) => {
     const params = useSearchParams();
     const loginStatus = params.get("signin");
+    const [forgotPassword, setForgotPassword] = useState(false);
     return (
         <div className="flex flex-col gap-12 justify-center w-1/3 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
             <div className="flex flex-col text-center items-center w-full gap-3">
@@ -31,7 +32,24 @@ const LoginSection = ({ role, label }: LoginSectionProps) => {
                     </p>
                 </div>
             )}
+            {forgotPassword && (
+                <div className="flex gap-5 text-center text-red-500 p-5 bg-red-200 border-red-500 border-2 rounded-3xl items-center justify-center">
+                    <BellAlertIcon className="w-5 h-5 inline-block" />
+                    <p>
+                        Por favor, envía un correo de soporte@salufyorg.com y te
+                        ayudaremos a recuperar tu contraseña.
+                    </p>
+                </div>
+            )}
             <LoginForms role={role} />
+            <button
+                className="text-left w-full text-brand-600 font-semibold"
+                onClick={() => {
+                    setForgotPassword(true);
+                }}
+            >
+                ¿Olvidaste tu contraseña? Haz click aquí
+            </button>
             <LoginOptions role={role} />
         </div>
     );
