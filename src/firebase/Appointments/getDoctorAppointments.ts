c725eaf18108043f53ab5653816be8513865d444
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-05 07:12:22 -0500

    Update getDoctorAppointments to accept an array of statuses

--- src/firebase/Appointments/getDoctorAppointments.ts
+++ src/firebase/Appointments/getDoctorAppointments.ts
@@ -2,8 +2,8 @@ import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 import { and, collection, getDocs, query, where } from "firebase/firestore";
 import dbFirestore from "../config";
 
-const getDoctorAppointments = async (doctorId: string, status: string) => {
-    const q = query(collection(dbFirestore, "appointments"), and(where("doctorId", "==", doctorId),where("status", "==", status)));
+const getDoctorAppointments = async (doctorId: string, status: string[]) => {
+    const q = query(collection(dbFirestore, "appointments"), and(where("doctorId", "==", doctorId),where("status", "in", status)));
     const snapShot = await getDocs(q);
 
     const appointments: IAppointment[] = [];
