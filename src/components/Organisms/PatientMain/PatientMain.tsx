6e1810cbf1b49a4cc1a64592be8da680904c3cd0
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-12 19:14:02 -0500

    Add appointment count to PatientMain component

--- src/components/Organisms/PatientMain/PatientMain.tsx
+++ src/components/Organisms/PatientMain/PatientMain.tsx
@@ -1,21 +1,35 @@
 "use client";
+import getAppointmentCount from "@/firebase/Appointments/getAppointmentCount";
 import { useAppSelector } from "@/redux/hooks";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
 import { IState } from "@/redux/store";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PatientMain = () => {
+    const [appointmentCount, setAppointmentCount] = useState<number>(0);
     const user: IUserState = useAppSelector((state: IState) => state.user);
     const { userInfo } = user;
+    const { _id, role } = userInfo;
+
+    useEffect(() => {
+        const getCount = async (
+            _id: string,
+            role: string,
+            status: string[]
+        ) => {
+            const count = await getAppointmentCount(_id, role, status);
+            setAppointmentCount(count);
+        };
+        getCount(_id, role, ["pending", "accepted"]);
+    }, [_id]);
     return (
         <div className="flex justify-between w-full items-center rounded-3xl border-neutral-300 border-2 py-10 px-24 max-xl:px-5 max-xl:flex-col max-xl:gap-5">
             <div className="flex flex-col gap-5">
-                {/* <TimeDisplay /> */}
                 <p className="text-3xl font-semibold">{`¡Buenos días, ${userInfo.name}!`}</p>
-                {/* <p className="text-base font-normal">
-                    Tienes 1 cita agendada para hoy
-                </p> */}
+                <p className="text-base font-normal">
+                    {`Tienes ${appointmentCount} cita agendada para hoy`}
+                </p>
             </div>
             <Image
                 src="/user-icon.jpg"
