17692e82b7a73344e258501a874f0f8a76776896
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-14 17:38:36 -0500

    Add LinkSecondary component and update import statements

--- src/components/Templates/LoginSection/LoginSection.tsx
+++ src/components/Templates/LoginSection/LoginSection.tsx
@@ -1,10 +1,12 @@
 "use client";
 import LinkLogo from "@/components/Atoms/Links/LinkLogo/LinkLogo";
+import LinkSecondary from "@/components/Atoms/Links/LinkSecondary/LinkSecondary";
 import LoginForms from "@/components/Organisms/LoginForms/LoginForms";
 import LoginOptions from "@/components/Organisms/LoginOptions/LoginOptions";
+import Routes from "@/utils/routes/Routes";
 import { BellAlertIcon } from "@heroicons/react/24/solid";
 import { useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import React from "react";
 
 type LoginSectionProps = {
     role: string;
@@ -14,7 +16,6 @@ type LoginSectionProps = {
 const LoginSection = ({ role, label }: LoginSectionProps) => {
     const params = useSearchParams();
     const loginStatus = params.get("signin");
-    const [forgotPassword, setForgotPassword] = useState(false);
     return (
         <div className="flex flex-col gap-12 justify-center w-1/3 h-full m-auto max-xl:w-full max-xl:px-5 max-xl:py-28">
             <div className="flex flex-col text-center items-center w-full gap-3">
@@ -32,24 +33,11 @@ const LoginSection = ({ role, label }: LoginSectionProps) => {
                     </p>
                 </div>
             )}
-            {forgotPassword && (
-                <div className="flex gap-5 text-center text-red-500 p-5 bg-red-200 border-red-500 border-2 rounded-3xl items-center justify-center">
-                    <BellAlertIcon className="w-5 h-5 inline-block" />
-                    <p>
-                        Por favor, envía un correo de soporte@salufyorg.com y te
-                        ayudaremos a recuperar tu contraseña.
-                    </p>
-                </div>
-            )}
+
             <LoginForms role={role} />
-            <button
-                className="text-left w-full text-brand-600 font-semibold"
-                onClick={() => {
-                    setForgotPassword(true);
-                }}
-            >
+            <LinkSecondary to={Routes.LOST_PASSWORD}>
                 ¿Olvidaste tu contraseña? Haz click aquí
-            </button>
+            </LinkSecondary>
             <LoginOptions role={role} />
         </div>
     );
