9c6faf153a4d7df1208879fd9e91d706c4137aff
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-15 13:55:26 -0500

    Refactor addAvailabilities function

--- src/firebase/Availability/addAvailabilities.ts
+++ src/firebase/Availability/addAvailabilities.ts
@@ -1,20 +1,19 @@
 import { addDoc, and, collection, getDocs, query, updateDoc, where } from "firebase/firestore";
 import dbFirestore from "../config";
-import IAvailabilitySlots from "@/utils/Interfaces/dataModel/IAvailabilitySlots";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
 
 
 const addAvailabilities = async (date:string, specialityId : string, doctorId : string, slots: IAvailableAppointment[]) => {
 
-    try {
-        const q = query(collection(dbFirestore, "availability"), and(where("doctor_id", "==", doctorId), where("speciality_id", "==", specialityId)));
+    try {          
+        const q = query(collection(dbFirestore, "availability"), and(where("doctor_id", "==", doctorId), 
+                                                                           where("speciality_id", "==", specialityId)));
         let snapShot = await getDocs(q);
-    
+
         if (snapShot.empty) {
-            await addDoc(collection(dbFirestore, "availability"), {doctor_id: doctorId, speciality_id: specialityId});
-            snapShot = await getDocs(q);
+            throw new Error("No existe disponibilidad para el doctor y especialidad seleccionados.");
         }
-    
+
         const docDate = snapShot.docs[0];
         const dateCollection = collection(docDate.ref, 'availability_slots');
         const dateQuery = query(dateCollection, where("date", "==", date));
@@ -26,19 +25,6 @@ const addAvailabilities = async (date:string, specialityId : string, doctorId : 
         }
     
         const dateDoc = dateDocs.docs[0];
-        let availabilitySlots =  dateDoc.data() as IAvailabilitySlots;
-    
-        // check if slot already exists, if not, add it
-        // slots.forEach((slot) => {
-        //     const index = availabilitySlots.slots.findIndex((s) => {
-        //         return s.startDate === slot.startDate && s.endDate === slot.endDate;
-        //     });
-        //     if (index === -1) {
-        //         availabilitySlots.slots.push(slot);
-        //     }else{
-        //         availabilitySlots.slots[index] = slot;
-        //     }
-        // });
         await updateDoc(dateDoc.ref, {...{slots: slots}})
     } catch (error) {
         throw error;
