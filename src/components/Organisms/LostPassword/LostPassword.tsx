5e0d34249e328fd5919bb3b81a3d9197bc2e8afe
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-14 22:07:54 -0500

    Refactor LostPassword component to handle loading state and display success message

--- src/components/Organisms/LostPassword/LostPassword.tsx
+++ src/components/Organisms/LostPassword/LostPassword.tsx
@@ -1,30 +1,45 @@
 "use client";
 import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
 import InputText from "@/components/Atoms/Inputs/InputText/InputText";
+import LinkSecondary from "@/components/Atoms/Links/LinkSecondary/LinkSecondary";
+import LoadingHorizontal from "@/components/Molecules/Loaders/LoadingHorizontal/LoadingHorizontal";
 import SendLostPassword from "@/firebase/Mail/SendLostPassword";
+import Routes from "@/utils/routes/Routes";
 import { Checkbox, FormControlLabel } from "@mui/material";
 import React, { useState } from "react";
 
-const LostPassword = () => {
-    const [email, setEmail] = useState("");
+type LostPasswordProps = {
+    email: string;
+    setEmail: (email: string) => void;
+};
+
+const LostPassword = ({ email, setEmail }: LostPasswordProps) => {
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [correct, setCorrect] = useState(false);
     const [isDoctor, setIsDoctor] = useState(false);
 
     const sendEmail = async () => {
+        setLoading(true);
         if (!email) {
             setError("Debes ingresar un correo electrónico");
+            setLoading(false);
             return;
         }
         setError("");
+        setCorrect(false);
         try {
             await SendLostPassword(email, isDoctor ? "doctor" : "patient");
+            setLoading(false);
+            setCorrect(true);
         } catch (e) {
             setError((e as Error).message);
+            setLoading(false);
         }
     };
 
     return (
-        <div className="w-full flex flex-col gap-5">
+        <div className="w-full flex flex-col gap-2">
             <InputText
                 onChangeFn={(email: string) => {
                     setEmail(email);
@@ -44,10 +59,21 @@ const LostPassword = () => {
                 }
                 label="Soy doctor"
             />
-            {error && <p className="text-red-500 py-2">{error}</p>}
-            <ButtonPrimary onClickFn={sendEmail}>
-                Enviar correo de recuperación
-            </ButtonPrimary>
+            {error && (
+                <p className="text-red-500 font-semibold py-2">{error}</p>
+            )}
+            {correct && (
+                <p className="text-green-500 font-semibold py-2">
+                    Se ha enviado un correo electrónico a {email} con
+                    instrucciones para recuperar tu contraseña
+                </p>
+            )}
+            <div>
+                <ButtonPrimary onClickFn={sendEmail} disabled={loading}>
+                    Enviar correo de recuperación
+                </ButtonPrimary>
+                {loading && <LoadingHorizontal />}
+            </div>
         </div>
     );
 };
