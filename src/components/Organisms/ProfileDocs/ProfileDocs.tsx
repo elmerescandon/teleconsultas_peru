bac3abe03e82c851768b6f93508c1ffab6f2667d
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-14 23:25:50 -0500

    Update document type in ProfileDocs component

--- src/components/Organisms/ProfileDocs/ProfileDocs.tsx
+++ src/components/Organisms/ProfileDocs/ProfileDocs.tsx
@@ -7,7 +7,7 @@ const ProfileDocs = () => {
             <div className="text-xl font-semibold">Documentos</div>
             <div className="flex flex-wrap items-center justify-center w-full mt-5 gap-14">
                 <DocumentItem document="DNI" type="id" />
-                <DocumentItem document="Colegiatura" type="cmp" />
+                <DocumentItem document="Colegiatura" type="colegiatura" />
                 <DocumentItem document="Curriculum" type="curriculum" />
                 <DocumentItem document="Título" type="titulo" />
             </div>
