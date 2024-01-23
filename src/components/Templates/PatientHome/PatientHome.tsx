bf09acc7f4600d65b2c2c566e1d2827ec009b73f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-23 13:21:34 -0500

    Update PatientHome styling

--- src/components/Templates/PatientHome/PatientHome.tsx
+++ src/components/Templates/PatientHome/PatientHome.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const PatientHome = () => {
     return (
-        <div className="flex flex-col items-center px-48 h-full py-10 max-xl:pt-36 gap-5 max-xl:px-5">
+        <div className="flex flex-col items-center px-48 h-full pb-5 max-lg:pt-36 gap-5 max-xl:px-5">
             <PatientMain />
             <div className="flex gap-5 w-full max-xl:flex-col">
                 <PatientHistory />
