2140f9367be4219895c305f12440a5509f9da675
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-23 13:18:21 -0500

    Add loading indicators and improve user experience

--- src/components/Templates/RegisterForm/RegisterForm.tsx
+++ src/components/Templates/RegisterForm/RegisterForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Loading from "@/components/Molecules/Loaders/Loading/Loading";
 import LoadingCircle from "@/components/Molecules/Loaders/LoadingCircle/LoadingCircle";
 import RegisterGeneral from "@/components/Organisms/RegisterGeneral/RegisterGeneral";
 import RegisterHeaders from "@/components/Organisms/RegisterHeaders/RegisterHeaders";
@@ -8,6 +9,8 @@ import { registerUser } from "@/firebase/User/addUser";
 import IPosting from "@/utils/Interfaces/hooks/IPosting";
 import { useRegisterState } from "@/utils/context/RegisterContext/RegisterContext";
 import Routes from "@/utils/routes/Routes";
+import next from "next";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
@@ -24,6 +27,16 @@ const RegisterForm = ({ role }: RegisterFormProps) => {
         error: null,
     });
 
+    const [correct, setCorrect] = useState(false);
+
+    const nextStep = () => {
+        setStep(step + 1);
+    };
+
+    const prevStep = () => {
+        setStep(step - 1);
+    };
+
     const postUser = async () => {
         setPosting({ loading: true, error: null });
         try {
@@ -36,8 +49,9 @@ const RegisterForm = ({ role }: RegisterFormProps) => {
                 throw new Error(
                     "Error al registrar. El email o DNI ya existe, intente de nuevo."
                 );
-            route.push(Routes.REGISTER_PATIENT_COMPLETE);
             setPosting({ loading: false, error: null });
+            setCorrect(true);
+            route.push(Routes.REGISTER_PATIENT_COMPLETE);
         } catch (error) {
             setPosting({ loading: false, error: error as Error });
         }
@@ -45,36 +59,26 @@ const RegisterForm = ({ role }: RegisterFormProps) => {
 
     return (
         <div className="w-1/2 flex flex-col justify-center m-auto h-fit py-14 max-xl:w-2/3 max-md:w-full">
-            <RegisterHeaders
-                currentStep={step}
-                steps={["General", "Ubicación", "Información"]}
-            />
+            {!posting.loading && !correct && (
+                <RegisterHeaders
+                    currentStep={step}
+                    steps={["General", "Ubicación", "Información"]}
+                />
+            )}
 
-            {!posting.loading && (
+            {!posting.loading && !correct && (
                 <div className="pb-3 flex flex-col justify-start">
-                    {step === 1 && (
-                        <RegisterGeneral
-                            nextFn={() => {
-                                setStep(step + 1);
-                            }}
-                        />
-                    )}
+                    {step === 1 && <RegisterGeneral nextFn={nextStep} />}
                     {step === 2 && (
                         <RegisterLocation
                             role={role}
-                            prevFn={() => {
-                                setStep(step - 1);
-                            }}
-                            nextFn={() => {
-                                setStep(step + 1);
-                            }}
+                            prevFn={prevStep}
+                            nextFn={nextStep}
                         />
                     )}
                     {step === 3 && (
                         <RegisterInformation
-                            prevFn={() => {
-                                setStep(step - 1);
-                            }}
+                            prevFn={prevStep}
                             nextFn={() => {
                                 postUser();
                             }}
@@ -82,11 +86,46 @@ const RegisterForm = ({ role }: RegisterFormProps) => {
                     )}
                 </div>
             )}
-            {posting.loading && <LoadingCircle />}
+            {posting.loading && !correct && (
+                <div className="flex flex-col justify-center items-center h-[70vh]">
+                    <Image
+                        className="pt-5"
+                        src="/LOGO_SALUFY.png"
+                        width={100}
+                        height={200}
+                        alt="logo-loader"
+                    />
+                    <p className="text-2xl font-bold text-brand-600">
+                        Registrando usuario
+                    </p>
+                    <p className="text-lg font-bold text-brand-600 pb-5">
+                        Espere por favor...
+                    </p>
+                    <Loading />
+                </div>
+            )}
             {posting.error && (
                 <p className="text-lg text-rose-600 font-bold">
                     {posting.error.message}
                 </p>
+            )}
+            {correct && (
+                <div className="flex flex-col justify-center items-center h-[70vh]">
+                    <Image
+                        className="pt-5"
+                        src="/LOGO_SALUFY.png"
+                        width={100}
+                        height={200}
+                        alt="logo-loader"
+                    />
+                    <p className="text-2xl font-bold text-brand-600">
+                        Registro exitoso
+                    </p>
+                    <p className="text-lg font-bold text-brand-600 pb-5">
+                        Redirigiendo...
+                    </p>
+                    <Loading />
+                </div>
             )}
         </div>
     );
