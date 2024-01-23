4d4f6115afe2555ff8f5c5ffe8c6638a6d0f85f8
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-23 13:21:30 -0500

    Update Header component styling

--- src/components/Organisms/Header/Header.tsx
+++ src/components/Organisms/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
             <BurgerHeader />
             <div className="max-lg:hidden flex flex-row justify-between items-center py-8 px-48 max-2xl:px-10">
                 <LinkLogo />
-                <div className="w-full flex justify-between gap-28 max-2xl:gap-2">
+                <div className="flex justify-between gap-28 max-2xl:gap-10">
                     <NavigationBar />
                     <LoginBar />
                 </div>
