12ebdf025f6ace8cfb984d3dcc70707086006830
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-24 00:30:21 -0500

    Add user state and selector to PatientHome component

--- src/components/Templates/PatientHome/PatientHome.tsx
+++ src/components/Templates/PatientHome/PatientHome.tsx
@@ -2,9 +2,17 @@
 import PatientAppointments from "@/components/Organisms/PatientAppointments/PatientAppointments";
 import PatientHistory from "@/components/Organisms/PatientHistory/PatientHistory";
 import PatientMain from "@/components/Organisms/PatientMain/PatientMain";
+import { useAppSelector } from "@/redux/hooks";
+import IUserState from "@/redux/state-interfaces/User/IUserState";
+import { IState } from "@/redux/store";
 import React from "react";
 
 const PatientHome = () => {
+    const state: IUserState = useAppSelector(
+        (state: IState) => state.user as IUserState
+    );
+    const { logged, userInfo } = state;
+
     return (
         <div className="flex flex-col items-center px-48 h-full pb-5 max-lg:pt-36 gap-5 max-xl:px-5">
             <PatientMain />
