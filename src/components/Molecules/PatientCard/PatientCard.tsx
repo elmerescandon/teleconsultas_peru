81447beee1383f7deefe20e4af934e1f9f8b50be
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-24 16:30:46 -0500

    Add cancelAppointment function to PatientCard component

--- src/components/Molecules/PatientCard/PatientCard.tsx
+++ src/components/Molecules/PatientCard/PatientCard.tsx
@@ -1,7 +1,9 @@
+import updateAppointmentField from "@/firebase/Appointments/updateAppointmentField";
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 import {
     getAppointmentHours,
     getSpecialityName,
+    stringToDate,
 } from "@/utils/functions/utils";
 import useAppointmentInfo from "@/utils/hooks/useAppointmentInfo";
 import specialitiesMockup from "@/utils/mockups/specialitiesMockup";
@@ -13,7 +15,7 @@ import {
     UserIcon,
 } from "@heroicons/react/24/solid";
 import { Timestamp } from "firebase/firestore";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import React from "react";
 
 type PatientCardProps = {
@@ -21,7 +23,6 @@ type PatientCardProps = {
 };
 
 const PatientCard = ({ appointment }: PatientCardProps) => {
-    const router = useRouter();
     const {
         specialityId,
         patientId,
@@ -33,6 +34,15 @@ const PatientCard = ({ appointment }: PatientCardProps) => {
     } = appointment;
 
     const { summary } = useAppointmentInfo(doctorId, specialityId, patientId);
+
+    const cancelAppointment = async () => {
+        try {
+            updateAppointmentField(_id, "status", "cancelled");
+        } catch {
+            console.log("Error al cancelar la cita");
+        }
+    };
+
     return (
         <div className="bg-neutral-100 rounded-2xl w-72 max-w-md">
             <div className="flex flex-col justify-start items-start p-4 gap-2">
@@ -46,36 +56,27 @@ const PatientCard = ({ appointment }: PatientCardProps) => {
                 </div>
                 <div className="flex flex-col gap-2">
                     <div className="text-sm font-semibold">
-                        {(appointment.date as unknown as Timestamp)
-                            .toDate()
-                            .toDateString()}
+                        {stringToDate(appointment.date as unknown as Timestamp)}
                     </div>
                     <div className="text-sm">
                         {getAppointmentHours(startDate, endDate)}
                     </div>
                 </div>
                 <div className="flex gap-5 py-1">
-                    <button
-                        onClick={() => {
-                            router.push(
-                                `${Routes.DOCTOR_APPOINTMENTS}/${patientId}`
-                            );
-                        }}
-                    >
+                    <a href={`${Routes.DOCTOR_APPOINTMENTS}/${patientId}`}>
                         <UserIcon className="w-5 h-5" />
-                    </button>
-                    <button
-                        onClick={() => {
-                            router.push(
-                                `${Routes.DOCTOR_APPOINTMENTS}/${patientId}/${_id}`
-                            );
-                        }}
+                    </a>
+                    <a
+                        href={`${Routes.DOCTOR_APPOINTMENTS}/${patientId}/${_id}`}
                     >
                         <PencilSquareIcon className="w-5 h-5" />
-                    </button>
+                    </a>
                     <a href={joinURL} target="_blank">
                         <ComputerDesktopIcon className="w-5 h-5" />
                     </a>
+                    <button onClick={cancelAppointment}>
+                        <TrashIcon className="w-5 h-5" />
+                    </button>
                 </div>
             </div>
         </div>
