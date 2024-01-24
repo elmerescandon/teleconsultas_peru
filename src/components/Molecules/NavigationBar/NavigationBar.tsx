49ae907599138c22acf0db0f7cc9dbf0d9c442fa
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-24 00:16:52 -0500

    Add session loading indicator to NavigationBar component

--- src/components/Molecules/NavigationBar/NavigationBar.tsx
+++ src/components/Molecules/NavigationBar/NavigationBar.tsx
@@ -2,22 +2,48 @@
 import { useAppSelector } from "@/redux/hooks";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
 import { IState } from "@/redux/store";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBarPatient from "../NavigationBarPatient/NavigationBarPatient";
 import NavigationBarGeneral from "../NavigationBarGeneral/NavigationBarGeneral";
 import NavigationBarDoctor from "../NavigationBarDoctor/NavigationBarDoctor";
+import { useSession } from "next-auth/react";
 
 const NavigationBar = () => {
+    const [loaded, setLoaded] = useState(false);
+
     const state: IUserState = useAppSelector(
         (state: IState) => state.user as IUserState
     );
     const { logged, userInfo } = state;
     const { role } = userInfo;
+    const { data: session, status } = useSession();
+
+    useEffect(() => {
+        if (status === "authenticated") {
+            setLoaded(true);
+        } else if (status === "loading") {
+            setLoaded(false);
+        } else {
+            setLoaded(true);
+        }
+    }, [status]);
+
     return (
         <div className="flex items-center pl-44 max-xl:pl-0">
-            {!logged ? <NavigationBarGeneral /> : null}
-            {logged && role === "patient" ? <NavigationBarPatient /> : null}
-            {logged && role === "doctor" ? <NavigationBarDoctor /> : null}
+            {!loaded && (
+                <div className="animate-pulse bg-gray-100 h-14 w-56 rounded-2xl "></div>
+            )}
+            {loaded && (
+                <div>
+                    {!logged ? <NavigationBarGeneral /> : null}
+                    {logged && role === "patient" ? (
+                        <NavigationBarPatient />
+                    ) : null}
+                    {logged && role === "doctor" ? (
+                        <NavigationBarDoctor />
+                    ) : null}
+                </div>
+            )}
         </div>
     );
 };
