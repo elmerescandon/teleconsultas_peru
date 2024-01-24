58fff333d6a63f0c585c7dc8fd77866afe267f0c
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-23 23:06:49 -0500

    Remove unused import and update date format in PaymentReview component

--- src/components/Organisms/PaymentReview/PaymentReview.tsx
+++ src/components/Organisms/PaymentReview/PaymentReview.tsx
@@ -7,7 +7,6 @@ import {
     getAppointmentHours,
     stringToDate,
 } from "@/utils/functions/utils";
-import { Timestamp } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 const PaymentReview = () => {
@@ -50,12 +49,7 @@ const PaymentReview = () => {
                     name={summary.specialityName}
                 />
                 <PaymentItem label="Profesional" name={summary.doctorName} />
-                <PaymentItem
-                    label="Fecha"
-                    name={stringToDate(
-                        appointment.date as unknown as Timestamp
-                    )}
-                />
+                <PaymentItem label="Fecha" name={dateToSpanishISO(date)} />
                 <PaymentItem
                     label="Horario"
                     name={getAppointmentHours(startDate, endDate)}
