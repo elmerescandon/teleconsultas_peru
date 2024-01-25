eb18be58fafd1515e731fff138e39f169fd47fd1
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-25 03:34:53 -0500

    Update getAppointmentsFiltered function to include limit and orderBy clauses

--- src/firebase/Appointments/getAppointmentsFiltered.ts
+++ src/firebase/Appointments/getAppointmentsFiltered.ts
@@ -1,10 +1,10 @@
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
-import { Firestore, Timestamp, and, collection, getDocs, query, where } from "firebase/firestore";
+import { Firestore, Timestamp, and, collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
 import dbFirestore from "../config";
 import IDateRangeAppointment from "@/utils/Interfaces/IDateRangeAppointment";
 import getAppointments from "./getAppointments";
 
-const getAppointmentsFiltered = async (id: string, role: string, status: string[], date: IDateRangeAppointment , specialityId: string) => {
+const getAppointmentsFiltered = async (id: string, role: string, status: string[], date: IDateRangeAppointment, specialityId: string) => {
 
     if (date.init === null || date.finish === null) {
         return await getAppointments(id, role, status);
@@ -13,13 +13,13 @@ const getAppointmentsFiltered = async (id: string, role: string, status: string[
     const initDate = Timestamp.fromDate(new Date(date.init));
     const finishDate = Timestamp.fromDate(new Date(date.finish));
     const q = query(collection(dbFirestore, "appointments"), and(where(`${role === "patient" ? "patientId" : "doctorId"}`, "==", id),
-                                                                 where("status", "in", status), 
-                                                                //  where("specialityId", "==", specialityId),
-                                                                 where("date", ">=",initDate), 
-                                                                 where("date", "<=", finishDate)
-                                                                 ));
-                                                                 
-    
+        where("status", "in", status),
+        //  where("specialityId", "==", specialityId),
+        where("date", ">=", initDate),
+        where("date", "<=", finishDate)
+    ), limit(10));
+
+
     const snapShot = await getDocs(q);
     const appointments: IAppointment[] = [];
     if (snapShot.empty) {
@@ -30,7 +30,7 @@ const getAppointmentsFiltered = async (id: string, role: string, status: string[
         appointments.push(appointment);
     });
     return appointments;
-    
+
 
 
 }
