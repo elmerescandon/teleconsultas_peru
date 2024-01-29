48355fa7b750ee9db8a9138723fdc99f68bbe8eb
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-29 18:04:03 -0500

    Add cmpId field to IUser interface

--- src/utils/Interfaces/dataModel/IUser.ts
+++ src/utils/Interfaces/dataModel/IUser.ts
@@ -1,53 +1,54 @@
 interface IUser {
-    _id: string;
-    role: "patient" | "doctor";
-    name: string;
-    lastName: string;
-    email: string;
-    password: string;
-    termsAndConditions?: boolean;
-    phone: string;
-    id: string;
+  _id: string;
+  role: "patient" | "doctor";
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  termsAndConditions?: boolean;
+  phone: string;
+  id: string;
 
-    //  Location
-    region?: string;
-    province?: string;
-    district?: string;
-    address?: string;
-    reference?: string;
-    interiorNumber?: string;
+  //  Location
+  region?: string;
+  province?: string;
+  district?: string;
+  address?: string;
+  reference?: string;
+  interiorNumber?: string;
 
-    // Info
-    age?: string;
-    sex?: string;
-    height?: string;
-    weight?: string;
-    bornDate?: string;
+  // Info
+  age?: string;
+  sex?: string;
+  height?: string;
+  weight?: string;
+  bornDate?: string;
 
-    profile_picture?: string;
+  profile_picture?: string;
 
-    // Patient fields
-    allergies?: string[];
-    medicalHistory?: string[];
+  // Patient fields
+  allergies?: string[];
+  medicalHistory?: string[];
 
-    // Doctor fields
+  // Doctor fields
 
-    specialities?: string[];
-    studies?: string[];
-    curriculum?: string;
-    cmpNumber?: string;
-    cmpPicture?: string;
-    availableHours?: {
-      date: string,
-      hours: string[]
-    }[];
+  specialities?: string[];
+  studies?: string[];
+  curriculum?: string;
+  cmpNumber?: string;
+  cmpId?: string;
+  cmpPicture?: string;
+  availableHours?: {
+    date: string,
+    hours: string[]
+  }[];
 
-    other_fields?: any;
-    reserved_appointments?: {
-        date: string, appointments: string[]; 
-      }[];
-    active_prescriptions?: string[];
-    ingresos?: {date: string, ingreso: string}[];
+  other_fields?: any;
+  reserved_appointments?: {
+    date: string, appointments: string[];
+  }[];
+  active_prescriptions?: string[];
+  ingresos?: { date: string, ingreso: string }[];
 }
 
 export default IUser;
