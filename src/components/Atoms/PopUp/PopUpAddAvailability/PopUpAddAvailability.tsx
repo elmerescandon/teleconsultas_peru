b6c211533388d0b9c241c54856e2b9aa89d23b74
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-29 19:23:29 -0500

    Add ExclamationCircleIcon and informational message to PopUpAddAvailability component

--- src/components/Atoms/PopUp/PopUpAddAvailability/PopUpAddAvailability.tsx
+++ src/components/Atoms/PopUp/PopUpAddAvailability/PopUpAddAvailability.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import AddAvailabilityGeneral from "@/components/Molecules/AddAvailabilityGeneral/AddAvailabilityGeneral";
+import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
 
 type PopUpAddAvailabilityProps = {
     onClose: () => void;
@@ -25,13 +26,17 @@ const PopUpAddAvailability = ({
                         className="text-3xl font-semibold
                                     max-lg:text-2xl"
                     >
-                        Añadir disponibilidad
+                        Agregar/Modificar disponibilidad
                     </h2>
                     <button onClick={onClose}>
                         <XMarkIcon className="w-10 h-10 ml-auto" />
                     </button>
                 </div>
 
+                <div className="flex items-center justify-center flex-wrap gap-5 bg-brand-50 p-3 rounded-2xl">
+                    <ExclamationCircleIcon className="w-10 h-10 text-brand-900" />
+                    <p className="font-semibold text-base italic">Si deseas modificar algún horario existente, crea un nuevo horario indicando la misma fecha.</p>
+                </div>
                 <AddAvailabilityGeneral
                     doctorId={doctorId}
                     specialityId={specialityId}
