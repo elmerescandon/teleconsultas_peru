1bb542b515c8ec4f22de393706b89828d24981aa
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-01-29 19:23:50 -0500

    Add date and order by date in getDoctorAppointments

--- src/firebase/Appointments/getDoctorAppointments.ts
+++ src/firebase/Appointments/getDoctorAppointments.ts
@@ -1,9 +1,16 @@
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
-import { and, collection, getDocs, query, where } from "firebase/firestore";
+import { Timestamp, and, collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import dbFirestore from "../config";
 
 const getDoctorAppointments = async (doctorId: string, status: string[]) => {
-    const q = query(collection(dbFirestore, "appointments"), and(where("doctorId", "==", doctorId),where("status", "in", status)));
+    const q = query(collection(dbFirestore, "appointments"),
+        and(where("doctorId", "==", doctorId),
+            where("status", "in", status),
+            where("date", ">=", Timestamp.fromDate(new Date(Date.now() - 86400000)))),
+        orderBy("date", "asc"));
+
+
+
     const snapShot = await getDocs(q);
 
     const appointments: IAppointment[] = [];
@@ -15,7 +22,7 @@ const getDoctorAppointments = async (doctorId: string, status: string[]) => {
         appointments.push(appointment);
     });
     return appointments;
-    
+
 
 
 }
