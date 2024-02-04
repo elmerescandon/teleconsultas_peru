0b2beb9521ef0962c5a6f06ae94b40c6565082bc
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-04 17:52:30 -0500

    Refactor DoctorAppointmentFilter component to include patient select options

--- src/components/Organisms/DoctorAppointmentFilter/DoctorAppointmentFilter.tsx
+++ src/components/Organisms/DoctorAppointmentFilter/DoctorAppointmentFilter.tsx
@@ -1,48 +1,75 @@
 "use client";
-import InputSearch from "@/components/Atoms/Inputs/InputSearch/InputSearch";
-import InputSelect from "@/components/Atoms/Inputs/InputSelect/InputSelect";
 import DateFilter from "@/components/Molecules/DateFilter/DateFilter";
+import { getUsersFromArray } from "@/firebase/User/getUserFromArray";
 import IAppointmentFilter from "@/utils/Interfaces/IAppointmentFilter";
-import { getSpecialitiesOptions } from "@/utils/functions/utils";
-import specialitiesMockup from "@/utils/mockups/specialitiesMockup";
-import { useEffect } from "react";
+import ISelectOptions from "@/utils/Interfaces/ISelectOptions";
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import { defaultOptions } from "@/utils/constants/constants";
+import { useEffect, useState } from "react";
+import Select from "react-select";
 
 type DoctorAppointmentFilterProps = {
+    appointments: IAppointment[];
     filter: IAppointmentFilter;
     setFilter: (filter: IAppointmentFilter) => void;
 };
 
 const DoctorAppointmentFilter = ({
+    appointments,
     filter,
     setFilter,
 }: DoctorAppointmentFilterProps) => {
-    const specialitiesOptions = getSpecialitiesOptions(specialitiesMockup);
     const { date, patientName, speciality } = filter;
+    const [patientOptions, setPatientOptions] = useState<ISelectOptions[]>([]);
+
+    useEffect(() => {
+        const getUsersFromAppointments = async (appointments: IAppointment[]) => {
+            try {
+                const appointmentsUsers = appointments.map((appointment) => appointment.patientId);
+                const uniqueUsers = Array.from(new Set(appointmentsUsers));
+                const users = await getUsersFromArray(uniqueUsers);
+                if (users.length === 0) {
+                    throw new Error("No se encontraron usuarios");
+                }
+                const newUserOptions = users.map((user) => ({ label: `${user.name} ${user.lastName}`, value: user._id }));
+                setPatientOptions(newUserOptions);
+            } catch (error) {
+                console.error(error);
+            }
+
+        }
+        if (appointments.length === 0) return;
+        getUsersFromAppointments(appointments);
+    }, [appointments]);
 
     return (
         <div className="flex gap-14 justify-between py-5">
             <div className="flex gap-5">
-                {/* <InputSelect
-                    onChange={(e: string) => {
-                        setFilter({ ...filter, speciality: e });
-                    }}
-                    placeholder="Escoge tu especialidad"
-                    selectId="doctor-speciality"
-                    options={specialitiesOptions}
-                /> */}
                 <DateFilter
                     date={date}
                     setDate={(date) => {
                         setFilter({ ...filter, date: date });
                     }}
                 />
+                <Select
+                    id="patients-appointments"
+                    placeholder={'Selecciona al paciente'}
+                    styles={{
+                        control: (baseStyles, state) => ({
+                            ...baseStyles,
+                            width: "259px",
+                            height: "56px",
+                        }),
+                    }}
+                    options={patientOptions}
+                    isSearchable
+                    defaultValue={defaultOptions[0]}
+                    isClearable
+                    onChange={(option) => {
+                        setFilter({ ...filter, patientName: option?.value || "" });
+                    }}
+                />
             </div>
-            {/* <InputSearch
-                placeholder="Busca a un paciente"
-                onInputChange={(e: string) => {
-                    setFilter({ ...filter, patientName: e });
-                }}
-            /> */}
         </div>
     );
 };
