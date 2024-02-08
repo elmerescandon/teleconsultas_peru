b4c08619031e840739c24eaa56d9c97d37a1fee8
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-08 15:38:40 -0500

    Refactor getAppointmentId function

--- src/firebase/Appointments/getAppointmentId.ts
+++ src/firebase/Appointments/getAppointmentId.ts
@@ -4,13 +4,7 @@ import { readAppointmentSize } from "./readAppointmentSize";
 const getAppointmentId = async (appointment: IAppointment) => {
     const sizeAppointment = await readAppointmentSize();
     const userId = sizeAppointment === undefined ? 1 : sizeAppointment + 1;
-    const dateParts = appointment.date.split("-");
-    const newDate = new Date(
-        parseInt(dateParts[0]),
-        parseInt(dateParts[1]) - 1,
-        parseInt(dateParts[2])
-    );
-    const newAppointment = { ...appointment, _id: `appointment${userId}`, date: newDate }
+    const newAppointment = { ...appointment, _id: `appointment${userId}` }
     return newAppointment;
 }
 
