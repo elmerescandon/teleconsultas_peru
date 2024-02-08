257f82aeba0ba632dcf4608b7fba9b78f54c3f68
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-08 12:07:53 -0500

    Refactor getAppointmentId to parse appointment date correctly

--- src/firebase/Appointments/getAppointmentId.ts
+++ src/firebase/Appointments/getAppointmentId.ts
@@ -4,7 +4,13 @@ import { readAppointmentSize } from "./readAppointmentSize";
 const getAppointmentId = async (appointment: IAppointment) => {
     const sizeAppointment = await readAppointmentSize();
     const userId = sizeAppointment === undefined ? 1 : sizeAppointment + 1;
-    const newAppointment = {...appointment, _id: `appointment${userId}`, date: new Date(appointment.date)}
+    const dateParts = appointment.date.split("-");
+    const newDate = new Date(
+        parseInt(dateParts[0]),
+        parseInt(dateParts[1]) - 1,
+        parseInt(dateParts[2])
+    );
+    const newAppointment = { ...appointment, _id: `appointment${userId}`, date: newDate }
     return newAppointment;
 }
 
