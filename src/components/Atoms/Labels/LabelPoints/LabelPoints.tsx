ae7c8491061d2706e53955319d1cb3b63085fb27
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-08 11:43:26 -0500

    Refactor LabelPoints component styling

--- src/components/Atoms/Labels/LabelPoints/LabelPoints.tsx
+++ src/components/Atoms/Labels/LabelPoints/LabelPoints.tsx
@@ -10,12 +10,12 @@ const LabelPoints = ({ label, points }: LabelPointsProps) => {
         <div className="flex flex-col justify-start items-center gap-5 py-5">
             <p className="text-lg text-gray-500 w-full">{label}</p>
             <ul
-                className="flex w-full pl-24
+                className="flex w-full first-letter:pl-4 gap-2 justify-start
                             max-xl:pl-5"
             >
                 {points.map((point, index) => {
                     return (
-                        <li className="text-lg  w-full list-disc" key={index}>
+                        <li className="text-lg list-none rounded-lg py-2 px-4 bg-brand-200" key={index}>
                             {point}
                         </li>
                     );
