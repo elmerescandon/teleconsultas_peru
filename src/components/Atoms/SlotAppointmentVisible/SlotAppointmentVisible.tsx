e23cfd2e3486f9b90598deea4f81332d11ce9108
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-20 14:37:44 -0500

    :sparkles: Add slot appointment button delete functionality

--- src/components/Atoms/SlotAppointmentVisible/SlotAppointmentVisible.tsx
+++ src/components/Atoms/SlotAppointmentVisible/SlotAppointmentVisible.tsx
@@ -1,33 +1,69 @@
+import LoadingHorizontal from "@/components/Molecules/Loaders/LoadingHorizontal/LoadingHorizontal";
+import eliminateAvailability from "@/firebase/Availability/eliminateAvailability";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
-import React from "react";
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
 
 type SlotAppointmentVisibleProps = {
     availableAppointment: IAvailableAppointment;
+    doctorId: string,
+    specialityId: string,
+    date: string;
 };
 
 const SlotAppointmentVisible = ({
     availableAppointment,
+    doctorId,
+    specialityId,
+    date,
 }: SlotAppointmentVisibleProps) => {
+
+    const [visible, setVisible] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
     const { startDate, endDate, available } = availableAppointment;
+
+
+
+    const handleDelete = () => {
+        try {
+            setLoading(true);
+            eliminateAvailability(date, specialityId, doctorId, startDate, endDate);
+            setVisible(false);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <button
-            disabled={!available}
-            className={`
+
+        <div className={`relative ${visible ? "" : 'hidden'} `}>
+            <button className="absolute left-[90%] -top-2" onClick={handleDelete}>
+                <XCircleIcon className="w-6 h-6 rounded-full bg-brand-900 text-basic-white
+                                        hover:bg-brand-800 hover:text-brand-100
+                                        active:bg-brand-50 active:text-brand-800
+                " />
+            </button>
+            <button
+                disabled={!available}
+                className={`
                 px-3 py-2 rounded-md text-center text-xs font-semibold
-                ${
-                    available
+                ${available
                         ? "bg-brand-600 text-basic-white "
                         : "bg-neutral-50 text-neutral-400 border-0"
-                }
+                    }
                 `}
-            onClick={() => {}}
-        >
-            {`${new Date(startDate)
-                .toLocaleTimeString()
-                .replace(/:\d+ /, " ")} - ${new Date(endDate)
-                .toLocaleTimeString()
-                .replace(/:\d+ /, " ")} `}
-        </button>
+                onClick={() => { }}
+            >
+                {`${new Date(startDate)
+                    .toLocaleTimeString()
+                    .replace(/:\d+ /, " ")} - ${new Date(endDate)
+                        .toLocaleTimeString()
+                        .replace(/:\d+ /, " ")} `}
+            </button>
+            {loading && <LoadingHorizontal />}
+        </div>
+
     );
 };
 
