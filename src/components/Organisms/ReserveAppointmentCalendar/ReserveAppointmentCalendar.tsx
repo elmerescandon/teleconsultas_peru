7d29b3e4c3fe43ca54a0a4293ba47fe038594d47
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-26 17:30:44 -0500

    :sparkrles: Refactor calendar appointment numbers

--- src/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar.tsx
+++ src/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar.tsx
@@ -1,10 +1,26 @@
 "use client";
-import { useAppointmentDispatch } from "@/utils/context/AppointmentContext/AppointmentContext";
+import { ServerDay } from "@/components/Atoms/Days/ServerDay";
+import LoadingCircle from "@/components/Molecules/Loaders/LoadingCircle/LoadingCircle";
+import LoadingHorizontal from "@/components/Molecules/Loaders/LoadingHorizontal/LoadingHorizontal";
+import { getAvailableDays } from "@/firebase/Availability/geAvailableDays";
+import { useAppointment, useAppointmentDispatch } from "@/utils/context/AppointmentContext/AppointmentContext";
+import { dateValuesToDates } from "@/utils/functions/utilsDate";
 import { DateCalendar } from "@mui/x-date-pickers";
-import React, { useState } from "react";
+import { set } from "lodash";
+import React, { useEffect, useState } from "react";
+import { LoadingIndicator } from "react-select/dist/declarations/src/components/indicators";
 
-const ReserveAppointmentCalendar = ({}) => {
+
+const ReserveAppointmentCalendar = () => {
+
+    const appointment = useAppointment();
+    const { doctorId, specialityId } = appointment;
+    const dispatch = useAppointmentDispatch();
+
     const [dateJS, setDateJS] = useState<IDateJS | null>(null);
+    const [highlightedDays, setHighlightedDays] = useState<string[]>([]);
+    const [error, setError] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
 
     const onChangeDate = (date: IDateJS | null) => {
         if (!date) return;
@@ -16,7 +32,31 @@ const ReserveAppointmentCalendar = ({}) => {
         });
     };
 
-    const dispatch = useAppointmentDispatch();
+    useEffect(() => {
+        const getHighlightedDays = async (doctorId: string, specialityId: string) => {
+            try {
+                setLoading(true);
+                const highlightedDays = await getAvailableDays(doctorId, specialityId);
+                if (highlightedDays === null) throw new Error("No hay días disponibles para este doctor");
+                setHighlightedDays(dateValuesToDates(highlightedDays.dateArray));
+                setError("");
+                setLoading(false);
+            } catch (error) {
+                setHighlightedDays([]);
+                setError((error as Error).message);
+                setLoading(false);
+            }
+        }
+
+        if (doctorId && specialityId) {
+            getHighlightedDays(doctorId, specialityId);
+        } else {
+            setHighlightedDays([]);
+            setDateJS(null);
+            setError("");
+        }
+    }, [doctorId, specialityId]);
+
     return (
         <div className="px-5 w-auto">
             <div className="text-xl font-semibold py-4">
@@ -28,7 +68,17 @@ const ReserveAppointmentCalendar = ({}) => {
                 }}
                 value={dateJS}
                 onChange={onChangeDate}
+                slots={{
+                    day: ServerDay,
+                }}
+                slotProps={{
+                    day: {
+                        highlightedDays
+                    } as any,
+                }}
             />
+            {error && <p className="text-brand-600 font-semibold -mt-8 text-center" >{error}</p>}
+            {loading && <LoadingHorizontal />}
         </div>
     );
 };
