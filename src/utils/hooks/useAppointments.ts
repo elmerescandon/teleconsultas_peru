daa4388b93b2b4b545e413458f6a61a4ba43cc09
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-27 11:43:15 -0500

    :sparkles: use appointment

--- src/utils/hooks/useAppointments.ts
+++ src/utils/hooks/useAppointments.ts
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 import IAppointment from "../Interfaces/reducers/IAppointment";
 import IDateRangeAppointment from "../Interfaces/IDateRangeAppointment";
 import IUserInfo from "@/redux/state-interfaces/User/IUserInfo";
-import getUserAppointments from "@/firebase/Appointments/getUserAppointments";
 import getAppointmentsFiltered from "@/firebase/Appointments/getAppointmentsFiltered";
 
-const useAppointments = (date : IDateRangeAppointment, user: IUserInfo, specialityId : string ,setSelectedAppointment: (value: string) => void ) : IAppointment[] => {
+const useAppointments = (date: IDateRangeAppointment, user: IUserInfo, specialityId: string, setSelectedAppointment: (value: string) => void): IAppointment[] => {
 
     const [appointments, setAppointments] = useState<IAppointment[]>([]);
 
     const getAppointments = async () => {
         let appointments: IAppointment[] = [];
-        appointments = await getAppointmentsFiltered(user._id, user.role, ["scheduled","pending"], date, specialityId);
+        appointments = await getAppointmentsFiltered(user._id, user.role, ["scheduled", "pending"], date, specialityId);
         setAppointments(appointments);
     };
 
