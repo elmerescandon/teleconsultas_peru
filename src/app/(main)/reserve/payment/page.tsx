94cbe659f9e9c23f287442ce893f7dff9c134c93
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-27 12:22:38 -0500

    :fire: Remove PaymentAppointmentSection component

--- src/app/(main)/reserve/payment/page.tsx
+++ src/app/(main)/reserve/payment/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-import PaymentAppointmentSection from "@/components/Templates/PaymentAppointmentSection/PaymentAppointmentSection";
 import PaymentAppointmentSection2 from "@/components/Templates/PaymentAppointmentSection/PaymentAppointmentSection2";
 
 const page = () => {
     return (
         <div>
-            {/* <PaymentAppointmentSection /> */}
             <PaymentAppointmentSection2 />
         </div>
     );
