d2890c0a3e1130bb30da87fc92aaec79631f3af3
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-27 12:24:01 -0500

    :sparkles: Update getAvailableDates function signature

--- src/firebase/Availability/getAvailableDates.ts
+++ src/firebase/Availability/getAvailableDates.ts
@@ -1,9 +1,10 @@
 import { and, collection, getDocs, query, where } from "firebase/firestore";
 import dbFirestore from "../config";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
+import { DateValue } from "@/utils/alias/alias";
 
 
-export const getAvailableDates = async (date: string, doctorId : string, specialityId: string) => {
+export const getAvailableDates = async (date: DateValue, doctorId: string, specialityId: string) => {
     const q = query(collection(dbFirestore, "availability"), and(where("doctor_id", "==", doctorId), where("speciality_id", "==", specialityId)));
     const snapShot = await getDocs(q);
 
