5c033704b866ac144ac65b8a3391fc9a316bc3b9
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-27 12:22:57 -0500

    :sparkles: Refactor ReserveAppointmentHours component to fetch available slots based on doctorId, specialityId, and date

--- src/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours.tsx
+++ src/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours.tsx
@@ -1,20 +1,51 @@
 import SlotAppointment from "@/components/Atoms/SlotAppointment/SlotAppointment";
 import Loading from "@/components/Molecules/Loaders/Loading/Loading";
+import { getAvailableDates } from "@/firebase/Availability/getAvailableDates";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
+import { DateValue } from "@/utils/alias/alias";
+import { useAppointment, useAppointmentDispatch } from "@/utils/context/AppointmentContext/AppointmentContext";
 import React, { useEffect, useState } from "react";
 
-type ReserveAppointmentHoursProps = {
-    availableAppointments: IAvailableAppointment[];
-    loading: boolean;
-};
 
-const ReserveAppointmentHours = ({
-    availableAppointments,
-    loading,
-}: ReserveAppointmentHoursProps) => {
+const ReserveAppointmentHours = () => {
+
+    const appointment = useAppointment();
+    const { doctorId, specialityId, date } = appointment;
+
+    const [availableSlots, setAvailableSlots] = useState<IAvailableAppointment[]>([]);
+
+
     const [currentId, setCurrentId] = useState(-1);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string>("");
 
-    const slots = availableAppointments.map((appointment, index) => (
+
+
+    useEffect(() => {
+        const getAvailableSlots = async (date: DateValue, doctorId: string, specialityId: string) => {
+            try {
+                setLoading(true);
+                const availableSlots = await getAvailableDates(date, doctorId, specialityId);
+                if (availableSlots === null) throw new Error("No hay horarios disponibles");
+                setAvailableSlots(availableSlots);
+                setError("");
+                setLoading(false);
+            } catch (error) {
+                setAvailableSlots([]);
+                setError((error as Error).message);
+                setLoading(false);
+            }
+        }
+
+        if (doctorId && specialityId && date) {
+            getAvailableSlots(date, doctorId, specialityId);
+        } else {
+            setAvailableSlots([]);
+            setCurrentId(-1);
+        }
+    }, [doctorId, specialityId, date]);
+
+    const slots = availableSlots.map((appointment, index) => (
         <SlotAppointment
             key={index}
             availableAppointment={appointment}
@@ -26,10 +57,6 @@ const ReserveAppointmentHours = ({
         />
     ));
 
-    useEffect(() => {
-        setCurrentId(-1);
-    }, [availableAppointments]);
-
     return (
         <div className="px-5">
             <div className="flex items-center gap-5">
@@ -38,7 +65,7 @@ const ReserveAppointmentHours = ({
                 </div>
                 {loading && <Loading size={5} />}
             </div>
-
+            {error && <p className="italic">{error}</p>}
             {!loading && (
                 <div className="flex flex-wrap gap-2 justify-start max-xl:justify-start max-md:justify-center">
                     {slots.length > 0 ? (
