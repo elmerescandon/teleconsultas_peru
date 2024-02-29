5699ca9e5820cb2471c855f4ca9ea58188a8dcca
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-29 20:22:13 +0100

    :bug: Correct timezone comparison between days

--- src/utils/functions/utils.ts
+++ src/utils/functions/utils.ts
@@ -3,12 +3,16 @@ import IAvailableAppointment from "../Interfaces/IAvailableAppointment";
 import Ispeciality from "../Interfaces/dataModel/ISpeciality";
 import IUser from "../Interfaces/dataModel/IUser";
 import IAppointment from "../Interfaces/reducers/IAppointment";
-import { parse } from "date-fns";
+import {parse} from "date-fns";
 import ISelectOptions from "../Interfaces/ISelectOptions";
 import dayjs from "dayjs";
-import { Timestamp } from "firebase/firestore";
-import { changeTimezone, setDateToTimezoneConstant, setDateToTimezoneConstantWithTime } from "./utilsDate";
-import { DateValue } from "../alias/alias";
+import {Timestamp} from "firebase/firestore";
+import {
+  changeTimezone,
+  setDateToTimezoneConstant,
+  setDateToTimezoneConstantWithTime,
+} from "./utilsDate";
+import {DateValue} from "../alias/alias";
 
 // function from specialitiesMokcup to select options
 export const getSpecialitiesOptions = (specialities: Ispeciality[]) => {
@@ -32,46 +36,82 @@ export const getDoctorsOptions = (doctors: IUser[]) => {
   return options;
 };
 
-
 export const validateAppointment = (appointment: IAppointment) => {
-  const { specialityId, doctorId, reason, date, startDate, endDate, patientId, _id, status } = appointment;
-  if (!specialityId || !doctorId || !reason || !date || !startDate || !endDate || !patientId || !_id) return false;
-  if (status !== 'pending' && status !== "doctor-canceled") return false;
+  const {
+    specialityId,
+    doctorId,
+    reason,
+    date,
+    startDate,
+    endDate,
+    patientId,
+    _id,
+    status,
+  } = appointment;
+  if (
+    !specialityId ||
+    !doctorId ||
+    !reason ||
+    !date ||
+    !startDate ||
+    !endDate ||
+    !patientId ||
+    !_id
+  )
+    return false;
+  if (status !== "pending" && status !== "doctor-canceled") return false;
   return true;
-}
+};
 
 export const validateReservation = (appointment: IAppointment) => {
-  const { specialityId, doctorId, reason, date, startDate, endDate, patientId, _id, status } = appointment;
-  if (!specialityId || !doctorId || !reason || !date || !startDate || !endDate) return false;
+  const {
+    specialityId,
+    doctorId,
+    reason,
+    date,
+    startDate,
+    endDate,
+    patientId,
+    _id,
+    status,
+  } = appointment;
+  if (!specialityId || !doctorId || !reason || !date || !startDate || !endDate)
+    return false;
   return true;
-}
+};
 
 export const stringToDate = (date: Timestamp) => {
+  const newDate = date.toDate().toISOString().split("T")[0];
 
-  const newDate = date.toDate()
-    .toISOString()
-    .split("T")[0]
-
-  const spanishDate = parse(newDate, 'yyyy-MM-dd', new Date()).toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  const spanishDate = parse(
+    newDate,
+    "yyyy-MM-dd",
+    new Date()
+  ).toLocaleDateString("es-ES", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return spanishDate.charAt(0).toUpperCase() + spanishDate.slice(1);
-}
+};
 
 export const statusToSpanish = (status: string) => {
   switch (status) {
-    case 'scheduled':
-      return 'Agendada';
-    case 'completed':
-      return 'Aceptada';
-    case 'canceled':
-      return 'Rechazada';
-    case 'pending':
-      return 'Pendiente';
-    case 'doctor-canceled':
-      return 'Cancelada por el doctor';
+    case "scheduled":
+      return "Agendada";
+    case "completed":
+      return "Aceptada";
+    case "canceled":
+      return "Rechazada";
+    case "pending":
+      return "Pendiente";
+    case "doctor-canceled":
+      return "Cancelada por el doctor";
     default:
-      return 'Pendiente';
+      return "Pendiente";
   }
-}
+};
 
 export const getAppointmentHours = (startDate: string, endDate: string) => {
   if (startDate === "" || endDate === "") return "";
@@ -79,34 +119,38 @@ export const getAppointmentHours = (startDate: string, endDate: string) => {
   return `${new Date(startDate)
     .toLocaleTimeString()
     .replace(/:\d+ /, " ")} - ${new Date(endDate)
-      .toLocaleTimeString()
-      .replace(/:\d+ /, " ")} `;
-}
+    .toLocaleTimeString()
+    .replace(/:\d+ /, " ")} `;
+};
 
-export const getSpecialityName = (specialities: Ispeciality[], specialityId: string) => {
-  const speciality = specialities.filter((speciality) => speciality._id === specialityId);
-  if (speciality.length === 0) return '';
+export const getSpecialityName = (
+  specialities: Ispeciality[],
+  specialityId: string
+) => {
+  const speciality = specialities.filter(
+    (speciality) => speciality._id === specialityId
+  );
+  if (speciality.length === 0) return "";
   return speciality[0].name;
-}
-
+};
 
 export const getDoctorNameMockup = (doctors: IUser[], doctorId: string) => {
   const doctor = doctors.filter((doctor) => doctor._id === doctorId);
-  if (doctor.length === 0) return '';
+  if (doctor.length === 0) return "";
   return doctor[0].name;
 };
 
 export const getPatientName = (patients: IUser[], patientId: string) => {
   const patient = patients.filter((patient) => patient._id === patientId);
-  if (patient.length === 0) return '';
+  if (patient.length === 0) return "";
   return patient[0].name;
-}
+};
 
 export const getReasonName = (reasons: ISelectOptions[], reasonId: string) => {
   const reason = reasons.filter((reason) => reason.value === reasonId);
-  if (reason.length === 0) return '';
+  if (reason.length === 0) return "";
   return reason[0].label;
-}
+};
 
 export const areDatesEqual = (dateA: Date, dateB: Date) => {
   return (
@@ -114,22 +158,28 @@ export const areDatesEqual = (dateA: Date, dateB: Date) => {
     dateA.getMonth() === dateB.getMonth() &&
     dateA.getDate() === dateB.getDate()
   );
-}
+};
 
-export const createAppointment = (appointment: IAppointment, patient: IUserInfo) => {
-  const { _id } = patient;
-  const { date } = appointment;
+export const createAppointment = (
+  appointment: IAppointment,
+  patient: IUserInfo
+) => {
+  const {_id} = patient;
+  const {date} = appointment;
   const newAppointment: IAppointment = {
     ...appointment,
     patientId: _id,
-    status: 'pending',
+    status: "pending",
     price: 80,
-  }
+  };
   return newAppointment;
+};
 
-}
-
-export const createAvailabilitiesSlots = (date: string, startTime: string, endTime: string) => {
+export const createAvailabilitiesSlots = (
+  date: string,
+  startTime: string,
+  endTime: string
+) => {
   const dates: IAvailableAppointment[] = [];
   const start = dayjs(date)
     .hour(parseInt(startTime.split(":")[0]))
@@ -140,7 +190,9 @@ export const createAvailabilitiesSlots = (date: string, startTime: string, endTi
 
   for (let i = start; i.isBefore(end); i = i.add(30, "minute")) {
     let startDate = setDateToTimezoneConstantWithTime(new Date(i.toDate()));
-    let endDate = setDateToTimezoneConstantWithTime(new Date(i.add(30, "minute").toDate()));
+    let endDate = setDateToTimezoneConstantWithTime(
+      new Date(i.add(30, "minute").toDate())
+    );
     dates.push({
       available: true,
       startDate,
@@ -150,38 +202,53 @@ export const createAvailabilitiesSlots = (date: string, startTime: string, endTi
   return dates;
 };
 
-
 export const dateToSpanishISO = (dateString: string) => {
-  const date = new Date(dateString.replace(/-/g, '\/'));
-  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('es-ES', options);
-}
+  const date = new Date(dateString.replace(/-/g, "/"));
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("es-ES", options);
+};
 
-
-export const getWeekdaysDatesBetween = (date: string, endDate: string, interval: 'daily' | 'weekly' | 'monthly'): Date[] => {
+export const getWeekdaysDatesBetween = (
+  date: string,
+  endDate: string,
+  interval: "daily" | "weekly" | "monthly"
+): Date[] => {
   const dates = [];
   const current = new Date(date);
   const lastDate = new Date(endDate);
 
   let increment;
   switch (interval) {
-    case 'daily':
+    case "daily":
       increment = 1;
       break;
-    case 'weekly':
+    case "weekly":
       increment = 7;
       break;
-    case 'monthly':
+    case "monthly":
       increment = 30; // Approximation for a month
       break;
     default:
-      throw new Error('Invalid interval. Supported intervals are "daily", "weekly", and "monthly".');
+      throw new Error(
+        'Invalid interval. Supported intervals are "daily", "weekly", and "monthly".'
+      );
   }
 
   // Generate an array of dates between date and endDate with the specified interval
-  for (let i = current.getTime(); i <= lastDate.getTime(); i += increment * 24 * 60 * 60 * 1000) {
+  for (
+    let i = current.getTime();
+    i <= lastDate.getTime();
+    i += increment * 24 * 60 * 60 * 1000
+  ) {
     const currentDate = new Date(i);
-    if (interval === 'daily' || (currentDate.getDay() > 0 && currentDate.getDay() < 6)) {
+    if (
+      interval === "daily" ||
+      (currentDate.getDay() > 0 && currentDate.getDay() < 6)
+    ) {
       dates.push(currentDate);
     }
   }
@@ -191,15 +258,16 @@ export const getWeekdaysDatesBetween = (date: string, endDate: string, interval:
 
 export const formatDate = (date: Date): string => {
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(date.getDate()).padStart(2, "0");
 
   return `${year}-${month}-${day}`;
-}
+};
 
-export const dateToHours = (startDate: string, endDate: string) => `${new Date(startDate)
-  .toLocaleTimeString()
-  .replace(/:\d+ /, " ")} - ${new Date(endDate)
+export const dateToHours = (startDate: string, endDate: string) =>
+  `${new Date(startDate)
+    .toLocaleTimeString()
+    .replace(/:\d+ /, " ")} - ${new Date(endDate)
     .toLocaleTimeString()
     .replace(/:\d+ /, " ")} `;
 
@@ -207,8 +275,10 @@ export const getHourFromDate = (dateString: string): string => {
   const date = new Date(dateString);
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-}
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+};
 
 // compare date in a Firebase Timestamp format with a Date format for today considering the date is in LIMA timezone, return true if the difference is more than 24 hours
 export const isDateOlderThan24Hours = (date: Timestamp): boolean => {
@@ -216,11 +286,18 @@ export const isDateOlderThan24Hours = (date: Timestamp): boolean => {
   const dateToCompare = date.toDate();
   const difference = dateNow.getTime() - dateToCompare.getTime();
   return 24 * 60 * 60 * 1000 > difference;
-}
+};
 
 export const isDateOlderThan24HoursFromNow = (date: DateValue): boolean => {
   const dateNow = new Date(new Date().setHours(0, 0, 0, 0));
-  const dateToCompare = date instanceof Date ? date : date.toDate();
+  const dateToCompare =
+    date instanceof Date
+      ? new Date(date.setHours(0, 0, 0, 0))
+      : new Date(date.toDate().setHours(0, 0, 0, 0));
   const difference = dateToCompare.getTime() - dateNow.getTime();
+  console.log(dateNow);
+  console.log(dateToCompare);
+  console.log(difference);
+  console.log(24 * 60 * 60 * 1000);
   return difference > 24 * 60 * 60 * 1000;
-}
+};
