eb4fba8090287ffb3e5f306e0c495b4976043bd6
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-29 12:46:26 -0500

    :sparkles: Add zoom fetch

--- src/firebase/Appointments/createNewAppointment.ts
+++ src/firebase/Appointments/createNewAppointment.ts
@@ -1,32 +1,35 @@
-import { collection, addDoc } from 'firebase/firestore';
-import dbFirestore from '../config';
-import IAppointment from '@/utils/Interfaces/reducers/IAppointment';
-import createZoomAppointment from '@/lib/zoom/createZoomAppointment';
-import updateAppointmentField from './updateAppointmentField';
-import setAvailabilityToSlot from '../Availability/setAvailabilitySlotsState';
+import {collection, addDoc} from "firebase/firestore";
+import dbFirestore from "../config";
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import createZoomAppointment from "@/lib/zoom/createZoomAppointment";
+import updateAppointmentField from "./updateAppointmentField";
+import setAvailabilityToSlot from "../Availability/setAvailabilitySlotsState";
 
 const createNewAppointment = async (appointment: IAppointment) => {
-    const { date, specialityId, doctorId, startDate, endDate, _id } = appointment;
-    try {
-        await addDoc(collection(dbFirestore, "appointments"), appointment);
-        if (startDate === null || endDate === null || date === null) throw new Error("No se pudo agendar la cita, inténtelo nuevamente luego.");
-        await setAvailabilityToSlot(
-            date,
-            specialityId,
-            doctorId,
-            false,
-            startDate,
-            endDate
-        );
-        // const joinURL = await createZoomAppointment(_id, startDate);
-        // console.log("hello")
-        // if (joinURL === null || joinURL === undefined) throw new Error("No se generó la cita en Zoom, inténtelo nuevamente luego");
-        // updateAppointmentField(_id, "joinURL", joinURL);
-    } catch (e) {
-        throw e;
-    }
-}
-
-
+  const {date, specialityId, doctorId, startDate, endDate, _id} = appointment;
+  try {
+    await addDoc(collection(dbFirestore, "appointments"), appointment);
+    if (startDate === null || endDate === null || date === null)
+      throw new Error(
+        "No se pudo agendar la cita, inténtelo nuevamente luego."
+      );
+    await setAvailabilityToSlot(
+      date,
+      specialityId,
+      doctorId,
+      false,
+      startDate,
+      endDate
+    );
+    const joinURL = await createZoomAppointment(_id, startDate);
+    if (joinURL === null || joinURL === undefined)
+      throw new Error(
+        "No se generó la cita en Zoom, inténtelo nuevamente luego"
+      );
+    updateAppointmentField(_id, "joinURL", joinURL);
+  } catch (e) {
+    throw e;
+  }
+};
 
 export default createNewAppointment;
