d7ff73a5c127f8601309391f9fe761649004ca20
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-29 14:16:18 -0500

    :art: Impove code

--- src/firebase/Availability/getAvailableDates.ts
+++ src/firebase/Availability/getAvailableDates.ts
@@ -1,26 +1,32 @@
-import { and, collection, getDocs, query, where } from "firebase/firestore";
+import {and, collection, getDocs, query, where} from "firebase/firestore";
 import dbFirestore from "../config";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
-import { DateValue } from "@/utils/alias/alias";
+import {DateValue} from "@/utils/alias/alias";
 
+export const getAvailableDates = async (
+  date: DateValue,
+  doctorId: string,
+  specialityId: string
+) => {
+  const q = query(
+    collection(dbFirestore, "availability"),
+    and(
+      where("doctor_id", "==", doctorId),
+      where("speciality_id", "==", specialityId)
+    )
+  );
+  const snapShot = await getDocs(q);
+  if (snapShot.empty) {
+    return null;
+  }
 
-export const getAvailableDates = async (date: DateValue, doctorId: string, specialityId: string) => {
-    const q = query(collection(dbFirestore, "availability"), and(where("doctor_id", "==", doctorId), where("speciality_id", "==", specialityId)));
-    const snapShot = await getDocs(q);
+  const docDate = snapShot.docs[0];
+  const dateCollection = collection(docDate.ref, "availability_slots");
+  const dateQuery = query(dateCollection, where("date", "==", date));
+  const dateDocs = await getDocs(dateQuery);
+  if (dateDocs.empty) {
+    return null;
+  }
 
-    if (snapShot.empty) {
-        return null;
-    }
-
-    const docDate = snapShot.docs[0];
-    const dateCollection = collection(docDate.ref, 'availability_slots');
-    const dateQuey = query(dateCollection, where("date", "==", date));
-    const dateDocs = await getDocs(dateQuey);
-
-    if (dateDocs.empty) {
-        return null;
-    }
-
-    return dateDocs.docs[0].data().slots as IAvailableAppointment[];
-
-}
+  return dateDocs.docs[0].data().slots as IAvailableAppointment[];
+};
