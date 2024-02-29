ba784d03685c3bc5b52eea8828ec8ee140e599ac
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-29 12:43:59 -0500

    :wrench: Modify environment configuration

--- src/app/api/zoom/auth/route.ts
+++ src/app/api/zoom/auth/route.ts
@@ -1,15 +1,22 @@
-import { zoomAuthURL } from "@/utils/constants/APIConstants"
-import { NextResponse } from "next/server";
+import {zoomAuthURL} from "@/utils/constants/APIConstants";
+import {NextResponse} from "next/server";
 
-export async function GET(){
-    const res = await fetch(`${zoomAuthURL}${process.env.ZOOM_ACCOUNT_ID}`,{
-        headers: {
-            'Authorization': 'Basic ' + Buffer.from(`${process.env.ZOOM_CLIENT_ID}:${process.env.ZOOM_CLIENT_SECRET}`).toString('base64'),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        method: 'POST',
-        cache: 'no-cache',
-    });
-    const data = await res.json();
-    return NextResponse.json({data});
+export async function GET() {
+  const res = await fetch(
+    `${zoomAuthURL}${process.env.NEXT_PUBLIC_ZOOM_ACCOUNT_ID}`,
+    {
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            `${process.env.NEXT_PUBLIC_ZOOM_CLIENT_ID}:${process.env.ZOOM_CLIENT_SECRET}`
+          ).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+      cache: "no-cache",
+    }
+  );
+  const data = await res.json();
+  return NextResponse.json({data});
 }
