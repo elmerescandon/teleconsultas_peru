e7125e523b497c27844fad50296b1f259ec46c3f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-04 16:48:09 -0500

    :sparkles: Refactor useAppointmentURLParams hook

--- src/utils/hooks/useAppointmentURLParams.ts
+++ src/utils/hooks/useAppointmentURLParams.ts
@@ -1,87 +1,111 @@
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useAppointment, useAppointmentDispatch } from "../context/AppointmentContext/AppointmentContext";
-import { useAppSelector } from "@/redux/hooks";
-import { IState } from "@/redux/store";
+import {useSearchParams} from "next/navigation";
+import {useEffect, useState} from "react";
+import {
+  useAppointment,
+  useAppointmentDispatch,
+} from "../context/AppointmentContext/AppointmentContext";
+import {useAppSelector} from "@/redux/hooks";
+import {IState} from "@/redux/store";
 import getAppointment from "@/firebase/Appointments/getAppointment";
-import { createAppointment, validateAppointment } from "../functions/utils";
+import {createAppointment, validateAppointment} from "../functions/utils";
 import getAppointmentId from "@/firebase/Appointments/getAppointmentId";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
 import createNewAppointment from "@/firebase/Appointments/createNewAppointment";
 import updateAppointmentField from "@/firebase/Appointments/updateAppointmentField";
 import checkAppointment from "@/firebase/Appointments/checkAppointment";
+import {ca} from "date-fns/locale";
 
 const useAppointmentURLParams = () => {
-    const params = useSearchParams();
-    const dispatch = useAppointmentDispatch();
-    const appointment = useAppointment();
+  const params = useSearchParams();
+  const dispatch = useAppointmentDispatch();
+  const appointment = useAppointment();
 
-    const [appointmentExisted, setAppointmentExisted] = useState<null | boolean>(null);
-    const [appointmentUpdated, setAppointmentUpdated] = useState<boolean>(false);
+  const [appointmentExisted, setAppointmentExisted] = useState<null | boolean>(
+    null
+  );
+  const [appointmentUpdated, setAppointmentUpdated] = useState<boolean>(false);
 
-    const user: IUserState = useAppSelector((state: IState) => state.user);
-    const { userInfo } = user;
+  const user: IUserState = useAppSelector((state: IState) => state.user);
+  const {userInfo} = user;
 
-    const [pageState, setPageState] = useState<{
-        loading: boolean;
-        error: string;
-    }>({ loading: false, error: "" });
+  const [pageState, setPageState] = useState<{
+    loading: boolean;
+    error: string;
+  }>({loading: false, error: ""});
 
+  useEffect(() => {
+    const constructAppointment = async () => {
+      try {
+        const newAppointmentId = (await getAppointmentId(appointment))._id;
+        const newAppointment = {
+          ...createAppointment(appointment, userInfo),
+          _id: newAppointmentId,
+        };
+        dispatch({type: "SET_APPOINTMENT", payload: newAppointment});
+        setAppointmentUpdated(true);
+      } catch (error) {
+        setPageState({loading: false, error: "Error creating appointment"});
+      }
+    };
 
-    useEffect(() => {
-
-        const constructAppointment = async () => {
-            const newAppointmentId = (await getAppointmentId(appointment))._id
-            const newAppointment = { ...createAppointment(appointment, userInfo), _id: newAppointmentId };
-            dispatch({ type: "SET_APPOINTMENT", payload: newAppointment });
-            setAppointmentUpdated(true);
+    const getAppointmentData = async () => {
+      try {
+        const appointmentId = params.get("appId");
+        if (appointmentId === null) {
+          setAppointmentExisted(false);
+          return;
         }
 
-        const getAppointmentData = async () => {
-            const appointmentId = params.get("appId");
-            if (appointmentId === null) {
-                setAppointmentExisted(false);
-                return;
-            }
-
-            const newAppointment = await getAppointment(appointmentId);
-            if (newAppointment === null) {
-                setAppointmentExisted(false);
-                return;
-            }
-            dispatch({ type: "SET_APPOINTMENT", payload: newAppointment });
-            setAppointmentExisted(true);
-            setPageState({ loading: false, error: "" });
-
+        const newAppointment = await getAppointment(appointmentId);
+        if (newAppointment === null) {
+          setAppointmentExisted(false);
+          return;
         }
+        dispatch({type: "SET_APPOINTMENT", payload: newAppointment});
+        setAppointmentExisted(true);
+        setPageState({loading: false, error: ""});
+      } catch (error) {
+        setPageState({loading: false, error: "Error creating appointment"});
+      }
+    };
 
-        const uploadAppointment = async () => {
-            if (!appointmentExisted && validateAppointment(appointment) && !(await checkAppointment(appointment._id))) {
-                await createNewAppointment(appointment);
-            } else {
-                await updateAppointmentField(appointment._id, "status", "pending");
-            }
-            setPageState({ loading: false, error: "" });
-        }
-
-        if (appointmentExisted === null) {
-            setPageState({ loading: true, error: "" });
-            getAppointmentData();
+    const uploadAppointment = async () => {
+      try {
+        if (
+          !appointmentExisted &&
+          validateAppointment(appointment) &&
+          !(await checkAppointment(appointment._id))
+        ) {
+          await createNewAppointment(appointment);
         } else {
-            if (!appointmentExisted) {
-                if (appointment.patientId === "" && userInfo._id !== "" && appointment.date !== null && appointment._id === "") {
-                    constructAppointment();
-                    return;
-                }
-                if (appointmentUpdated) uploadAppointment();
-            }
+          await updateAppointmentField(appointment._id, "status", "pending");
         }
-    }, [appointment, user, appointmentExisted])
+        setPageState({loading: false, error: ""});
+      } catch (error) {
+        setPageState({loading: false, error: "Error creating appointment"});
+      }
+    };
 
-    return { appointmentExisted, appointment, pageState, setPageState };
+    if (appointmentExisted === null) {
+      setPageState({loading: true, error: ""});
+      getAppointmentData();
+    } else {
+      if (!appointmentExisted) {
+        if (
+          appointment.patientId === "" &&
+          userInfo._id !== "" &&
+          appointment.date !== null &&
+          appointment._id === ""
+        ) {
+          constructAppointment();
+          return;
+        }
+        if (appointmentUpdated) uploadAppointment();
+      }
+    }
+  }, [appointment, user, appointmentExisted]);
 
-}
-
-
+  return {appointmentExisted, appointment, pageState, setPageState};
+};
 
 export default useAppointmentURLParams;
