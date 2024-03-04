0fbf9ba9ce2f6fb2df27aba53ef7a7cc2a809bdb
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-04 16:46:01 -0500

    :sparkles: Refactor createZoomAppointment function

--- src/lib/zoom/createZoomAppointment.ts
+++ src/lib/zoom/createZoomAppointment.ts
@@ -1,24 +1,36 @@
-import { ICreateMeeting } from "@/utils/Interfaces/API/Zoom/ICreateMeeting";
-import { DateValue } from "@/utils/alias/alias";
+import {ICreateMeeting} from "@/utils/Interfaces/API/Zoom/ICreateMeeting";
+import {DateValue} from "@/utils/alias/alias";
 import RoutesZoom from "@/utils/routes/RoutesZoom";
-import { Timestamp } from "firebase/firestore";
-
+import {Timestamp} from "firebase/firestore";
 
 interface IResponse {
-    data: ICreateMeeting
+  data: ICreateMeeting;
 }
 
+const createZoomAppointment = async (
+  appointmentId: string,
+  startTime: DateValue
+) => {
+  try {
+    const dateString = (startTime as Timestamp).toDate().toISOString();
+    const rest = await fetch(
+      `${process.env.NEXT_PUBLIC_MYPAGE_URL}${RoutesZoom.ZOOM_MEETING}?appId=${appointmentId}&start_time=${dateString}`
+    );
 
-const createZoomAppointment = async (appointmentId: string, startTime: DateValue) => {
-    try {
-        const dateString = (startTime as Timestamp).toDate().toISOString();
-        const rest = await fetch(`${process.env.NEXT_PUBLIC_MYPAGE_URL}${RoutesZoom.ZOOM_MEETING}?appId=${appointmentId}&start_time=${dateString}`);
-        const data: IResponse = await rest.json();
-        return data.data.join_url;
-    } catch (error) {
-        throw new Error("Error creating Zoom appointment, please contact support");
+    if (!rest.ok) {
+      throw new Error(
+        "Error creating Zoom appointment, please contact support"
+      );
     }
-};
 
+    const data: IResponse = await rest.json();
+    if (!data.data.join_url || data.data.join_url === "")
+      throw new Error("Link not created");
+
+    return data.data.join_url;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export default createZoomAppointment;
