de181bef9477fd3d137a63499e8e098fa4f25f13
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-04 16:42:55 -0500

    :sparkles: Get token auth handlers

--- src/lib/zoom/getTokenAuth.ts
+++ src/lib/zoom/getTokenAuth.ts
@@ -2,15 +2,19 @@ import IAuthResponse from "@/utils/Interfaces/API/Zoom/IAuthResponse";
 import RoutesZoom from "@/utils/routes/RoutesZoom";
 
 const getTokenAuth = async () => {
-    try{    
-        const res = await fetch(`${process.env.NEXT_PUBLIC_MYPAGE_URL}${RoutesZoom.ZOOM_AUTH}`);
-        const dataResponse : IAuthResponse = await res.json();
-        return dataResponse.data.access_token;
-
-    } catch (error) {
-        throw new Error("Token not received during Zoom authentication");
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_MYPAGE_URL}${RoutesZoom.ZOOM_AUTH}`
+    );
+    if (!res.ok) {
+      throw new Error("Failed to get the Zoom Token");
     }
-};
 
+    const dataResponse: IAuthResponse = await res.json();
+    return dataResponse.data.access_token;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export default getTokenAuth;
