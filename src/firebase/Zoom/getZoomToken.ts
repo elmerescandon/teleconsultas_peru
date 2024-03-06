30eb627442c6315eb7aeb5bbcea1ee53ac52782b
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-06 11:25:28 -0500

    :sparkles: Add getZoomToken function to retrieve Zoom token from Firestore

---
+++ src/firebase/Zoom/getZoomToken.ts
@@ -0,0 +1,20 @@
+import {and, collection, getDocs, query, where} from "firebase/firestore";
+import dbFirestore from "../config";
+import IZoomToken from "@/utils/Interfaces/API/Zoom/IZoomToken";
+
+export const getZoomToken = async () => {
+  try {
+    const q = query(
+      collection(dbFirestore, "token"),
+      and(where("id", "==", "zoom"))
+    );
+    const snapShot = await getDocs(q);
+    if (snapShot.empty) {
+      return null;
+    }
+    const zoomToken = snapShot.docs[0].data();
+    return zoomToken as IZoomToken;
+  } catch (error) {
+    throw error;
+  }
+};
