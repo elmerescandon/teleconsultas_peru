79f045db540eecd06bc527e4e81e06e7887bc587
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-06 11:25:44 -0500

    :sparkles: Add handleZoomToken function to manage Zoom tokens

---
+++ src/lib/zoom/handleZoomToken.ts
@@ -0,0 +1,28 @@
+import {getZoomToken} from "@/firebase/Zoom/getZoomToken";
+import IZoomToken from "@/utils/Interfaces/API/Zoom/IZoomToken";
+import getTokenAuth from "./getTokenAuth";
+import addZoomToken from "@/firebase/Zoom/addZoomToken";
+import {Timestamp} from "firebase/firestore";
+
+export const handleZoomToken = async () => {
+  try {
+    let newToken: string;
+    const zoomToken: IZoomToken | null = await getZoomToken();
+    if (zoomToken === null) {
+      newToken = await getTokenAuth();
+      await addZoomToken(newToken);
+    } else {
+      const zoomTime = zoomToken.date as Timestamp;
+      // compare if zoom time is less than 1 hour considering zoomTime as Timestamp from firebase
+      if (Date.now() - zoomTime.toMillis() > 3600000) {
+        newToken = await getTokenAuth();
+        await addZoomToken(newToken);
+      } else {
+        newToken = zoomToken.token;
+      }
+    }
+    return newToken;
+  } catch (error) {
+    throw error;
+  }
+};
