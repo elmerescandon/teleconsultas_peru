eadaf32e5d18ba3e313f7a2bd240c133d33c57ad
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-18 14:03:52 -0500

    :lipstick: Change style with flexing

--- src/components/Templates/DoctorHome/DoctorHome.tsx
+++ src/components/Templates/DoctorHome/DoctorHome.tsx
@@ -4,18 +4,18 @@ import PatientMain from "@/components/Organisms/PatientMain/PatientMain";
 import React from "react";
 
 const DoctorHome = () => {
-    return (
-        <div className="flex flex-col items-center px-48 h-full py-10 max-xl:pt-36 gap-5 max-xl:px-5 w-full">
-            <PatientMain />
-            <div
-                className="w-full flex
+  return (
+    <div className="flex flex-col items-center px-48 py-10 max-xl:pt-36 gap-5 max-xl:px-5 w-full">
+      <PatientMain />
+      <div
+        className="w-full flex
                             max-xl:flex-col"
-            >
-                {/* <DoctorDashboard /> */}
-                <DoctorAppointments />
-            </div>
-        </div>
-    );
+      >
+        {/* <DoctorDashboard /> */}
+        <DoctorAppointments />
+      </div>
+    </div>
+  );
 };
 
 export default DoctorHome;
