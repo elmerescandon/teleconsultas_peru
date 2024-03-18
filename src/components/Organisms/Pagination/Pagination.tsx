56ff46c3492c874fded95b20266d7117c8083657
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-18 14:04:29 -0500

    :art: Structure format in component

--- src/components/Organisms/Pagination/Pagination.tsx
+++ src/components/Organisms/Pagination/Pagination.tsx
@@ -1,99 +1,99 @@
-import { ArrowLeftIcon, ArrowRightIcon } from "@mui/x-date-pickers";
-import React, { useState, useEffect, ReactElement } from "react";
+import {ArrowLeftIcon, ArrowRightIcon} from "@mui/x-date-pickers";
+import React, {useState, useEffect, ReactElement} from "react";
 
 interface PaginationProps {
-    items: ReactElement[];
-    itemsPerPage: number;
-    orientation: "row" | "col";
-    paginationPosition?: "left" | "right";
-    paginationVerbose?: boolean;
-    wrap?: boolean;
-    center?: boolean;
+  items: ReactElement[];
+  itemsPerPage: number;
+  orientation: "row" | "col";
+  paginationPosition?: "left" | "right";
+  paginationVerbose?: boolean;
+  wrap?: boolean;
+  center?: boolean;
 }
 
 const Pagination: React.FC<PaginationProps> = ({
-    items,
-    itemsPerPage,
-    orientation,
-    paginationPosition = "left",
-    paginationVerbose = true,
-    wrap = false,
-    center = false,
+  items,
+  itemsPerPage,
+  orientation,
+  paginationPosition = "left",
+  paginationVerbose = true,
+  wrap = false,
+  center = false,
 }) => {
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const [totalPages, setTotalPages] = useState<number>(1);
-    const [currentItems, setCurrentItems] = useState<ReactElement[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [currentItems, setCurrentItems] = useState<ReactElement[]>([]);
 
-    useEffect(() => {
-        // Calculate the total number of pages
-        const totalPagesCount = Math.ceil(items.length / itemsPerPage);
-        setTotalPages(totalPagesCount);
+  useEffect(() => {
+    // Calculate the total number of pages
+    const totalPagesCount = Math.ceil(items.length / itemsPerPage);
+    setTotalPages(totalPagesCount);
 
-        // Update the current items based on the current page
-        const startIndex = (currentPage - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const itemsToDisplay = items.slice(startIndex, endIndex);
-        setCurrentItems(itemsToDisplay);
-    }, [items, itemsPerPage, currentPage]);
+    // Update the current items based on the current page
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const itemsToDisplay = items.slice(startIndex, endIndex);
+    setCurrentItems(itemsToDisplay);
+  }, [items, itemsPerPage, currentPage]);
 
-    const handlePageChange = (newPage: number) => {
-        if (newPage >= 1 && newPage <= totalPages) {
-            setCurrentPage(newPage);
-        }
-    };
+  const handlePageChange = (newPage: number) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
 
-    return (
-        <div className="w-full justify-center">
-            <div
-                className={`flex ${
-                    orientation === "col"
-                        ? "flex-col"
-                        : "flex-row justify-start items-start"
-                } gap-2 max-xl:flex-col ${wrap ? "flex-wrap" : ""} ${
-                    center ? "justify-center" : ""
-                }`}
-            >
-                {currentItems.map((item, index) => (
-                    <div key={index}>{item}</div>
-                ))}
-            </div>
-            <div
-                className={`pagination pt-3 flex items-center ${
-                    paginationPosition === "left"
-                        ? " justify-start "
-                        : paginationPosition === "right"
-                        ? "justify-end"
-                        : ""
-                }`}
-            >
-                <button
-                    onClick={() => handlePageChange(currentPage - 1)}
-                    disabled={currentPage === 1}
-                >
-                    <ArrowLeftIcon
-                        className={`w-10 h-10 text-brand-900 ${
-                            currentPage === 1 ? "text-neutral-200" : ""
-                        }`}
-                    />
-                </button>
-                {paginationVerbose && (
-                    <span>
-                        {currentPage} de {totalPages}
-                    </span>
-                )}
-                <button
-                    onClick={() => handlePageChange(currentPage + 1)}
-                    disabled={currentPage === totalPages}
-                >
-                    <ArrowRightIcon
-                        className={`w-10 h-10 text-brand-900 ${
-                            currentPage === totalPages ? "text-neutral-200" : ""
-                        }`}
-                    />
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full justify-center h-full">
+      <div
+        className={`flex ${
+          orientation === "col"
+            ? "flex-col"
+            : "flex-row justify-start items-start"
+        } gap-2 max-xl:flex-col ${wrap ? "flex-wrap" : ""} ${
+          center ? "justify-center" : ""
+        }`}
+      >
+        {currentItems.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+      <div
+        className={`pagination pt-3 flex items-center ${
+          paginationPosition === "left"
+            ? " justify-start "
+            : paginationPosition === "right"
+            ? "justify-end"
+            : ""
+        }`}
+      >
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <ArrowLeftIcon
+            className={`w-10 h-10 text-brand-900 ${
+              currentPage === 1 ? "text-neutral-200" : ""
+            }`}
+          />
+        </button>
+        {paginationVerbose && (
+          <span>
+            {currentPage} de {totalPages}
+          </span>
+        )}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          <ArrowRightIcon
+            className={`w-10 h-10 text-brand-900 ${
+              currentPage === totalPages ? "text-neutral-200" : ""
+            }`}
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
