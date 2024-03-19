8a3b9f08e552fca0af4f6d4c5978188fb354f12b
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-19 10:30:44 -0500

    :sparkles: Add PopUpAlert component

---
+++ src/components/Molecules/PopUpAlert/PopUpAlert.tsx
@@ -0,0 +1,20 @@
+import {ExclamationCircleIcon} from "@heroicons/react/24/outline";
+import React from "react";
+
+type PopUpAlertProps = {
+  text: string;
+};
+
+const PopUpAlert = ({text}: PopUpAlertProps) => {
+  return (
+    <div className="flex items-center justify-center gap-4 bg-neutral-100">
+      <div className="bg-neutral-500 w-2 h-full"></div>
+      <div className="w-full flex justify-start items-center py-4 gap-x-4">
+        <ExclamationCircleIcon className="w-10 h-10 text-neutral-700" />
+        <p className="text-neutral-700 font-medium text-center">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PopUpAlert;
