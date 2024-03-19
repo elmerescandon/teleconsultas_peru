c1fb5cf91e0528814fe2a8e5384dbd979ad2e456
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-19 20:40:49 +0100

    :sparkles: Add DoctorAvailableDays component to PopUpMyAvailabilityV2

--- src/components/Atoms/PopUp/PopUpMyAvailability/PopUpMyAvailabilityV2.tsx
+++ src/components/Atoms/PopUp/PopUpMyAvailability/PopUpMyAvailabilityV2.tsx
@@ -4,6 +4,8 @@ import PopUpWrapper from "@/components/Templates/PopUpWrapper/PopUpWrapper";
 import DateFilter from "@/components/Molecules/DateFilter/DateFilter";
 import IDateRangeAppointment from "@/utils/Interfaces/IDateRangeAppointment";
 import PopUpAlert from "@/components/Molecules/PopUpAlert/PopUpAlert";
+import DoctorAvailableDays from "@/components/Organisms/DoctorAvailableDays/DoctorAvailableDays";
+import {getNowDay, getNowDayPlusDays} from "@/utils/functions/utilsDate";
 
 type PopUpMyAvailabilityV2Props = {
   onClose: () => void;
@@ -17,17 +19,24 @@ const PopUpMyAvailabilityV2 = ({
   specialityId,
 }: PopUpMyAvailabilityV2Props) => {
   const [date, setDate] = useState<IDateRangeAppointment>({
-    init: null,
-    finish: null,
+    init: getNowDay(),
+    finish: getNowDayPlusDays(7),
   });
 
   return (
     <PopUpWrapper>
       <PopUpHeadline title="Mi Disponibilidad" onClose={onClose} />
       <DateFilter date={date} setDate={setDate} />
-      {date.init === null && date.finish === null ? (
-        <PopUpAlert text="Indique el rango de fechas para ver las disponibilidades+" />
-      ) : null}
+      {date.init === null || date.finish === null ? (
+        <PopUpAlert text="Indique el rango de fechas para ver las disponibilidades" />
+      ) : (
+        <DoctorAvailableDays
+          doctorId={doctorId}
+          specialityId={specialityId}
+          startDate={date.init}
+          finishDate={date.finish}
+        />
+      )}
     </PopUpWrapper>
   );
 };
