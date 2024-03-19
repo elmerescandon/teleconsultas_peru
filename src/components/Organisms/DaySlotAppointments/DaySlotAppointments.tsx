41c69c2efcdd78b42f943778595cf69be0b3b9b5
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-19 20:40:19 +0100

    :sparkles: Refactor DaySlotAppointments component

--- src/components/Organisms/DaySlotAppointments/DaySlotAppointments.tsx
+++ src/components/Organisms/DaySlotAppointments/DaySlotAppointments.tsx
@@ -1,57 +1,62 @@
 import SlotAppointmentVisible from "@/components/Atoms/SlotAppointmentVisible/SlotAppointmentVisible";
 import IAvailabilitySlots from "@/utils/Interfaces/dataModel/IAvailabilitySlots";
-import { dateToSpanish } from "@/utils/functions/utilsDate";
-import { CheckCircleIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
-import React, { useState } from "react";
+import {dateToSpanish} from "@/utils/functions/utilsDate";
+import {CheckCircleIcon, PencilSquareIcon} from "@heroicons/react/24/solid";
+import React, {useState} from "react";
 
 type DaySlotAppointmentsProps = {
-    availability: IAvailabilitySlots;
-    doctorId: string;
-    specialityId: string;
-    setErased: (erased: boolean) => void;
+  availability: IAvailabilitySlots;
+  doctorId: string;
+  specialityId: string;
+  setErased: (erased: boolean) => void;
 };
 
 const DaySlotAppointments = ({
-    availability,
-    doctorId,
-    specialityId,
-    setErased,
+  availability,
+  doctorId,
+  specialityId,
+  setErased,
 }: DaySlotAppointmentsProps) => {
+  const [eliminate, setEliminate] = useState<boolean>(true);
 
-    const [eliminate, setEliminate] = useState<boolean>(true);
+  const toggleEliminate = () => {
+    setEliminate(!eliminate);
+  };
 
-    const toggleEliminate = () => {
-        setEliminate(!eliminate);
-    }
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex gap-1 items-center">
+        <p className="text-xl font-semibold max-lg:text-lg">
+          {dateToSpanish(availability.date)}
+        </p>
+        <button
+          className={`text-lg font-normal max-lg:text-base`}
+          onClick={toggleEliminate}
+        >
+          {!eliminate ? (
+            <PencilSquareIcon className="w-5 h-5 text-basic-black" />
+          ) : (
+            <CheckCircleIcon className="w-5 h-5 bg-basic-black rounded-full text-basic-white" />
+          )}
+        </button>
+      </div>
 
-    return (
-        <div className="flex flex-col gap-5">
-            <div className="flex gap-1 items-center">
-                <p className="text-xl font-semibold max-lg:text-lg">
-                    {dateToSpanish(availability.date)}
-                </p>
-                <button className={`text-lg font-normal max-lg:text-base`} onClick={toggleEliminate}>
-                    {!eliminate ?
-                        <PencilSquareIcon className="w-5 h-5 text-basic-black" /> :
-                        <CheckCircleIcon className="w-5 h-5 bg-basic-black rounded-full text-basic-white" />}
-                </button>
-            </div>
-
-            <div className="flex gap-5 flex-wrap">
-                {availability.slots.map((slot, index) => (
-                    <SlotAppointmentVisible
-                        key={index}
-                        availableAppointment={slot}
-                        date={availability.date}
-                        doctorId={doctorId}
-                        specialityId={specialityId}
-                        onEliminate={eliminate}
-                        setErased={setErased}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+      <div className="flex gap-5 flex-wrap">
+        {availability.slots.map((slot, index) => (
+          <SlotAppointmentVisible
+            key={index}
+            availableAppointment={slot}
+            date={availability.date}
+            doctorId={doctorId}
+            specialityId={specialityId}
+            setErased={setErased}
+            setError={(error) => console.log(error)}
+            setLoading={(loading) => console.log(loading)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DaySlotAppointments;
