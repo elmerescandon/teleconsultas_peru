86489213ad521a3af7b72673f1c057b2283129df
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-19 10:30:36 -0500

    :art: Structure new component

--- src/components/Molecules/DateFilter/DateFilter.tsx
+++ src/components/Molecules/DateFilter/DateFilter.tsx
@@ -1,41 +1,41 @@
 "use client";
-import { DatePicker } from "@mui/x-date-pickers";
-import { useState } from "react";
+import {DatePicker} from "@mui/x-date-pickers";
+import {useState} from "react";
 
 type DateFilterProps = {
-    date: { init: string | null; finish: string | null };
-    setDate: (date: { init: string | null; finish: string | null }) => void;
+  date: {init: string | null; finish: string | null};
+  setDate: (date: {init: string | null; finish: string | null}) => void;
 };
 
-const DateFilter = ({ date, setDate }: DateFilterProps) => {
-    const [init, setInit] = useState<string | null>(null);
-    const [finish, setFinish] = useState<string | null>(null);
-    return (
-        <div className="flex gap-5 justify-end max-xl:flex-col">
-            <DatePicker
-                maxDate={finish}
-                label="Fecha Inicio"
-                value={init}
-                onChange={(newDate) => {
-                    if (newDate) {
-                        setInit(newDate);
-                        setDate({ ...date, init: newDate.toString() });
-                    }
-                }}
-            />
-            <DatePicker
-                minDate={init}
-                label="Fecha Final"
-                value={finish}
-                onChange={(newDate) => {
-                    if (newDate) {
-                        setDate({ ...date, finish: newDate.toString() });
-                        setFinish(newDate);
-                    }
-                }}
-            />
-        </div>
-    );
+const DateFilter = ({date, setDate}: DateFilterProps) => {
+  const [init, setInit] = useState<string | null>(null);
+  const [finish, setFinish] = useState<string | null>(null);
+  return (
+    <div className="flex gap-5 justify-start max-xl:flex-col">
+      <DatePicker
+        maxDate={finish}
+        label="Fecha Inicio"
+        value={init}
+        onChange={(newDate) => {
+          if (newDate) {
+            setInit(newDate);
+            setDate({...date, init: newDate.toString()});
+          }
+        }}
+      />
+      <DatePicker
+        minDate={init}
+        label="Fecha Final"
+        value={finish}
+        onChange={(newDate) => {
+          if (newDate) {
+            setDate({...date, finish: newDate.toString()});
+            setFinish(newDate);
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default DateFilter;
