e6aba28e90101b554abd65d1f32d72131c046793
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-19 10:31:22 -0500

    :sparkles: Refactor DoctorAvailabilityHours component

--- src/components/Organisms/DoctorAvailabilityHours/DoctorAvailabilityHours.tsx
+++ src/components/Organisms/DoctorAvailabilityHours/DoctorAvailabilityHours.tsx
@@ -2,60 +2,61 @@
 import ButtonPrimary from "@/components/Atoms/Buttons/ButtonPrimary/ButtonPrimary";
 import PopUpAddAvailability from "@/components/Atoms/PopUp/PopUpAddAvailability/PopUpAddAvailability";
 import PopUpMyAvailability from "@/components/Atoms/PopUp/PopUpMyAvailability/PopUpMyAvailability";
-import { useAppSelector } from "@/redux/hooks";
+import PopUpMyAvailabilityV2 from "@/components/Atoms/PopUp/PopUpMyAvailability/PopUpMyAvailabilityV2";
+import {useAppSelector} from "@/redux/hooks";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 const DoctorAvailabilityHours = () => {
-    const user: IUserState = useAppSelector((state) => state.user);
-    const { userInfo } = user;
-    const { _id, specialities } = userInfo;
-    const [showAddAvailability, setShowAddAvailability] =
-        useState<boolean>(false);
+  const user: IUserState = useAppSelector((state) => state.user);
+  const {userInfo} = user;
+  const {_id, specialities} = userInfo;
+  const [showAddAvailability, setShowAddAvailability] =
+    useState<boolean>(false);
 
-    const [myAvaialability, setMyAvailability] = useState<boolean>(false);
+  const [myAvaialability, setMyAvailability] = useState<boolean>(false);
 
-    return (
-        <div
-            className="flex items-center gap-4
+  return (
+    <div
+      className="flex items-center gap-4
                         max-2xl:w-full
                         max-md:flex-col"
-        >
-            <ButtonPrimary
-                onClickFn={() => {
-                    setMyAvailability(true);
-                }}
-            >
-                Ver/Modificar horario
-            </ButtonPrimary>
-            <ButtonPrimary
-                onClickFn={() => {
-                    setShowAddAvailability(true);
-                }}
-            >
-                Agregar horario
-            </ButtonPrimary>
+    >
+      <ButtonPrimary
+        onClickFn={() => {
+          setMyAvailability(true);
+        }}
+      >
+        Ver/Modificar horario
+      </ButtonPrimary>
+      <ButtonPrimary
+        onClickFn={() => {
+          setShowAddAvailability(true);
+        }}
+      >
+        Agregar horario
+      </ButtonPrimary>
 
-            {myAvaialability && (
-                <PopUpMyAvailability
-                    doctorId={_id}
-                    specialityId={specialities ? specialities.toString() : ""}
-                    onClose={() => {
-                        setMyAvailability(false);
-                    }}
-                />
-            )}
-            {showAddAvailability && (
-                <PopUpAddAvailability
-                    specialityId={specialities ? specialities.toString() : ""}
-                    onClose={() => {
-                        setShowAddAvailability(false);
-                    }}
-                    doctorId={_id}
-                />
-            )}
-        </div>
-    );
+      {myAvaialability && (
+        <PopUpMyAvailabilityV2
+          doctorId={_id}
+          specialityId={specialities ? specialities.toString() : ""}
+          onClose={() => {
+            setMyAvailability(false);
+          }}
+        />
+      )}
+      {showAddAvailability && (
+        <PopUpAddAvailability
+          specialityId={specialities ? specialities.toString() : ""}
+          onClose={() => {
+            setShowAddAvailability(false);
+          }}
+          doctorId={_id}
+        />
+      )}
+    </div>
+  );
 };
 
 export default DoctorAvailabilityHours;
