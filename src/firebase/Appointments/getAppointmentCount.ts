a76f74d99d8e2690bc1f11025eeecbb860ce72ac
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 13:41:24 -0500

    :sparkles: Refactor getAppointmentCount function to include date filter

--- src/firebase/Appointments/getAppointmentCount.ts
+++ src/firebase/Appointments/getAppointmentCount.ts
@@ -1,10 +1,29 @@
-import { and, collection, getCountFromServer, query, where } from "firebase/firestore";
+import {
+  Timestamp,
+  and,
+  collection,
+  getCountFromServer,
+  query,
+  where,
+} from "firebase/firestore";
 import dbFirestore from "../config";
+import {getNowDayDate} from "@/utils/functions/utilsDate";
 
-const getAppointmentCount = async (id: string, role: string, status: string[]) => {
-    const q = query(collection(dbFirestore, "appointments"), and(where(`${role === 'doctor' ? "doctorId" : "patientId" }`, "==", id),where("status", "in", status)));
-    const snapShot = await getCountFromServer(q);
-    return snapShot.data().count;
-}
+const getAppointmentCount = async (
+  id: string,
+  role: string,
+  status: string[]
+) => {
+  const q = query(
+    collection(dbFirestore, "appointments"),
+    and(
+      where(`${role === "doctor" ? "doctorId" : "patientId"}`, "==", id),
+      where("status", "in", status),
+      where("date", ">=", Timestamp.fromDate(getNowDayDate()))
+    )
+  );
+  const snapShot = await getCountFromServer(q);
+  return snapShot.data().count;
+};
 
 export default getAppointmentCount;
