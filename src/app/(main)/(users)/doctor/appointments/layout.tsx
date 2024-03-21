eb2d3a9004d25749388a7279cc7f8cf4b2274f46
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 13:29:18 -0500

    :sparkles: Refactor DoctorAppointmentLayout component

--- src/app/(main)/(users)/doctor/appointments/layout.tsx
+++ src/app/(main)/(users)/doctor/appointments/layout.tsx
@@ -1,22 +1,20 @@
 "use client";
 import React from "react";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/es-mx";
 `
 `;
 type DoctorAppointmentLayoutProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
-const DoctorAppointmentLayout = ({
-    children,
-}: DoctorAppointmentLayoutProps) => {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es-mx">
-            {children}
-        </LocalizationProvider>
-    );
+const DoctorAppointmentLayout = ({children}: DoctorAppointmentLayoutProps) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es-mx">
+      {children}
+    </LocalizationProvider>
+  );
 };
 
 export default DoctorAppointmentLayout;
