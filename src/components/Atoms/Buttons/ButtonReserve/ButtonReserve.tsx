ccab802599bca75fca96a540a3a5488b285c9fb2
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-24 13:22:37 -0500

    :sparkles: Update ButtonReserve component to use "Siguiente" instead of "Agendar"

--- src/components/Atoms/Buttons/ButtonReserve/ButtonReserve.tsx
+++ src/components/Atoms/Buttons/ButtonReserve/ButtonReserve.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
 
 type ButtonReserveProps = {
-    onClickFn: () => void;
+  onClickFn: () => void;
 };
 
-const ButtonReserve = ({ onClickFn }: ButtonReserveProps) => {
-    return (
-        <div className="max-xl:pt-10 flex justify-end w-full max-xl:justify-center">
-            <div className="max-xl:w-1/2">
-                <ButtonPrimary onClickFn={onClickFn}>Agendar</ButtonPrimary>
-            </div>
-        </div>
-    );
+const ButtonReserve = ({onClickFn}: ButtonReserveProps) => {
+  return (
+    <div className="max-xl:pt-10 flex justify-end w-full max-xl:justify-center">
+      <div className="max-xl:w-1/2">
+        <ButtonPrimary onClickFn={onClickFn}>Siguiente</ButtonPrimary>
+      </div>
+    </div>
+  );
 };
 
 export default ButtonReserve;
