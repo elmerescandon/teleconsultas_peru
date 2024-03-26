85126c70c85270c34796658d6937f6e22e2b86fd
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 17:38:16 -0500

    :sparkles: Add loading indicator to ButtonReserve component

--- src/components/Atoms/Buttons/ButtonReserve/ButtonReserve.tsx
+++ src/components/Atoms/Buttons/ButtonReserve/ButtonReserve.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
+import LoadingHorizontal from "@/components/Molecules/Loaders/LoadingHorizontal/LoadingHorizontal";
 
 type ButtonReserveProps = {
   onClickFn: () => void;
+  loading?: boolean;
 };
 
-const ButtonReserve = ({onClickFn}: ButtonReserveProps) => {
+const ButtonReserve = ({onClickFn, loading = false}: ButtonReserveProps) => {
   return (
     <div className="max-xl:pt-10 flex justify-end w-full max-xl:justify-center">
       <div className="max-xl:w-1/2">
         <ButtonPrimary onClickFn={onClickFn}>Siguiente</ButtonPrimary>
+        {loading && <LoadingHorizontal />}
       </div>
     </div>
   );
