3c6ef09dfe3695ab8d88c62079ba77cc4e8388fe
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 17:38:47 -0500

    :sparkles: Refactor useReservation hook and update dependencies

--- src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
+++ src/components/Templates/ReserveAppointmentSection/ReserveAppointmentSection.tsx
@@ -1,118 +1,64 @@
 "use client";
 import ButtonReserve from "@/components/Atoms/Buttons/ButtonReserve/ButtonReserve";
-import PopUpReservation from "@/components/Atoms/PopUp/PopUpReservation/PopUpReservation";
-import Loading from "@/components/Molecules/Loaders/Loading/Loading";
+import PopUpReservationOptions from "@/components/Atoms/PopUp/PopUpReservationOptions/PopUpReservationOptions";
 import ReserveAppointmentCalendar from "@/components/Organisms/ReserveAppointmentCalendar/ReserveAppointmentCalendar";
 import ReserveAppointmentForms from "@/components/Organisms/ReserveAppointmentForms/ReserveAppointmentForms";
 import ReserveAppointmentHours from "@/components/Organisms/ReserveAppointmentHours/ReserveAppointmentHours";
-import getAppointment from "@/firebase/Appointments/getAppointment";
 import {updateAppointmentCanceled} from "@/firebase/Appointments/updateAppointmentCanceled";
 import {useAppSelector} from "@/redux/hooks";
 import IUserState from "@/redux/state-interfaces/User/IUserState";
-import IPopUpReserve from "@/utils/Interfaces/IPopUpReserve";
+import SalufyService from "@/services/Salufy/Salufy.services";
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 import {
   useAppointment,
   useAppointmentDispatch,
 } from "@/utils/context/AppointmentContext/AppointmentContext";
 import {validateReservation} from "@/utils/functions/utils";
+import useReservation from "@/utils/hooks/useReservation";
 import Routes from "@/utils/routes/Routes";
-import {useRouter, useSearchParams} from "next/navigation";
-import React, {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
+import React, {useState} from "react";
 
 const ReserveAppointmentSection = () => {
-  // TODO: Refactor this component to smaller parts
-  const params = useSearchParams();
-  const appointmentSearch = params.get("appId");
+  const {reschedule, pageState, setLoading, setSuccess, setError} =
+    useReservation();
+
+  const Salufy = SalufyService.getInstance();
 
   const userState: IUserState = useAppSelector((state) => state.user);
   const {logged, userInfo} = userState;
   const router = useRouter();
 
-  const [popUps, setPopUps] = useState<IPopUpReserve>({
-    uncompletedFields: false,
-    unloggedUser: false,
-  });
+  const [popUps, setPopUps] = useState<number>(0);
 
   const appointment = useAppointment();
   const dispatch = useAppointmentDispatch();
 
-  const [confirm, setConfirm] = useState<boolean>(false);
-
   const onClickReserve = async (appointment: IAppointment) => {
-    if (validateReservation(appointment)) {
-      if (!logged) {
-        setPopUps({...popUps, unloggedUser: true});
-        return;
-      }
+    try {
+      setLoading();
+      if (!validateReservation(appointment)) setPopUps(2);
+      if (!logged) setPopUps(1);
+      if (popUps !== 0) return;
 
-      if (appointment.status === "doctor-canceled") {
-        await updateAppointmentCanceled(appointment);
-        setConfirm(true);
-        router.push(Routes.PATIENT_HOME);
-        return;
+      if (reschedule) {
+        await Salufy.updateAppointmentDoctorCanceled(
+          appointment._id,
+          appointment
+        );
+      } else if (appointment._id === "" && !reschedule) {
+        const appId = await Salufy.createAppointment(appointment, userInfo._id);
+        dispatch({
+          type: "SET_APPOINTMENT",
+          payload: {...appointment, _id: appId},
+        });
+        router.push(Routes.RESERVE_PAYMENT);
       }
-
-      setConfirm(true);
-      const res = await fetch("/api/salufy/appointment", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          appointment: {
-            ...appointment,
-            patientId: userInfo._id,
-            status: "pre-reserved",
-          },
-        }),
-      });
-      const resData = await res.json();
-      const {appointmentId} = resData;
-      dispatch({
-        type: "SET_APPOINTMENT",
-        payload: {...appointment, _id: appointmentId},
-      });
-      router.push(Routes.RESERVE_PAYMENT);
-    } else {
-      setPopUps({...popUps, uncompletedFields: true});
-      return;
+      setSuccess();
+    } catch (error) {
+      setError(error as Error);
     }
   };
-
-  useEffect(() => {
-    const getAppointmentData = async (id: string) => {
-      try {
-        const newAppointment = await getAppointment(id);
-        if (
-          newAppointment !== null &&
-          newAppointment.status === "doctor-canceled"
-        ) {
-          dispatch({
-            type: "SET_APPOINTMENT",
-            payload: newAppointment,
-          });
-          dispatch({
-            type: "SET_DOCTOR",
-            payload: "",
-          });
-          dispatch({
-            type: "SET_TIME",
-            payload: {
-              startDate: null,
-              endDate: null,
-            },
-          });
-        }
-      } catch (error) {
-        router.push(Routes.PATIENT_HOME);
-      }
-    };
-
-    if (appointmentSearch !== null && appointmentSearch !== "") {
-      getAppointmentData(appointmentSearch);
-    }
-  }, [appointmentSearch]);
 
   return (
     <div
@@ -127,34 +73,11 @@ const ReserveAppointmentSection = () => {
           <ReserveAppointmentHours />
         </div>
       </div>
-      <ButtonReserve onClickFn={() => onClickReserve(appointment)} />
-      {popUps.uncompletedFields && (
-        <PopUpReservation
-          title="¡Lo sentimos!"
-          message="Debes completar todos los campos para poder agendar una cita"
-          onClose={() => {
-            setPopUps({...popUps, uncompletedFields: false});
-          }}
-        />
-      )}
-      {popUps.unloggedUser && (
-        <PopUpReservation
-          requireSession={true}
-          title="¡Ya falta poco!"
-          message="Inicia sesión o regístrate para agendar una cita"
-          onClose={() => {
-            setPopUps({...popUps, unloggedUser: false});
-          }}
-        />
-      )}
-      {confirm && (
-        <div className="flex gap-3">
-          <p>
-            Se realizó correctamente el registro de la cita, por favor espere
-          </p>
-          <Loading />
-        </div>
-      )}
+      <ButtonReserve
+        loading={pageState === "loading"}
+        onClickFn={() => onClickReserve(appointment)}
+      />
+      <PopUpReservationOptions popUps={popUps} setPopUps={setPopUps} />
     </div>
   );
 };
