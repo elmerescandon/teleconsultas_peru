df96bdf02f7a24499484fb88c2462259f58e59bb
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 16:11:59 -0500

    :sparkles: Update payLater function return type to Promise<void>

--- src/components/Molecules/PaymentLater/PaymentLater.tsx
+++ src/components/Molecules/PaymentLater/PaymentLater.tsx
@@ -2,21 +2,21 @@ import ButtonThird from "@/components/Atoms/Buttons/ButtonThird/ButtonThird";
 import React from "react";
 
 type PaymentLaterProps = {
-    payLater: () => void;
+  payLater: () => Promise<void>;
 };
 
-const PaymentLater = ({ payLater }: PaymentLaterProps) => {
-    return (
-        <div>
-            <ButtonThird onClickFn={payLater}>
-                Ahora no, deseo pagar más tarde.
-            </ButtonThird>
-            <p className="text-sm text-gray-500">
-                * Recuerda que tienes hasta 48 horas antes de tu cita para
-                realizar el pago o se cancelará de forma automática.
-            </p>
-        </div>
-    );
+const PaymentLater = ({payLater}: PaymentLaterProps) => {
+  return (
+    <div>
+      <ButtonThird onClickFn={payLater}>
+        Ahora no, deseo pagar más tarde.
+      </ButtonThird>
+      <p className="text-sm text-gray-500">
+        * Recuerda que tienes hasta 48 horas antes de tu cita para realizar el
+        pago o se cancelará de forma automática.
+      </p>
+    </div>
+  );
 };
 
 export default PaymentLater;
