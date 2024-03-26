6c70102a284d7bc33e6bbbc731f46aadfdf7fd1a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 12:42:33 -0500

    :fire: Refactor ReserveMainSection component

--- src/components/Templates/ReserveMainSection/ReserveMainSection.tsx
+++ src/components/Templates/ReserveMainSection/ReserveMainSection.tsx
@@ -1,18 +1,16 @@
 "use client";
-import SelectReservation from "@/components/Organisms/SelectReservation/SelectReservation";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import ReserveAppointmentSection from "../ReserveAppointmentSection/ReserveAppointmentSection";
-import ReserveAvailabilitySection from "../ReserveAvailabilitySection/ReserveAvailabilitySection";
 
 const ReserveMainSection = () => {
-    const [type, setType] = useState(1);
-    return (
-        <div>
-            {/* <SelectReservation type={type} setType={setType} /> */}
-            {/* {type === 0 ? <ReserveAvailabilitySection /> : null} */}
-            {type === 1 ? <ReserveAppointmentSection /> : null}
-        </div>
-    );
+  const [type, setType] = useState(1);
+  return (
+    <div>
+      {/* <SelectReservation type={type} setType={setType} /> */}
+      {/* {type === 0 ? <ReserveAvailabilitySection /> : null} */}
+      {type === 1 ? <ReserveAppointmentSection /> : null}
+    </div>
+  );
 };
 
 export default ReserveMainSection;
