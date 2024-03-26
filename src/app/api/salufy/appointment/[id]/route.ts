705d6494221888262de327cc5359e6d39c109531
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 17:34:28 -0500

    :sparkles: Add route for fetching appointment by ID

---
+++ src/app/api/salufy/appointment/[id]/route.ts
@@ -0,0 +1,20 @@
+import getAppointment from "@/firebase/Appointments/getAppointment";
+import {NextApiRequest, NextApiResponse} from "next";
+import {NextResponse} from "next/server";
+
+export async function GET(req: NextApiRequest) {
+  try {
+    const {id} = req.query;
+    if (!id || id === "") {
+      throw new Error("No id provided");
+    }
+    const appointment = await getAppointment(id as string);
+
+    if (!appointment) {
+      throw new Error("Failed to fetch appointment");
+    }
+    return new NextResponse(JSON.stringify({appointment}), {status: 200});
+  } catch (error) {
+    return new NextResponse((error as Error).message, {status: 406});
+  }
+}
