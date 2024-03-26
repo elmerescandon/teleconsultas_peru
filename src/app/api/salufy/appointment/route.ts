71f7dc4ae71afd59ed2b41b36e86f880cb5f7a11
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 12:37:51 -0500

    :sparkles: Fix async issue in createAppointment function

--- src/app/api/salufy/appointment/route.ts
+++ src/app/api/salufy/appointment/route.ts
@@ -6,7 +6,7 @@ import {NextResponse} from "next/server";
 export async function POST(request: Request) {
   try {
     const {appointment} = (await request.json()) as {appointment: IAppointment};
-    const appointmentId = createAppointment(appointment);
+    const appointmentId = await createAppointment(appointment);
     return new NextResponse(JSON.stringify({appointmentId}), {status: 200});
   } catch (error) {
     return new NextResponse((error as Error).message, {status: 406});
