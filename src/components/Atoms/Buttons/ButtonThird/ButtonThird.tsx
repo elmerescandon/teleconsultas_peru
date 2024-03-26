10ff01e3126888f29bd7da3caf92a62316d48fda
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 13:16:38 -0500

    :sparkles: Refactor ButtonThird component

--- src/components/Atoms/Buttons/ButtonThird/ButtonThird.tsx
+++ src/components/Atoms/Buttons/ButtonThird/ButtonThird.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 type ButtonThirdProps = {
-    children: React.ReactNode;
-    onClickFn: () => void;
+  children: React.ReactNode;
+  onClickFn: () => void | Promise<void> | (() => void);
 };
 
-const ButtonThird = ({ children, onClickFn }: ButtonThirdProps) => {
-    return (
-        <button
-            className="text-brand-600 bg-classic-white rounded-lg p-2 text-base font-medimum border-2 border-brand-600 hover:bg-brand-600 hover:text-basic-white transition duration-300 ease-in-out"
-            onClick={onClickFn}
-        >
-            {children}
-        </button>
-    );
+const ButtonThird = ({children, onClickFn}: ButtonThirdProps) => {
+  return (
+    <button
+      className="text-brand-600 bg-classic-white rounded-lg p-2 text-base font-medimum border-2 border-brand-600 hover:bg-brand-600 hover:text-basic-white transition duration-300 ease-in-out"
+      onClick={onClickFn}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default ButtonThird;
