571fa470711ca4c46f96b9443b77b4f9b54d32c9
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 12:29:59 -0500

    :hammer: Add createAppointment function to handle appointment creation

---
+++ src/firebase/Appointments/createAppointment.ts
@@ -0,0 +1,27 @@
+import {collection, addDoc, Timestamp, updateDoc} from "firebase/firestore";
+import dbFirestore from "../config";
+import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
+import {validateAppointment} from "../utils/appointmentUtils";
+
+const createAppointment = async (appointment: IAppointment) => {
+  try {
+    if (!validateAppointment(appointment))
+      throw new Error("Datos de la cita inválidos");
+
+    const docRef = await addDoc(collection(dbFirestore, "appointments"), {
+      ...appointment,
+      date: new Date(appointment.date as Date),
+      startDate: new Date(appointment.startDate as Date),
+      endDate: new Date(appointment.endDate as Date),
+    });
+
+    // Update according to new ID
+    await updateDoc(docRef, {_id: docRef.id});
+
+    return docRef.id;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export default createAppointment;
