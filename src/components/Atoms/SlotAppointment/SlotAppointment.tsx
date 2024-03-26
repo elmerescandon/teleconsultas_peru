102fed438b26c2abe6e01796051da4c2235b46fa
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 11:37:19 -0500

    :hammer: Send time stamps as dates to appointment context

--- src/components/Atoms/SlotAppointment/SlotAppointment.tsx
+++ src/components/Atoms/SlotAppointment/SlotAppointment.tsx
@@ -1,50 +1,56 @@
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
-import { useAppointmentDispatch } from "@/utils/context/AppointmentContext/AppointmentContext";
-import { getHourRange } from "@/utils/functions/utilsDate";
+import {useAppointmentDispatch} from "@/utils/context/AppointmentContext/AppointmentContext";
+import {getHourRange} from "@/utils/functions/utilsDate";
+import {Timestamp} from "firebase/firestore";
 import React from "react";
 
 type SlotAppointmentProps = {
-    availableAppointment: IAvailableAppointment;
-    id: number;
-    currentId: number;
-    setId: (id: number) => void;
+  availableAppointment: IAvailableAppointment;
+  id: number;
+  currentId: number;
+  setId: (id: number) => void;
 };
 
 const SlotAppointment = ({
-    availableAppointment,
-    id,
-    currentId,
-    setId,
+  availableAppointment,
+  id,
+  currentId,
+  setId,
 }: SlotAppointmentProps) => {
-    const { startDate, endDate, available } = availableAppointment;
-    const dispatch = useAppointmentDispatch();
-    return (
-        <button
-            disabled={!available}
-            className={`
+  const {startDate, endDate, available} = availableAppointment;
+  const dispatch = useAppointmentDispatch();
+  return (
+    <button
+      disabled={!available}
+      className={`
                 px-3 py-2 rounded-md text-center text-xs font-semibold
-                ${available
+                ${
+                  available
                     ? "bg-brand-600 text-basic-white "
                     : "bg-neutral-50 text-neutral-400 border-0"
                 }
 
-                ${currentId === id
+                ${
+                  currentId === id
                     ? "bg-brand-600 text-basic-white"
                     : "bg-white text-brand-900 border-2 border-brand-600"
                 }
 
                 `}
-            onClick={() => {
-                setId(id);
-                dispatch({
-                    type: "SET_TIME",
-                    payload: { startDate, endDate },
-                });
-            }}
-        >
-            {getHourRange(startDate, endDate)}
-        </button>
-    );
+      onClick={() => {
+        setId(id);
+        dispatch({
+          type: "SET_TIME",
+          payload: {
+            startDate: (startDate as Timestamp).toDate(),
+            endDate: (endDate as Timestamp).toDate(),
+          },
+        });
+      }}
+    >
+      {getHourRange(startDate, endDate)}
+    </button>
+  );
 };
 
 export default SlotAppointment;
