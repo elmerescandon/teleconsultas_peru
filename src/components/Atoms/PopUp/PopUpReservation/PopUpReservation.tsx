b933cba06e83f13346f7ebde4f9acb61c9ed228e
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 17:34:12 -0500

    :hammer: Refactor PopUpReservation component to remove unnecessary code and add optional link

--- src/components/Atoms/PopUp/PopUpReservation/PopUpReservation.tsx
+++ src/components/Atoms/PopUp/PopUpReservation/PopUpReservation.tsx
@@ -1,42 +1,34 @@
 import React from "react";
 import ButtonPrimary from "../../Buttons/ButtonPrimary/ButtonPrimary";
 import LinkPrimary from "../../Links/LinkPrimary/LinkPrimary";
-import Routes from "@/utils/routes/Routes";
 
 type PopUpReservationProps = {
-    title: string;
-    message: string;
-    onClose: () => void;
-    requireSession?: boolean;
+  title: string;
+  message: string;
+  onClose: () => void;
+  linkMessage?: string;
+  linkTo?: string;
 };
 
 const PopUpReservation = ({
-    onClose,
-    title,
-    message,
-    requireSession = false,
+  title,
+  message,
+  onClose,
+  linkMessage,
+  linkTo,
 }: PopUpReservationProps) => {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-28 rounded-3xl">
-                <div className="flex flex-col items-center gap-10">
-                    <h2 className="text-4xl font-bold">{title}</h2>
-                    <p className="text-xl">{message} </p>
-                    {requireSession && (
-                        <div className="w-full flex justify-around h-14">
-                            <LinkPrimary to={Routes.LOGIN}>
-                                Iniciar sesión
-                            </LinkPrimary>
-                            <LinkPrimary to={Routes.REGISTER}>
-                                Registrarse
-                            </LinkPrimary>
-                        </div>
-                    )}
-                    <ButtonPrimary onClickFn={onClose}>Cerrar</ButtonPrimary>
-                </div>
-            </div>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white p-28 rounded-3xl">
+        <div className="flex flex-col items-center gap-10">
+          <h2 className="text-4xl font-bold">{title}</h2>
+          <p className="text-xl">{message} </p>
+          <ButtonPrimary onClickFn={onClose}>Cerrar</ButtonPrimary>
+          {linkTo && <LinkPrimary to={linkTo}>{linkMessage}</LinkPrimary>}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PopUpReservation;
