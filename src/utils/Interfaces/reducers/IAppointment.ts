01adc48942463127aa44c2ad0e53a997bb5fb861
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 12:42:57 -0500

    :art: Fix formatting in IAppointment interface

--- src/utils/Interfaces/reducers/IAppointment.ts
+++ src/utils/Interfaces/reducers/IAppointment.ts
@@ -1,24 +1,31 @@
-import { DateValue } from "@/utils/alias/alias";
+import {DateValue} from "@/utils/alias/alias";
 
 interface IAppointment {
-    _id: string,
-    patientId: string,
-    doctorId: string,
-    startDate: DateValue | null,
-    date: DateValue | null,
-    endDate: DateValue | null,
-    status: 'scheduled' | 'completed' | 'canceled' | 'pending' | "doctor-canceled" | "patient-canceled",
-    reason: string,
-    details: string,
-    specialityId: string,
-    symptoms: string[],
-    // Doctor Side
-    price?: number,
-    diagnosis?: string[],
-    treatment?: string[],
-    prescription?: string[],
-    summary?: string,
-    joinURL?: string,
+  _id: string;
+  patientId: string;
+  doctorId: string;
+  startDate: DateValue | null;
+  date: DateValue | null;
+  endDate: DateValue | null;
+  status:
+    | "pre-reserved"
+    | "scheduled"
+    | "completed"
+    | "canceled"
+    | "pending"
+    | "doctor-canceled"
+    | "patient-canceled";
+  reason: string;
+  details: string;
+  specialityId: string;
+  symptoms: string[];
+  // Doctor Side
+  price?: number;
+  diagnosis?: string[];
+  treatment?: string[];
+  prescription?: string[];
+  summary?: string;
+  joinURL?: string;
 }
 
 export default IAppointment;
