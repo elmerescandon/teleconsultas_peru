3d3982a48c00eb29179a20a43bbda8cf0f7defe6
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-26 13:16:17 -0500

    :sparkles: Add useReservation hook for handling appointments

---
+++ src/utils/hooks/useReservation.ts
@@ -0,0 +1,31 @@
+import {useAppointment} from "../context/AppointmentContext/AppointmentContext";
+import usePageState from "./usePageState";
+
+const useReservation = () => {
+  const {pageState, setLoading, setError, setSuccess, errorMessage} =
+    usePageState();
+  const appointment = useAppointment();
+
+  const handleUpload = async (type: "scheduled" | "pending") => {
+    try {
+      setLoading();
+      await fetch("/api/salufy/appointment", {
+        method: "PUT",
+        body: JSON.stringify({
+          id: appointment._id,
+          appointmentFields: {status: type},
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setSuccess();
+    } catch (error) {
+      setError(error as Error);
+    }
+  };
+
+  return {appointment, pageState, errorMessage, handleUpload};
+};
+
+export default useReservation;
