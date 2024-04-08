f362b30292fe19c99eb62ccea77c98d30a82e4d2
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-08 15:43:36 -0500

    Refactor DoctorAvailableDays component to improve readability and performance

--- src/components/Organisms/DoctorAvailableDays/DoctorAvailableDays.tsx
+++ src/components/Organisms/DoctorAvailableDays/DoctorAvailableDays.tsx
@@ -1,7 +1,7 @@
 import {getAvailableDays} from "@/firebase/Availability/geAvailableDays";
 import {
-  getDaysFromRangeWithTimezone,
-  timestampToDateArray,
+    getDaysFromRangeWithTimezone,
+    timestampToDateArray,
 } from "@/utils/functions/utilsDate";
 import {Timestamp} from "firebase/firestore";
 import React, {useEffect, useState} from "react";
@@ -10,70 +10,72 @@ import PopUpAlert from "@/components/Molecules/PopUpAlert/PopUpAlert";
 import LoadingCircle from "@/components/Molecules/Loaders/LoadingCircle/LoadingCircle";
 
 type DoctorAvailableDaysProps = {
-  doctorId: string;
-  specialityId: string;
-  startDate: string;
-  finishDate: string;
+    doctorId: string;
+    specialityId: string;
+    startDate: string;
+    finishDate: string;
 };
 
 const DoctorAvailableDays = ({
-  doctorId,
-  finishDate,
-  specialityId,
-  startDate,
+    doctorId,
+    finishDate,
+    specialityId,
+    startDate,
 }: DoctorAvailableDaysProps) => {
-  const [dates, setDates] = useState<Date[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+    const [dates, setDates] = useState<Date[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
-  useEffect(() => {
-    const getAvailableDates = async () => {
-      try {
-        setLoading(true);
-        const daysRange = getDaysFromRangeWithTimezone(startDate, finishDate);
-        console.log("daysRange", daysRange);
-        const doctorAvailability = await getAvailableDays(
-          doctorId,
-          specialityId
-        );
-        if (doctorAvailability === null)
-          throw new Error("No hay disponibilidad");
+    useEffect(() => {
+        const getAvailableDates = async () => {
+            try {
+                setLoading(true);
+                const daysRange = getDaysFromRangeWithTimezone(
+                    startDate,
+                    finishDate
+                );
+                console.log("daysRange", daysRange);
+                const doctorAvailability = await getAvailableDays(
+                    doctorId,
+                    specialityId
+                );
+                if (doctorAvailability === null)
+                    throw new Error("No hay disponibilidad");
 
-        const doctorAvailableDates = timestampToDateArray(
-          doctorAvailability.dateArray as Timestamp[]
-        );
-        console.log("doctorAvailableDates", doctorAvailableDates);
-        const availableDays = daysRange.filter((day) =>
-          doctorAvailableDates.some(
-            (date) => date.toDateString() === day.toDateString()
-          )
-        );
-        setDates(availableDays);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError((error as Error).message);
-      }
-    };
-    getAvailableDates();
-  }, [doctorId, specialityId, startDate, finishDate]);
+                const doctorAvailableDates = timestampToDateArray(
+                    doctorAvailability.dateArray as Timestamp[]
+                );
+                const availableDays = daysRange.filter((day) =>
+                    doctorAvailableDates.some(
+                        (date) => date.toDateString() === day.toDateString()
+                    )
+                );
+                setDates(availableDays);
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                setError((error as Error).message);
+            }
+        };
+        getAvailableDates();
+    }, [doctorId, specialityId, startDate, finishDate]);
 
-  return (
-    <div className="flex flex-col justify-start gap-10">
-      {!loading &&
-        error === "" &&
-        dates.map((date, index) => (
-          <DaySlotAppointmentsV2
-            date={date}
-            doctorId={doctorId}
-            specialityId={specialityId}
-            key={index}
-          />
-        ))}
-      {error !== "" && <PopUpAlert text={error} />}
-      {loading && <LoadingCircle />}
-    </div>
-  );
+    return (
+        <div className="flex flex-col justify-start gap-10">
+            {!loading &&
+                error === "" &&
+                dates.map((date, index) => (
+                    <DaySlotAppointmentsV2
+                        date={date}
+                        doctorId={doctorId}
+                        specialityId={specialityId}
+                        key={index}
+                    />
+                ))}
+            {error !== "" && <PopUpAlert text={error} />}
+            {loading && <LoadingCircle />}
+        </div>
+    );
 };
 
 export default DoctorAvailableDays;
