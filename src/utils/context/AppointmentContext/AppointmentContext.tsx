5d37562892aaf500d124beb81859c80aa7a258f8
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-08 19:32:57 -0500

    Refactor AppointmentContext component for improved readability and consistency

--- src/utils/context/AppointmentContext/AppointmentContext.tsx
+++ src/utils/context/AppointmentContext/AppointmentContext.tsx
@@ -4,86 +4,101 @@ import IAppointmentActions from "../../Interfaces/reducers/IAppointmentActions";
 
 export const AppointmentContext = createContext<IAppointment>(undefined!);
 export const AppointmentDispatchContext = createContext<
-  Dispatch<IAppointmentActions>
+    Dispatch<IAppointmentActions>
 >(undefined!);
 
 type AppointmentProviderProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 const AppointmentProvider = ({children}: AppointmentProviderProps) => {
-  const [appointment, dispatch] = useReducer(appointmentReducer, {
-    specialityId: "",
-    doctorId: "",
-    reason: "",
-    symptoms: [],
-    details: "",
-    date: null,
-    startDate: null,
-    endDate: null,
-    _id: "",
-    patientId: "",
-    status: "pre-reserved",
-  });
+    const [appointment, dispatch] = useReducer(appointmentReducer, {
+        specialityId: "",
+        doctorId: "",
+        reason: "",
+        symptoms: [],
+        details: "",
+        date: null,
+        startDate: null,
+        endDate: null,
+        _id: "",
+        patientId: "",
+        status: "pre-reserved",
+    });
 
-  return (
-    <AppointmentContext.Provider value={appointment}>
-      <AppointmentDispatchContext.Provider value={dispatch}>
-        {children}
-      </AppointmentDispatchContext.Provider>
-    </AppointmentContext.Provider>
-  );
+    return (
+        <AppointmentContext.Provider value={appointment}>
+            <AppointmentDispatchContext.Provider value={dispatch}>
+                {children}
+            </AppointmentDispatchContext.Provider>
+        </AppointmentContext.Provider>
+    );
 };
 
 const appointmentReducer = (
-  appointment: IAppointment,
-  action: IAppointmentActions
+    appointment: IAppointment,
+    action: IAppointmentActions
 ) => {
-  switch (action.type) {
-    case "SET_SPECIALITY":
-      return {
-        ...appointment,
-        specialityId: action.payload,
-        doctorId: "",
-      };
-    case "SET_DOCTOR":
-      return {...appointment, doctorId: action.payload};
-    case "SET_ID":
-      return {...appointment, _id: action.payload};
-    case "SET_STATUS":
-      return {...appointment, _id: action.payload};
-    case "SET_REASON":
-      return {...appointment, reason: action.payload};
-    case "SET_SYMPTOMS":
-      return {...appointment, symptoms: action.payload};
-    case "SET_DETAILS":
-      return {...appointment, details: action.payload};
-    case "SET_DATE":
-      return {
-        ...appointment,
-        date: action.payload,
-        startDate: null,
-        endDate: null,
-      };
-    case "SET_TIME":
-      return {
-        ...appointment,
-        startDate: action.payload.startDate,
-        endDate: action.payload.endDate,
-      };
-    case "SET_APPOINTMENT":
-      return {...appointment, ...action.payload};
-    default:
-      return appointment;
-  }
+    switch (action.type) {
+        case "SET_SPECIALITY":
+            return {
+                ...appointment,
+                specialityId: action.payload,
+                doctorId: "",
+            };
+        case "SET_DOCTOR":
+            return {...appointment, doctorId: action.payload};
+        case "SET_ID":
+            return {...appointment, _id: action.payload};
+        case "SET_STATUS":
+            return {...appointment, status: action.payload};
+        case "SET_REASON":
+            return {...appointment, reason: action.payload};
+        case "SET_SYMPTOMS":
+            return {...appointment, symptoms: action.payload};
+        case "SET_DETAILS":
+            return {...appointment, details: action.payload};
+        case "SET_DATE":
+            return {
+                ...appointment,
+                date: action.payload,
+                startDate: null,
+                endDate: null,
+            };
+        case "SET_TIME":
+            return {
+                ...appointment,
+                startDate: action.payload.startDate,
+                endDate: action.payload.endDate,
+            };
+        case "SET_APPOINTMENT":
+            return {...appointment, ...action.payload};
+        case "RESET":
+            return {
+                ...appointment,
+                specialityId: "",
+                doctorId: "",
+                reason: "",
+                symptoms: [],
+                details: "",
+                date: null,
+                startDate: null,
+                endDate: null,
+                _id: "",
+                patientId: "",
+                status: "pre-reserved",
+            } as IAppointment;
+        default:
+            return appointment;
+    }
 };
 
 export const useAppointment = () => {
-  return useContext(AppointmentContext);
+    return useContext(AppointmentContext);
 };
 
 export const useAppointmentDispatch = () => {
-  return useContext(AppointmentDispatchContext);
+    return useContext(AppointmentDispatchContext);
 };
 
 export default AppointmentProvider;
