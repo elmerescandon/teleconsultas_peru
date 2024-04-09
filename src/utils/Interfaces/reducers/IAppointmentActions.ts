a48b205ad5d12ce4694f2b1c74bc66994a234a12
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-08 19:33:04 -0500

    Refactor IAppointmentActions.ts for improved readability and consistency

--- src/utils/Interfaces/reducers/IAppointmentActions.ts
+++ src/utils/Interfaces/reducers/IAppointmentActions.ts
@@ -1,66 +1,78 @@
-import { DateValue } from "@/utils/alias/alias"
-import IAppointment from "./IAppointment"
+import {DateValue} from "@/utils/alias/alias";
+import IAppointment from "./IAppointment";
 
 type IAppointmentActionSetSpeciality = {
-    type: "SET_SPECIALITY",
-    payload: string,
-}
+    type: "SET_SPECIALITY";
+    payload: string;
+};
 
 type IAppointmentActionSetDoctor = {
-    type: "SET_DOCTOR",
-    payload: string,
-}
+    type: "SET_DOCTOR";
+    payload: string;
+};
 
 type IAppointmentActionSetReason = {
-    type: "SET_REASON",
-    payload: string,
-}
+    type: "SET_REASON";
+    payload: string;
+};
 
 type IAppointmentActionSetSymptoms = {
-    type: "SET_SYMPTOMS",
-    payload: string[],
-}
+    type: "SET_SYMPTOMS";
+    payload: string[];
+};
 
 type IAppointmentActionSetDetails = {
-    type: "SET_DETAILS",
-    payload: string,
-}
+    type: "SET_DETAILS";
+    payload: string;
+};
 
 type IAppointmentActionSetDate = {
-    type: "SET_DATE",
-    payload: DateValue | null,
-}
+    type: "SET_DATE";
+    payload: DateValue | null;
+};
 
 type IAppointmentActionSetTime = {
-    type: "SET_TIME",
-    payload: { startDate: DateValue | null, endDate: DateValue | null },
-}
+    type: "SET_TIME";
+    payload: {startDate: DateValue | null; endDate: DateValue | null};
+};
 
 type IAppointmentActionSetAppointment = {
-    type: "SET_APPOINTMENT",
-    payload: IAppointment,
-}
+    type: "SET_APPOINTMENT";
+    payload: IAppointment;
+};
 
 type IAppointmentActionSetId = {
-    type: "SET_ID",
-    payload: string,
-}
+    type: "SET_ID";
+    payload: string;
+};
 
 type IAppointmentActionSetStatus = {
-    type: "SET_STATUS",
-    payload: string,
-}
+    type: "SET_STATUS";
+    payload:
+        | "pre-reserved"
+        | "scheduled"
+        | "completed"
+        | "canceled"
+        | "pending"
+        | "doctor-canceled"
+        | "patient-canceled";
+};
+
+type IAppointmentActionReset = {
+    type: "RESET";
+};
 
 type IAppointmentActions =
-    IAppointmentActionSetSpeciality |
-    IAppointmentActionSetDoctor |
-    IAppointmentActionSetReason |
-    IAppointmentActionSetSymptoms |
-    IAppointmentActionSetDetails |
-    IAppointmentActionSetDate |
-    IAppointmentActionSetTime |
-    IAppointmentActionSetId |
-    IAppointmentActionSetStatus |
-    IAppointmentActionSetAppointment;
+    | IAppointmentActionSetSpeciality
+    | IAppointmentActionSetDoctor
+    | IAppointmentActionSetReason
+    | IAppointmentActionSetSymptoms
+    | IAppointmentActionSetDetails
+    | IAppointmentActionSetDate
+    | IAppointmentActionSetTime
+    | IAppointmentActionSetId
+    | IAppointmentActionSetStatus
+    | IAppointmentActionSetAppointment
+    | IAppointmentActionReset;
 
 export default IAppointmentActions;
