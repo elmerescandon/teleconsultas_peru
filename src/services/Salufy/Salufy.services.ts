a6c5eea8e067de34ac87f0e7eebf8eb696a72780
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-08 19:32:53 -0500

    Refactor SalufyService class for improved readability and consistency

--- src/services/Salufy/Salufy.services.ts
+++ src/services/Salufy/Salufy.services.ts
@@ -1,115 +1,116 @@
 import IAppointment from "@/utils/Interfaces/reducers/IAppointment";
 
 class SalufyService {
-  private static instance: SalufyService;
-  URL = "/api/salufy";
+    private static instance: SalufyService;
+    URL = "/api/salufy";
 
-  static getInstance() {
-    if (!SalufyService.instance) {
-      SalufyService.instance = new SalufyService();
+    static getInstance() {
+        if (!SalufyService.instance) {
+            SalufyService.instance = new SalufyService();
+        }
+        return SalufyService.instance;
     }
-    return SalufyService.instance;
-  }
 
-  constructor() {
-    this.URL = process.env.NEXT_PUBLIC_MYPAGE_URL + this.URL;
-  }
-
-  createAppointment = async (
-    appointment: IAppointment,
-    id: string,
-    type: "pre-reserved" | "scheduled" | "pending" = "pre-reserved"
-  ): Promise<string> => {
-    try {
-      const res = await fetch(`${this.URL}/appointment`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          appointment: {
-            ...appointment,
-            patientId: id,
-            status: type,
-          },
-        }),
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to create appointment");
-      }
-
-      const resData = await res.json();
-      const {appointmentId} = resData;
-      return appointmentId as string;
-    } catch (error) {
-      throw error as Error;
+    constructor() {
+        this.URL = process.env.NEXT_PUBLIC_MYPAGE_URL + this.URL;
     }
-  };
 
-  updateAppointmentStatus = async (
-    id: string,
-    type: "scheduled" | "pending"
-  ) => {
-    try {
-      const response = await fetch(`${this.URL}/appointment`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: id,
-          appointmentFields: {
-            status: type,
-          },
-        }),
-      });
-      return response.json();
-    } catch (error) {
-      throw Error;
-    }
-  };
+    createAppointment = async (
+        appointment: IAppointment,
+        id: string,
+        type: "pre-reserved" | "scheduled" | "pending" = "pre-reserved"
+    ): Promise<string> => {
+        try {
+            const res = await fetch(`${this.URL}/appointment`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    appointment: {
+                        ...appointment,
+                        patientId: id,
+                        status: type,
+                    },
+                }),
+            });
 
-  updateAppointmentDoctorCanceled = async (
-    id: string,
-    appointment: IAppointment
-  ) => {
-    try {
-      const response = await fetch(`${this.URL}/appointment`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: id,
-          appointmentFields: {
-            status: "pending",
-            doctorId: appointment.doctorId,
-            date: appointment.date,
-            startDate: appointment.startDate,
-            endDate: appointment.endDate,
-          },
-        }),
-      });
-      return response.json();
-    } catch (error) {
-      throw Error;
-    }
-  };
+            if (!res.ok) {
+                throw new Error("Failed to create appointment");
+            }
 
-  getAppointment = async (id: string): Promise<IAppointment> => {
-    try {
-      console.log(`${this.URL}/appointment/${id}`);
-      const res = await fetch(`${this.URL}/appointment/${id}`);
-      if (!res.ok) {
-        throw new Error("Failed to fetch appointment");
-      }
-      const {appointment} = await res.json();
-      return appointment as IAppointment;
-    } catch (error) {
-      throw error as Error;
-    }
-  };
+            const resData = await res.json();
+            const {appointmentId} = resData;
+            return appointmentId as string;
+        } catch (error) {
+            throw error as Error;
+        }
+    };
+
+    updateAppointmentStatus = async (
+        id: string,
+        type: "scheduled" | "pending"
+    ) => {
+        try {
+            const response = await fetch(`${this.URL}/appointment`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id: id,
+                    appointmentFields: {
+                        status: type,
+                    },
+                }),
+            });
+            return response.json();
+        } catch (error) {
+            throw Error;
+        }
+    };
+
+    updateAppointmentDoctorCanceled = async (
+        id: string,
+        appointment: IAppointment
+    ) => {
+        try {
+            console.log(appointment);
+            console.log(id);
+            const response = await fetch(`${this.URL}/appointment`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id: id,
+                    appointmentFields: {
+                        status: "pending",
+                        doctorId: appointment.doctorId,
+                        date: appointment.date,
+                        startDate: appointment.startDate,
+                        endDate: appointment.endDate,
+                    },
+                }),
+            });
+        } catch (error) {
+            throw Error;
+        }
+    };
+
+    getAppointment = async (id: string): Promise<IAppointment> => {
+        try {
+            console.log(`${this.URL}/appointment/${id}`);
+            const res = await fetch(`${this.URL}/appointment/${id}`);
+            if (!res.ok) {
+                throw new Error("Failed to fetch appointment");
+            }
+            const {appointment} = await res.json();
+            return appointment as IAppointment;
+        } catch (error) {
+            throw error as Error;
+        }
+    };
 }
 
 export default SalufyService;
