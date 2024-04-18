e0df1702de6ca8b97946d73fac153ee3003f931a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-17 23:05:15 -0500

    :sparkles: Avoid rewrite existing time slots

--- src/firebase/Availability/createAvailabilitySlots.ts
+++ src/firebase/Availability/createAvailabilitySlots.ts
@@ -1,52 +1,58 @@
 import {
-  addDoc,
-  and,
-  arrayUnion,
-  collection,
-  getDocs,
-  query,
-  updateDoc,
-  where,
+    addDoc,
+    and,
+    arrayUnion,
+    collection,
+    getDocs,
+    query,
+    updateDoc,
+    where,
 } from "firebase/firestore";
 import dbFirestore from "../config";
 import IAvailableAppointment from "@/utils/Interfaces/IAvailableAppointment";
 
 const createAvailabilitySlots = async (
-  date: Date,
-  specialityId: string,
-  doctorId: string,
-  slots: IAvailableAppointment[]
+    date: Date,
+    specialityId: string,
+    doctorId: string,
+    slots: IAvailableAppointment[]
 ) => {
-  try {
-    const q = query(
-      collection(dbFirestore, "availability"),
-      and(
-        where("doctor_id", "==", doctorId),
-        where("speciality_id", "==", specialityId)
-      )
-    );
-    let snapShot = await getDocs(q);
+    try {
+        const q = query(
+            collection(dbFirestore, "availability"),
+            and(
+                where("doctor_id", "==", doctorId),
+                where("speciality_id", "==", specialityId)
+            )
+        );
+        let snapShot = await getDocs(q);
 
-    if (snapShot.empty) {
-      throw new Error(
-        "No existe disponibilidad para el doctor y especialidad seleccionados."
-      );
+        if (snapShot.empty) {
+            throw new Error(
+                "No existe disponibilidad para el doctor y especialidad seleccionados."
+            );
+        }
+
+        const docDate = snapShot.docs[0];
+        await updateDoc(docDate.ref, {
+            dateArray: arrayUnion(date),
+        });
+
+        const dateCollection = collection(docDate.ref, "availability_slots");
+
+        for (const slot of slots) {
+            const slotQuery = query(
+                dateCollection,
+                where("startDate", "==", slot.startDate)
+            );
+            const slotSnapshot = await getDocs(slotQuery);
+            if (slotSnapshot.empty) {
+                await addDoc(dateCollection, slot);
+            }
+        }
+    } catch (error) {
+        throw error;
     }
-
-    const docDate = snapShot.docs[0];
-    await updateDoc(docDate.ref, {
-      dateArray: arrayUnion(date),
-    });
-
-    const dateCollection = collection(docDate.ref, "availability_slots");
-
-    for (const slot of slots) {
-      console.log(slot);
-      await addDoc(dateCollection, slot);
-    }
-  } catch (error) {
-    throw error;
-  }
 };
 
 export default createAvailabilitySlots;
