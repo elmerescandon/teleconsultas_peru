0ec6a2fe8f119f7047c67da2be65815d71c507dc
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-21 19:55:13 -0500

    :lipstick: Refactor DoctorAppointmentSection component for improved readability and consistency

--- src/components/Templates/DoctorAppointmentSection/DoctorAppointmentSection.tsx
+++ src/components/Templates/DoctorAppointmentSection/DoctorAppointmentSection.tsx
@@ -11,83 +11,90 @@ import getAppointmentsFiltered from "@/firebase/Appointments/getAppointmentsFilt
 import IDateRangeAppointment from "@/utils/Interfaces/IDateRangeAppointment";
 
 const DoctorAppointmentSection = () => {
-  const user: IUserState = useAppSelector((state) => state.user);
-  const {_id, role} = user.userInfo;
-  const [filter, setFilter] = useState<IAppointmentFilter>({
-    date: {
-      init: null,
-      finish: null,
-    },
-    speciality: "",
-    patientName: "",
-  });
+    const user: IUserState = useAppSelector((state) => state.user);
+    const {_id, role} = user.userInfo;
+    const [filter, setFilter] = useState<IAppointmentFilter>({
+        date: {
+            init: null,
+            finish: null,
+        },
+        speciality: "",
+        patientName: "",
+    });
 
-  const [appointments, setAppointments] = useState<IAppointment[]>([]);
+    const [appointments, setAppointments] = useState<IAppointment[]>([]);
 
-  useEffect(() => {
-    const getAppointments = async (
-      id: string,
-      role: string,
-      status: string[],
-      date: IDateRangeAppointment,
-      specialityId: string
-    ) => {
-      const appointments = await getAppointmentsFiltered(
-        id,
-        role,
-        status,
-        date,
-        specialityId
-      );
-      if (appointments) {
-        const userFilteredAppointments = appointments.filter((appointment) => {
-          if (filter.patientName === "") return true;
-          return appointment.patientId === filter.patientName;
-        });
-        setAppointments(userFilteredAppointments);
-      }
-    };
-    getAppointments(
-      _id,
-      role,
-      ["scheduled", "pending", "doctor-canceled", "patient-canceled"],
-      filter.date,
-      filter.speciality
-    );
-  }, [filter, _id, role]);
+    useEffect(() => {
+        const getAppointments = async (
+            id: string,
+            role: string,
+            status: string[],
+            date: IDateRangeAppointment,
+            specialityId: string
+        ) => {
+            const appointments = await getAppointmentsFiltered(
+                id,
+                role,
+                status,
+                date,
+                specialityId
+            );
+            if (appointments) {
+                const userFilteredAppointments = appointments.filter(
+                    (appointment) => {
+                        if (filter.patientName === "") return true;
+                        return appointment.patientId === filter.patientName;
+                    }
+                );
+                setAppointments(userFilteredAppointments);
+            }
+        };
+        getAppointments(
+            _id,
+            role,
+            ["scheduled", "pending"],
+            filter.date,
+            filter.speciality
+        );
+    }, [filter, _id, role]);
 
-  return (
-    <div
-      className="px-48 pb-5
+    return (
+        <div
+            className="px-48 pb-5
                         max-xl:pt-24 max-xl:px-10"
-    >
-      <p className="text-2xl font-semibold">Mis citas</p>
-      <DoctorAppointmentFilter
-        appointments={appointments}
-        filter={filter}
-        setFilter={setFilter}
-      />
-      <div className="h-[624px]">
-        {appointments.length !== 0 && (
-          <Pagination
-            wrap={true}
-            center={true}
-            items={appointments.map((appointment, index) => {
-              return <PatientCard appointment={appointment} key={index} />;
-            })}
-            itemsPerPage={6}
-            orientation="row"
-            paginationPosition="left"
-          />
-        )}
-        {appointments.length === 0 && (
-          <p className="text-2xl font-semibold text-center mt-10 h-[40vh]">
-            Todavía no tienes citas, pronto se agendarán.
-          </p>
-        )}
-      </div>
-    </div>
-  );
+        >
+            <p className="text-2xl font-semibold">Mis citas</p>
+            <DoctorAppointmentFilter
+                appointments={appointments}
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <div className="h-[624px]">
+                {appointments.length !== 0 && (
+                    <Pagination
+                        wrap={true}
+                        center={true}
+                        items={appointments.map((appointment, index) => {
+                            return (
+                                <PatientCard
+                                    appointment={appointment}
+                                    key={index}
+                                />
+                            );
+                        })}
+                        itemsPerPage={6}
+                        orientation="row"
+                        paginationPosition="left"
+                    />
+                )}
+                {appointments.length === 0 && (
+                    <p className="text-2xl font-semibold text-center mt-10 h-[40vh]">
+                        Todavía no tienes citas, pronto se agendarán.
+                    </p>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default DoctorAppointmentSection;
